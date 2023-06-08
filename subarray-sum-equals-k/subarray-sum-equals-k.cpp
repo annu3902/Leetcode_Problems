34b8965d1726fb1cfdaa7cd9e99b95e389300799
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-08 18:00:05 +0530

    Time: 150 ms (13.91%) | Memory: 43.9 MB (45.48%) - LeetSync

--- subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,47 +1,19 @@
-// class Solution {
-// public:
-//     int subarraySum(vector<int>& nums, int k) {
-//         int sum =0; 
-//         int cnt =0;
-//         map<int, int > mpp;
-//         // mpp[0] = -1;
-//         for(int i=0; i<nums.size(); i++)
-//         {
-//             sum = sum + nums[i];
-//             if(sum == k){
-//                 cnt++;
-//             }
-//             int rem = sum - k;
-//             if(rem !=0){
-//                 if(mpp.find(rem) != mpp.end()){
-//                     cnt++;
-//                 }
-//             }
-//             if(mpp.find(sum) == mpp.end()){
-//                 mpp[sum] = i;
-                
-//             }
-//             // if(sum == 0){
-//             //     mpp[sum] = i;
-//             // }
-//         }
-//         return cnt;
-//     }
-// };
-
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int sum = 0; 
-        int cnt = 0;
-        unordered_map<int, int> freq;
-        freq[0] = 1; // To handle cases when sum == k
-        for (int i = 0; i < nums.size(); i++) {
+        map<int, int> mpp;
+        mpp[0] = 1;
+        int n =nums.size();
+        int cnt =0;
+        int sum =0;
+        for(int i=0; i<n; i++)
+        {
             sum += nums[i];
-            if (freq.count(sum - k)) {
-                cnt += freq[sum - k];
+            if(mpp.find(sum-k) != mpp.end())
+            {
+                cnt = cnt + mpp[sum-k];
             }
-            freq[sum]++;
+            mpp[sum]++;
         }
         return cnt;
     }
