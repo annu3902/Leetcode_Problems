022de8cedd264029455542f157b617d52be60df3
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-10 02:03:44 +0530

    Time: 170 ms (32.28%) | Memory: 71.6 MB (22.36%) - LeetSync

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            mpp[nums[i]]++;
+        }
+
+        for(auto it : mpp)
+        {
+            if(it.second >= 2){
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+};
