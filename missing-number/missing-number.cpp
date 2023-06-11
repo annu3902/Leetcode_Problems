66cdf3b2e578ba078fea0669832efbf65bac05ce
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-11 16:57:36 +0530

    Time: 21 ms (64.46%) | Memory: 18 MB (50.03%) - LeetSync

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n =nums.size();
+
+        int XOR_1 =0;
+        int XOR_2 =0;
+
+        for(int i=0; i<n; i++)
+        {
+            XOR_1 =XOR_1^ nums[i];
+        }
+
+        for(int i=0; i<=n; i++)
+        {
+            XOR_2 = XOR_2^ i;
+        }
+        int ans = (XOR_1 ^ XOR_2);
+        return ans;
+    }
+};
