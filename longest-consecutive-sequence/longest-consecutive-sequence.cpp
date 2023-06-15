f0e5785594b1fad938a4c037401f7ff48588fb8b
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-15 12:04:35 +0530

    Time: 256 ms (42.76%) | Memory: 68.9 MB (9.82%) - LeetSync

---
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n =nums.size();
+
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+
+        else{
+            set<int> st;
+            st.insert(nums.begin(), nums.end());
+            int cnt =1;
+            int maxi = 0;
+            for(set<int> :: iterator it =st.begin(); it != st.end(); it++)
+            {
+                if (st.find(*(it)+1) != st.end())
+                {
+                    cnt++;
+                }
+
+                else{
+                    maxi = max(maxi, cnt);
+                    cnt =1;
+                }
+            }
+
+            maxi = max(maxi , cnt);
+        return maxi;
+        }
+    return -1;
+    }
+};
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         int n = nums.size();
+
+//         if (n == 0) return 0;
+//         else if (n == 1) return 1;
+
+//         else {
+//             set<int> st(nums.begin(), nums.end());
+//             int cnt = 1;
+//             int maxi = 0;
+            
+//             for (set<int>::iterator it = st.begin(); it != st.end(); it++) {
+//                 if (st.find(*(it)+1) != st.end()) {
+//                     cnt++;
+//                 }
+//                 else {
+//                     maxi = max(maxi, cnt);
+//                     cnt = 1;
+//                 }
+//             }
+
+//             maxi = max(maxi, cnt);
+//         return maxi;
+//         }
+        
+//     return -1;
+//     }
+// };
