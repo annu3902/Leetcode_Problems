a5394d0818fd40000a825d557504faad9a624f4b
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-16 03:33:37 +0530

    Time: 200 ms (5.87%) | Memory: 28 MB (5.12%) - LeetSync

---
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans;
+
+        vector<int> prefix;
+        vector<int> postfix;
+        int n = nums.size();
+
+        int pre_product = 1;
+        prefix.push_back(1);
+
+        for(int i=1; i<n; i++)
+        {
+            pre_product *= nums[i-1];
+            prefix.push_back(pre_product);
+        }
+
+        for(int i=0; i<n; i++){
+            cout<<prefix[i]<<" ";
+        }
+        cout<<endl;
+
+        int post_product = 1;
+        postfix.push_back(1);
+
+        for(int i=n-2; i>=0; i--)
+        {
+            post_product *= nums[i+1];
+            postfix.push_back(post_product);
+        }
+        reverse(postfix.begin(),postfix.end());
+
+        for(int i=0; i<n; i++){
+            cout<<postfix[i]<<" ";
+        }
+        cout<<endl;
+
+        // vector<int> ans;
+
+        for(int i=0; i<n; i++)
+        {
+            int result = prefix[i] * postfix[i];
+            ans.push_back(result);
+        }
+        return ans;
+    }
+};
