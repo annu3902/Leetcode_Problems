1e0c30858c5c2498a33b613b7c83a1b17102aa0c
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-19 23:20:02 +0530

    Time: 463 ms (22.48%) | Memory: 194.2 MB (12.14%) - LeetSync

---
+++ sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n =nums.size();
+        int i =0;
+        int j=0;
+        list <int> ls;
+        int maxi =INT_MIN;
+        vector<int> ans;
+
+        while(j < n){
+            while(ls.size() > 0 && ls.back() < nums[j]){
+                ls.pop_back();
+            }
+                ls.push_back(nums[j]);
+
+            if(j - i +1 <k){
+                j++;
+            }
+
+            else if(j -i +1 == k)
+            {
+                ans.push_back(ls.front());
+                maxi = ls.front();
+
+                if( nums[i] == maxi){
+                    ls.pop_front();
+                }
+
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int i = 0;
+//         int j = 0;
+//         list<int> ls;
+//         vector<int> ans;
+
+//         while (j < n) {
+//             while (!ls.empty() && ls.back() < nums[j]) {
+//                 ls.pop_back();
+//             }
+//             ls.push_back(nums[j]);
+
+//             if (j - i + 1 < k) {
+//                 j++;
+//             } else if (j - i + 1 == k) {
+//                 ans.push_back(ls.front());
+
+//                 if (nums[i] == ls.front()) {
+//                     ls.pop_front();
+//                 }
+
+//                 i++;
+//                 j++;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
