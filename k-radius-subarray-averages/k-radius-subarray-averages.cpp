58ba9c77b0bfd9f2e8758d9753a11d4091e48287
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-20 14:15:34 +0530

    Time: 255 ms (63.71%) | Memory: 129.8 MB (94.45%) - LeetSync

---
+++ k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int i=0;
+        int j=0;
+        int n =nums.size();
+        int K = 2*k+1;
+        vector<int> ans(n,-1);
+        long long int sum =0;
+        while(j < n){
+
+            sum += nums[j];
+            // if()
+            if(j-i+1 < K){
+                j++;
+            }
+
+            else if(j-i+1 ==K){
+                long long int avg = sum/K;
+                int x =(i+j)/2;
+                ans[x] = avg;
+
+                sum -= nums[i];
+                i++; j++;
+
+            }
+        }
+        return ans;
+    }
+};
