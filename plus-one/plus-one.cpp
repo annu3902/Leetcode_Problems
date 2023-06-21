5ab19eec71ffb85721710987e9a282834dc22171
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-21 15:16:17 +0530

    Time: 7 ms (14.75%) | Memory: 8.6 MB (76.74%) - LeetSync

--- plus-one/plus-one.cpp
+++ plus-one/plus-one.cpp
@@ -1,17 +1,49 @@
-class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-        for(int i= n-1; i>=0; i--){
-            if(digits[i] != 9)
-            {
-                digits[i]++;
-                return digits;
-            }
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         int n = digits.size();
+//         for(int i= n-1; i>=0; i--){
+//             if(digits[i] != 9)
+//             {
+//                 digits[i]++;
+//                 return digits;
+//             }
 
-            digits[i] =0;
+//             digits[i] =0;
+//         }
+//         digits.insert(digits.begin(),1);
+//         return digits;
+//     }
+// };
+
+class Solution {
+    public:
+    vector<int> plusOne(vector<int> & digits){
+        int n = digits.size();
+        if(digits[n-1] != 9){
+            digits[n-1]++;
         }
-        digits.insert(digits.begin(),1);
-        return digits;
+        else if(digits[n-1] == 9){
+            digits[n-1] =0;
+            for(int i =n-2; i>=0; i--)
+            {
+                if(digits[i] != 9)
+                {
+                    digits[i]++;
+                    break;
+                }
+                else{
+                    digits[i] =0;
+                }
+            }
+            if(digits[0] == 0){
+                digits.insert(digits.begin(), 1);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            cout<<digits[i]<<" ";
+        }
+    return digits;
     }
 };
