adb146629b67f33508bef76baa29c508d7b3b20b
annu3902 <100219424+annu3902@users.noreply.github.com>
2023-06-21 14:36:41 +0530

    Time: 28 ms (55.92%) | Memory: 31.7 MB (15.79%) - LeetSync

--- find-pivot-index/find-pivot-index.cpp
+++ find-pivot-index/find-pivot-index.cpp
@@ -1,21 +1,47 @@
+// class Solution {
+// public:
+//     int pivotIndex(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> prefix(n+1, 0);
+//         int ans = -1;
+//         for(int i=0; i<n; i++)
+//         {
+//             prefix[i+1] = prefix[i]+nums[i];
+//         }
+
+//         for(int i=0; i<n; i++)
+//         {
+//             if(prefix[i] == prefix[n] - prefix[i+1]){
+//                 ans =i;
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> prefix(n+1, 0);
+        int n =nums.size();
+        vector<int> ls(n);
+        int ts =0;
         int ans = -1;
+
         for(int i=0; i<n; i++)
         {
-            prefix[i+1] = prefix[i]+nums[i];
+            ts += nums[i];
+            ls[i] = ts -nums[i];
         }
 
         for(int i=0; i<n; i++)
         {
-            if(prefix[i] == prefix[n] - prefix[i+1]){
-                ans =i;
+            int rs = ts - ls[i] - nums[i];
+            if(rs == ls[i]){
+                ans = i;
                 break;
             }
         }
         return ans;
     }
-};
+ };
