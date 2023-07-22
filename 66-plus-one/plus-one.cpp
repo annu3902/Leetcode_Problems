1e68fb3f0ea7070b4ff80635f004dfa628e9bdf4
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-07-22 13:30:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (72.01%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,117 @@
+// // class Solution {
+// // public:
+// //     vector<int> plusOne(vector<int>& digits) {
+// //         int n = digits.size();
+// //         for(int i= n-1; i>=0; i--){
+// //             if(digits[i] != 9)
+// //             {
+// //                 digits[i]++;
+// //                 return digits;
+// //             }
+
+// //             digits[i] =0;
+// //         }
+// //         digits.insert(digits.begin(),1);
+// //         return digits;
+// //     }
+// // };
+
+// // class Solution {
+// //     public:
+// //     vector<int> plusOne(vector<int> & digits){
+// //         int n = digits.size();
+// //         if(digits[n-1] != 9){
+// //             digits[n-1]++;
+// //         }
+// //         else if(digits[n-1] == 9){
+// //             digits[n-1] =0;
+// //             for(int i =n-2; i>=0; i--)
+// //             {
+// //                 if(digits[i] != 9)
+// //                 {
+// //                     digits[i]++;
+// //                     break;
+// //                 }
+// //                 else{
+// //                     digits[i] =0;
+// //                 }
+// //             }
+// //             if(digits[0] == 0){
+// //                 digits.insert(digits.begin(), 1);
+// //             }
+// //         }
+
+// //         for(int i=0; i<n; i++){
+// //             cout<<digits[i]<<" ";
+// //         }
+// //     return digits;
+// //     }
+// // };
+
+// class Solution {
+//     public:
+//     vector<int> plusOne(vector<int> & digits){
+//         int n = digits.size();
+//         int i =n-1;
+//         while(digits[i] == 9 && i>=0){
+//             digits[i] =0;
+//             i--;
+//         }
+//         if(i >= 0){
+//             digits[i]++;
+//         }
+//         else if(digits[0] == 0){
+//             digits.insert(digits.begin(),1);
+//         }
+//         for(int i=0; i<digits.size(); i++){
+//         cout<<digits[i]<<" ";
+//         }
+//         cout<<endl;
+//         return digits;
+//    }
+// };
+
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int> & digits){
+//         int n = digits.size();
+//         int i = n-1;
+//         while(digits[i] == 9 && i >= 0){
+//             digits[i] = 0;
+//             i--;
+//         }
+//         if(i >= 0){
+//             digits[i]++;
+//         }
+//         else if(digits[0] == 0){
+//             digits.insert(digits.begin(), 1);
+//         }
+//         for(int i=0; i<digits.size(); i++){
+//             cout << digits[i] << " ";
+//         }
+//         cout << endl;
+//         return digits;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>  digits){
+        int n = digits.size();
+        int i = n-1;
+
+        while(i >= 0 && digits[i] == 9 ){
+            digits[i] = 0;
+            i--;
+        }
+        if(i >= 0){
+            digits[i]=digits[i]+1;
+        }
+        else{
+            digits.insert(digits.begin(), 1);
+        }
+        // n = digits.size();
+        
+        return digits;
+    }
+};
