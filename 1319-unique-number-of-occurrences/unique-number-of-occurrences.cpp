40dfa34e7c29dab81d3fff854aacc9e3654f8094
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-07-26 14:32:12 +0530

    Time: 7 ms (28.47%) | Memory: 8 MB (97.34%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,53 +1,24 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-
-        // int n = arr.size();
-        // vector<int> hashh;
-
-        // sort(arr.begin(), arr.end());
-        // int i =0;
-        // int cnt =1;
-        // for(int j=i+1; j<n; j++)
-        // {
-        //     if(arr[j] == arr[i]){
-        //         cnt++;
-        //     }
-        //     else{
-        //         hashh.push_back(cnt);
-        //         i = j;
-        //         j = i+1;
-        //         cnt = 1;
-        //     }
-        // }
-        // sort(hashh.begin(), hashh.end());
-        // // int j =0;
-        // for(int i=1; i<hashh.size(); i++)
-        // {
-        //     if(hashh[i] == hashh[i-1]){
-        //         // j++;
-        //         return false;
-        //     }
-        // }
-        // return true;
-
+        vector<int> v;
         int n =arr.size();
-        map<int,int> mpp;
-        for(int i=0; i<n; i++)
+
+        sort(arr.begin(), arr.end());
+        int cnt =1;
+
+        for(int i=1; i<n; i++)
         {
-            mpp[arr[i]]++;
+            if(arr[i] == arr[i-1]){
+                cnt++;
+            }else{
+                v.push_back(cnt);
+                // i = j;
+                cnt =1;
+            }
         }
-        vector<int>v;
-        for(auto it: mpp)
-        {
-            int value = it.second;
-            v.push_back(value);
-        }
+        v.push_back(cnt);
         sort(v.begin(), v.end());
-        for(int i=0; i<v.size(); i++)
-        {
-            cout<<v[i];
-        }
 
         for(int i=1; i<v.size(); i++)
         {
@@ -56,5 +27,33 @@ public:
             }
         }
         return true;
+
+
+
+        // int n =arr.size();
+        // map<int,int> mpp;
+        // for(int i=0; i<n; i++)
+        // {
+        //     mpp[arr[i]]++;
+        // }
+        // vector<int>v;
+        // for(auto it: mpp)
+        // {
+        //     int value = it.second;
+        //     v.push_back(value);
+        // }
+        // sort(v.begin(), v.end());
+        // for(int i=0; i<v.size(); i++)
+        // {
+        //     cout<<v[i];
+        // }
+
+        // for(int i=1; i<v.size(); i++)
+        // {
+        //     if(v[i] == v[i-1]){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 };
