5d678d157c8ab555932db449de9212c4566e7d80
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-07-26 15:27:14 +0530

    Time: 16 ms (71.17%) | Memory: 13.3 MB (32.32%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,88 +1,122 @@
+class Solution {
+public:
+
+    void mark_row(vector<vector<int>> & matrix, int i ){
+        int m =matrix.size();
+        int n =matrix[0].size();
+
+        for(int j =0; j<n; j++){
+            if(matrix[i][j] != 0){
+                matrix[i][j] = '*';
+            }
+        }
+        // return;
+    }
+
+    void mark_col(vector<vector<int>> & matrix, int j){
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for(int i =0; i<m; i++)
+        {
+            if(matrix[i][j] != 0){
+                matrix[i][j] = '*';
+            }
+        }
+        // return;
+    }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(matrix[i][j] == 0){
+                    mark_row(matrix,i);
+                    mark_col(matrix,j);
+                }
+            }
+        }
+
+        for(int i =0; i<m; i++)
+        {
+            for(int j =0; j<n; j++)
+            {
+                if(matrix[i][j] == '*'){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // return;
+    }
+};
+
+// // // class Solution{
+// // // public:
+// // //    void setZeroes(vector<vector<int>>& matrix){
+
+// // //        int m = matrix.size();
+// // //        int n = matrix[0].size();
+
+// // //        vector<int> row(m,0);
+// // //        vector<int> col(n,0);
+
+// // //        for(int i=0; i<m; i++){
+// // //            for(int j=0; j<n; j++){
+// // //                if(matrix[i][j] == 0){
+// // //                    row[i] = 1;
+// // //                    col[j] = 1;
+// // //                }
+// // //            }
+// // //        }
+
+// // //        for(int i =0; i<m; i++)
+// // //        {
+// // //            for(int j=0; j<n; j++)
+// // //            {
+// // //                if(row[i] == 1 || col[j] ==1){
+// // //                    matrix[i][j] = 0;
+// // //                }
+// // //            }
+// // //        }
+// // //    }
+// // // };
+
 // class Solution {
 // public:
-
-//     void mark_row(vector<vector<int>> & matrix, int i ){
-//         int m =matrix.size();
-//         int n =matrix[0].size();
-
-//         for(int j =0; j<n; j++){
-//             if(matrix[i][j] == 0){
-//                 matrix[i][j] = -1;
-//             }
-//         }
-//         // return;
-//     }
-
-//     void mark_col(vector<vector<int>> & matrix, int j){
-//         int m = matrix.size();
-//         int n = matrix[0].size();
-
-//         for(int i =0; i<m; i++)
-//         {
-//             if(matrix[i][j] == 0){
-//                 matrix[i][j] = -1;
-//             }
-//         }
-//         // return;
-//     }
-
 //     void setZeroes(vector<vector<int>>& matrix) {
 //         int m = matrix.size();
 //         int n = matrix[0].size();
 
-//         for(int i=0; i<m; i++)
-//         {
-//             for(int j=0; j<n; j++)
-//             {
-//                 if(matrix[i][j] == 0){
-//                     mark_row(matrix,i);
-//                     mark_col(matrix,j);
+//         // Mark rows and columns to be converted to zeros
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(matrix[i][j] == 0) {
+//                     // Mark row and column with -1
+//                     for(int k=0; k<m; k++) {
+//                         if(matrix[k][j] != 0) {
+//                             matrix[k][j] = -1;
+//                         }
+//                     }
+//                     for(int k=0; k<n; k++) {
+//                         if(matrix[i][k] != 0) {
+//                             matrix[i][k] = -1;
+//                         }
+//                     }
 //                 }
 //             }
 //         }
 
-//         for(int i =0; i<m; i++)
-//         {
-//             for(int j =0; j<n; j++)
-//             {
-//                 if(matrix[i][j] == -1){
+//         // Convert marked rows and columns to zeros
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(matrix[i][j] == -1) {
 //                     matrix[i][j] = 0;
 //                 }
 //             }
 //         }
-//         return;
 //     }
 // };
-
-class Solution{
-public:
-   void setZeroes(vector<vector<int>>& matrix){
-
-       int m = matrix.size();
-       int n = matrix[0].size();
-
-       vector<int> row(m,0);
-       vector<int> col(n,0);
-
-       for(int i=0; i<m; i++){
-           for(int j=0; j<n; j++){
-               if(matrix[i][j] == 0){
-                   row[i] = 1;
-                   col[j] = 1;
-               }
-           }
-       }
-
-       for(int i =0; i<m; i++)
-       {
-           for(int j=0; j<n; j++)
-           {
-               if(row[i] == 1 || col[j] ==1){
-                   matrix[i][j] = 0;
-               }
-           }
-       }
-
-
-   }
-};
