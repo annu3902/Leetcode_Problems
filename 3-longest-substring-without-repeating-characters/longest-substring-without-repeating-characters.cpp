ce94b0e2cad04bc596069e1110c5530fad774e71
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-01 00:30:29 +0530

    Time: 24 ms (53.15%) | Memory: 8.2 MB (85.10%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,55 +1,25 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         list<char> ls;
-//         int n =s.size();
-//         if(n == 0) return 0;
-//         if(n == 1) return 1;
-//         int i=0;
-//         int j=0;
-//         int maxi = 0;
-//         map<char,int> mpp;
-//         while(j < n && mpp[s[j]] <= 1){
-//             // ls.push_back(s[j]);
-//             mpp[s[j]]++;
-//             while(mpp.find(s[j]) != mpp.end()){
-//                 int len = j-i+1;
-//                 maxi = max(maxi, len);
-//                 ls.pop_front();
-//                 i++;
-//             }
-
-//             ls.push_back(s[j]);
-//             j++;
-//         }
-//         maxi = max(maxi, j-i+1);
-//     return maxi;
-//     }
-// };
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        int i = 0;
-        int j = 0;
-        int maxi = 0;
-        unordered_map<char, int> mpp;
-        
-        while (j < n) {
+        int i =0; int j =0;
+        int maxi =0;
+        unordered_map<char,int> mpp;
+        while(j < n){
             mpp[s[j]]++;
-            while (mpp[s[j]] > 1) {
-                int len = j - i + 1;
-                maxi = max(maxi, len);
+
+            while(mpp[s[j]] > 1){
+                int len = j-i;
+                maxi = max(maxi,len);
                 mpp[s[i]]--;
                 i++;
             }
-
+            cout<<maxi<<" ";
             j++;
         }
-        maxi = max(maxi, j - i + 1);
-        return maxi-1;
+        maxi = max(maxi, j-i);
+        cout<<endl;
+        cout<<maxi;
+        return maxi;
     }
 };
