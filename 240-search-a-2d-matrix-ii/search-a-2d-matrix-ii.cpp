777d0f8f2bdfd8d985a2613101f459052537522d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-02 22:38:06 +0530

    Time: 223 ms (18.56%) | Memory: 14.7 MB (97.59%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,46 +1,60 @@
-class Solution {
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         // This approach gives TLE 
+//         // int m = matrix.size();
+//         // int n = matrix[0].size();
+//         // int low = 0;
+//         // int high = m*n - 1;
+//         // int mid = low + (high - low)/2;
+//         // while(low <= high){
+//         //     int row = mid / n;
+//         //     int col = mid % n;
+
+//         //     if(matrix[row][col] == target ) return true;
+//         //     else if(matrix[row][col] > target){
+//         //         high = mid-1;
+//         //     }
+//         //     else{
+//         //         low = mid+1;
+//         //     }
+//         //     // mid = low + (high - low)/2;
+//         // }
+//         // return false;
+
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+
+//         int row = 0;
+//         int col = n-1;
+//         while(row < m && col>=0){
+//             if(matrix[row][col] == target){
+//                 return true;
+//             }
+
+//             else if(matrix[row][col] > target){
+//                 col--;
+//             }
+
+//             else if(matrix[row][col] < target){
+//                 row++;
+//             }
+//         }
+//         return false;
+
+//     }
+// };
+
+class Solution{
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // This approach gives TLE 
-        // int m = matrix.size();
-        // int n = matrix[0].size();
-        // int low = 0;
-        // int high = m*n - 1;
-        // int mid = low + (high - low)/2;
-        // while(low <= high){
-        //     int row = mid / n;
-        //     int col = mid % n;
-
-        //     if(matrix[row][col] == target ) return true;
-        //     else if(matrix[row][col] > target){
-        //         high = mid-1;
-        //     }
-        //     else{
-        //         low = mid+1;
-        //     }
-        //     // mid = low + (high - low)/2;
-        // }
-        // return false;
-
+    bool searchMatrix(vector<vector<int>> & matrix, int target){
         int m = matrix.size();
-        int n = matrix[0].size();
-
-        int row = 0;
-        int col = n-1;
-        while(row < m && col>=0){
-            if(matrix[row][col] == target){
+        for(int i=0; i<m; i++)
+        {
+            if(binary_search(matrix[i].begin(), matrix[i].end(), target)){
                 return true;
-            }
-
-            else if(matrix[row][col] > target){
-                col--;
-            }
-
-            else if(matrix[row][col] < target){
-                row++;
             }
         }
         return false;
-
     }
 };
