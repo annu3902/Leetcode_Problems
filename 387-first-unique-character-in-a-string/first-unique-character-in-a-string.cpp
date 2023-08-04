3566b558b4f01a23e260ccaaec7808d79cdd217d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-05 01:19:02 +0530

    Time: 30 ms (70.78%) | Memory: 10.8 MB (33.44%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,14 +1,33 @@
-class Solution {
-public:
-    int firstUniqChar(string s) {
-        vector<int> hashh(26);
-        for(int i =0; i<s.size(); i++)
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         vector<int> hashh(26);
+//         for(int i =0; i<s.size(); i++)
+//         {
+//             hashh[s[i] - 'a']++;
+//         }
+//         for(int i =0; i<s.size(); i++)
+//         {
+//             if(hashh[s[i] - 'a'] == 1){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+class Solution{
+    public:
+    int firstUniqChar(string s){
+        unordered_map<int,int>mpp;
+        int n =s.size();
+        for(int i=0; i<n; i++)
         {
-            hashh[s[i] - 'a']++;
+            mpp[s[i]]++;
         }
-        for(int i =0; i<s.size(); i++)
+        for(int i=0; i<s.size(); i++)
         {
-            if(hashh[s[i] - 'a'] == 1){
+            if(mpp[s[i]] == 1){
                 return i;
             }
         }
