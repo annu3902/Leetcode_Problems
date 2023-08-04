51920e7a3d32f58fd538000c3e2aa71cf4d8ecb6
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-04 22:21:04 +0530

    Time: 7 ms (80.95%) | Memory: 13.7 MB (10.00%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,13 +1,53 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        if(binary_search(nums.begin(),nums.end(),target)){
-            int start = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-            int end = upper_bound(nums.begin(),nums.end(),target)-nums.begin() -1;
-            return {start,end};
+
+int last (vector<int>& nums, int target){
+    int low = 0;
+    int high = nums.size()-1;
+    int ans = -1;
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(nums[mid] == target){
+            ans = mid;
+            low = mid+1;
+        }else if(nums[mid] < target){
+            low =mid+1;
+        }else{
+            high = mid-1;
         }
-        else{
-            return{-1,-1};
+    }
+    return ans;
+}
+
+int first (vector<int>& nums,int target){
+        int low =0;
+        int high = nums.size()-1;
+        int ans = -1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                high = mid-1;
+            }
+            else if(nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
         }
+        return ans;
+    }
+vector<int> searchRange(vector<int>& nums, int target) {
+        // if(binary_search(nums.begin(),nums.end(),target)){
+        //     int start = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        //     int end = upper_bound(nums.begin(),nums.end(),target)-nums.begin() -1;
+        //     return {start,end};
+        // }
+        // else{
+        //     return{-1,-1};
+        // }
+        return{first(nums,target),last(nums,target)};
+
     }
 };
