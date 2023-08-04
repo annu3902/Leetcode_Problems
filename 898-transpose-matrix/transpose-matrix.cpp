965cd1ea7465cdbfbbd5150533029806780f953d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-05 00:35:46 +0530

    Time: 3 ms (99.93%) | Memory: 10.6 MB (23.99%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.cpp
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         int m =matrix.size();
         int n =matrix[0].size();
-        vector<vector<int>> ans(n);
+        vector<vector<int>> ans(n, vector<int> (m));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 int row = j;
                 int col = i;
-                ans[row].push_back(matrix[i][j]);
+                ans[row][col] = (matrix[i][j]);
             }
         }
         return ans;
