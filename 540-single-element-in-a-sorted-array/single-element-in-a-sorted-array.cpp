8133a51fe8c60f76d35b9fce0487ce9e45b95863
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-06 01:16:15 +0530

    Time: 33 ms (31.56%) | Memory: 27.3 MB (5.30%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,12 +1,31 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int ans = 0;
+//         int n = nums.size();
+//         for(int i=0; i<n; i++)
+//         {
+//             ans ^= nums[i];
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
+    public:
+    int singleNonDuplicate(vector<int>& nums){
+        unordered_map<int,int> mpp;
         int n = nums.size();
         for(int i=0; i<n; i++)
         {
-            ans ^= nums[i];
+            mpp[nums[i]]++;
         }
-        return ans;
+        for(auto x: mpp)
+        {
+            if(x.second == 1){
+                return x.first;
+            }
+        }
+        return -1;
     }
 };
