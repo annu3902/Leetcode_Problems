ed25983215efacd309c45a591aebc424a014a421
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-05 10:50:02 +0530

    Time: 11 ms (73.31%) | Memory: 8.5 MB (25.63%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -24,26 +24,49 @@
 // };
 
 
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         string ans;
+//         map<char, int> mpp;
+//         for (char ch : s) {
+//             mpp[ch]++;
+//         }
+//         multimap<int, char, greater<int>> mp;
+//         for (auto entry : mpp) {
+//             mp.insert({entry.second, entry.first});
+//         }
+//         for (auto entry : mp) {
+//             int cnt = entry.first;
+//             char ch = entry.second;
+//             while (cnt > 0) {
+//                 ans.push_back(ch);
+//                 cnt--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+typedef pair<int,char> pi;
 class Solution {
 public:
     string frequencySort(string s) {
-        string ans;
-        map<char, int> mpp;
-        for (char ch : s) {
-            mpp[ch]++;
+        map<char,int> m;
+        for(char c:s)
+            m[c]++;
+        priority_queue<pi> pq;
+        for(auto it:m)
+            pq.push({it.second,it.first});
+        string s1="";
+        while(!pq.empty())
+        {
+            int x=pq.top().first;
+            char c=pq.top().second;
+            pq.pop();
+            while(x--)
+                s1.push_back(c);
         }
-        multimap<int, char, greater<int>> mp;
-        for (auto entry : mpp) {
-            mp.insert({entry.second, entry.first});
-        }
-        for (auto entry : mp) {
-            int cnt = entry.first;
-            char ch = entry.second;
-            while (cnt > 0) {
-                ans.push_back(ch);
-                cnt--;
-            }
-        }
-        return ans;
+        return s1;
     }
 };
