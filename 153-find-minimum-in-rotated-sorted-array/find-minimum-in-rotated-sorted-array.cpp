41fab64a63dce299c153b85923fd7d5b3920fe0d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-06 00:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (43.09%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,9 +1,33 @@
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        set<int> st(nums.begin(),nums.end());
-        // st.insert(nums.begin(), nums.end());
-        auto it = st.begin();
-        return *it;
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         set<int> st(nums.begin(),nums.end());
+//         // st.insert(nums.begin(), nums.end());
+//         auto it = st.begin();
+//         return *it;
+//     }
+// };
+
+class Solution{
+    public:
+    int findMin(vector<int>& nums){
+        int n =nums.size();
+        int low = 0;
+        int high = n-1;
+        int mini =INT_MAX;
+        while(low <= high){
+            int mid = (low+high)/2;
+            // Left Sorted
+            if(nums[low] <= nums[mid]){
+                mini = min(mini, nums[low]);
+                low = mid+1;
+            }
+
+            else{
+                mini = min(mini,nums[mid]);
+                high = mid -1;
+            }
+        }
+        return mini;
     }
 };
