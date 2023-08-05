e243e0ff5e8cc3f84b9af9c1dffccf5596233321
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-05 10:43:18 +0530

    Time: 8 ms (12.23%) | Memory: 11.1 MB (44.07%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(nums[i] == target){
-                return i;
+        int n = nums.size();
+        int low =0;
+        int high =n-1;
+        int mid;
+        while(low <= high){
+            int mid = (low+high)/2;
+            // If target is present at the middle index
+            if(nums[mid] == target){
+                cout<<mid<<endl;    
+                return mid;
+            }
+
+            // Check which half is sorted
+            //Left Sorted
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid-1;
+                    
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            // Right Sorted
+            else{
+                if(nums[mid] <= target &&target <= nums[high]){
+                    low = mid+1;
+                    cout<<mid<<" "<<low<<endl;
+                }
+                else{
+                    high = mid-1;
+                    cout<<mid<<" "<<high<<endl;
+                }
             }
         }
+        cout<<mid<<endl;
         return -1;
     }
 };
