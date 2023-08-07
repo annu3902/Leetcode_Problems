3a1914cddafe17568c735e9cde7efecfe0536a86
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-08 00:04:26 +0530

    Time: 3 ms (75.53%) | Memory: 10.2 MB (43.75%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -10,24 +10,47 @@
 
 class Solution{
     public:
+    int a;
+    int func (vector<int>& nums, int low, int high, int mini){
+        if(low > high){
+            // return;
+            // a = mini;
+            return mini;
+        }
+
+        int mid = low + (high-low)/2;
+        if(nums[low] <= nums[mid]){
+            mini = min(mini,nums[low]);
+           return func(nums,low+1,high,mini);
+        }
+        else{
+            mini = min(mini, nums[mid]);
+            high = mid -1;
+            return func(nums,low,mid-1,mini);
+        }
+        // return mini;
+    }
+
     int findMin(vector<int>& nums){
         int n =nums.size();
-        int low = 0;
-        int high = n-1;
-        int mini =INT_MAX;
-        while(low <= high){
-            int mid = (low+high)/2;
-            // Left Sorted
-            if(nums[low] <= nums[mid]){
-                mini = min(mini, nums[low]);
-                low = mid+1;
-            }
+        // int low = 0;
+        // int high = n-1;
+        // int mini =INT_MAX;
+        // while(low <= high){
+        //     int mid = (low+high)/2;
+        //     // Left Sorted
+        //     if(nums[low] <= nums[mid]){
+        //         mini = min(mini, nums[low]);
+        //         low = mid+1;
+        //     }
 
-            else{
-                mini = min(mini,nums[mid]);
-                high = mid -1;
-            }
-        }
-        return mini;
+        //     else{
+        //         mini = min(mini,nums[mid]);
+        //         high = mid -1;
+        //     }
+        // }
+        // return mini;
+        // int mn = func()
+        return func(nums,0,n-1,INT_MAX);
     }
 };
