2cccbe77a79981201c89a60a00904b7ad276593b
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-09 14:26:11 +0530

    Time: 3 ms (56.46%) | Memory: 6 MB (9.55%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -11,30 +11,24 @@ public:
         // }
         // return i-1;
 
-        if(x == 0 || x == 1){
-            return x;
-        }
-
-        else{
-            int low = 2;
-            int high = INT_MAX;
+        
+            double low = 1;
+            double high = x;
             long long int mid = -1;
-
+            if(x == 0) return 0;
+            double ans = INT_MIN;
             while(low <= high){
                 mid = low + (high-low)/2;
 
-                if(mid*mid < x){
+                if(mid*mid <= x){
+                    ans = mid;
                     low = mid+1;
                 }
-                else if(mid*mid == x){
-                    return mid;
-                }
-
                 else{
                     high = mid-1;
                 }
             }
-            return high;
-        }
+            cout<<ans<<endl;
+            return ans;
     }
 };
