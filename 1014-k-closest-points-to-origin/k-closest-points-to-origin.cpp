161cc5cf94da397dd76de99ab5fb01584159f7dd
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-15 02:26:10 +0530

    Time: 196 ms (84.72%) | Memory: 73.8 MB (35.62%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -7,14 +7,12 @@ public:
         //Creating a max heap
         priority_queue <pair <double, pair<int,int>>> pq;
         int n = points.size();
-        // int m =points[0].size();
 
         for(int i =0; i<n; i++)
         {
             double x = points[i][0];
             double y = points[i][1];
 
-            // double dist = sqrt(pow(x,2) + pow(y,2));
             pq.push({sqrt(pow(x,2) + pow(y,2)), {points[i][0], points[i][1]}});
         }
 
@@ -22,33 +20,16 @@ public:
         {
             pq.pop();
         }
-        // cout<<pq.size()<<endl;
-        // for(int i =0; i<pq.size(); i++){
-        //     for(int j =0; j<2; j++)
-        //     {
-        //         cout<<pq.top().second[j]<<" ";
-        //     }
-        //     pq.pop();
-        //     cout<<endl;
-        // }
+  
         for(int i = 0; i<k; i++)
         {
             vector<int> temp;
             temp.push_back(pq.top().second.first);
             temp.push_back(pq.top().second.second);
             ans.push_back(temp);
-            // ans.push_back(pq.top().second.first);
             pq.pop();
         }
 
-        // vector<int> v;
-        // for(int i =0 i<k; i++)
-        // {
-        //     for(int j =0; j<k; j++)
-        //     {
-        //         cout<<pq.t
-        //     }
-        // }
         return ans;
     }
 };
