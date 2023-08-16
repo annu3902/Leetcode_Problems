a3bec7088b12332bdcdd188d31dcba85c056d67d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-16 13:51:49 +0530

    Time: 359 ms (70.86%) | Memory: 106.5 MB (11.66%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,85 +1,35 @@
-// class Solution {
-// public:
-//     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-
-//         int n = items.size();
-//         int q = queries.size();
-
-
-//         map<int,vector<int>>m;
-//         for(int i=0;i<n;i++){
-//             m[items[i][0]].push_back(items[i][1]);
-//         }        
-
-
-//         vector<int> ans;
-//         int x = m.size();
-//         vector<int>dp(x+1,-1);
-//         int k = 0;
-//         for(int i=0;i<q;i++){
-//             int maxi = INT_MIN;
-//             k = 0;
-//             for(auto it:m){
-//                 if(dp[k]!=-1){
-//                     maxi = dp[k++];
-//                 }
-//                 else{
-//                     if(it.first<=queries[i]){
-//                         for(int j=0;j<it.second.size();j++){
-//                             maxi = max(maxi , it.second[j]);
-//                         }
-//                         dp[k++] = maxi;
-//                     }
-//                 }
-//             }
-//             if(maxi == INT_MIN) maxi = 0;
-//             ans.push_back(maxi);
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
-
-    static bool mycmp(vector<int> &a , vector<int> &b){
-        return a[0]<b[0];
-    }
-
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        sort(items.begin(), items.end());
 
-        sort(items.begin() , items.end() , mycmp);
+        int beauty = INT_MIN;
+        map<int,int> mpp;
 
-        map<int , int> mp;
-
-        int bt = INT_MIN;
-
-        for(auto x:items){
-            bt = max(bt , x[1]);
-            mp[x[0]] = bt;
+        for(auto x : items){
+             beauty = max(beauty , x[1]);
+             mpp[x[0]] = beauty;
         }
 
-        int n = queries.size();
-        vector<int> ans(n);
+        vector<int> ans;
 
-        for(int i = 0 ; i<n ; i++){
-            
-            if(mp.count(queries[i])){
-                ans[i] = mp[queries[i]];
-            }else{
-
-                auto x = mp.lower_bound(queries[i]);
-
-                if(x == mp.begin())
-                ans[i] =0;
-                else{
-                    x--;
-                    ans[i] = x->second;
+        for(auto x : queries){
+            if(mpp.find(x) != mpp.end()){
+                int bt = mpp[x];
+                ans.push_back(bt);
+            }
+            else{
+                // int y = mpp.lower_bound(x) - mpp.begin();
+                auto it = mpp.lower_bound(x);
+                if(it != mpp.begin()){
+                    it--;
+                    ans.push_back(it->second);
                 }
-            }   
+                else{
+                    ans.push_back(0);
+                }
+            }
         }
-
         return ans;
-
     }
 };
