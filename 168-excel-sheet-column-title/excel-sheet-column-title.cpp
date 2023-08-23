81d08c8404bb63a3e3bc71da0884329297b94893
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-08-23 21:00:25 +0530

    Time: 2 ms (50.09%) | Memory: 6.1 MB (11.51%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -1,59 +1,59 @@
-// class Solution {
-// public:
-//     string convertToTitle(int columnNumber) {
-//         vector<char> hashh(26);
-//         string ans;
-
-//         for(int i =0; i<26; i++){
-//             if(i == 0){
-//                 hashh[i] = 'Z';
-//             }
-//             else{
-//                 hashh[i] = i-1 + 'A';
-//             }
-//             // hashh[i] = 65 + i;
-//         }
-
-//         while(columnNumber != 0){
-//             int last_digit = columnNumber % 26;
-//             ans.push_back(hashh[last_digit]);
-//             columnNumber /= 26;
-//         }
-
-//         cout<<ans<<" ";
-//         reverse(ans.begin(), ans.end());
-//         cout<<ans<<" ";
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
         vector<char> hashh(26);
         string ans;
 
-        for (int i = 0; i < 26; i++) {
-            if (i == 0) {
-                hashh[i] = 'Z';
-            } else {
-                hashh[i] = i - 1 + 'A';
+        for(int i =0; i<26; i++){
+            if(i == 0){
+                hashh[0] = 'Z';
             }
+            else{
+                hashh[i] = i-1 + 'A';
+            }
+            // hashh[i] = 65 + i;
         }
 
-        while (columnNumber != 0) {
+        while(columnNumber != 0){
             int last_digit = columnNumber % 26;
-
-            if (last_digit == 0) {
-                ans.push_back('Z');  // Special case for remainder 0
-                columnNumber = (columnNumber / 26) - 1;
-            } else {
-                ans.push_back(hashh[last_digit]);
-                columnNumber /= 26;
-            }
+            ans.push_back(hashh[last_digit]);
+            columnNumber = (columnNumber - 1) /26;
         }
 
+        cout<<ans<<" ";
         reverse(ans.begin(), ans.end());
+        cout<<ans<<" ";
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     string convertToTitle(int columnNumber) {
+//         vector<char> hashh(26);
+//         string ans;
+
+//         for (int i = 0; i < 26; i++) {
+//             if (i == 0) {
+//                 hashh[i] = 'Z';
+//             } else {
+//                 hashh[i] = i - 1 + 'A';
+//             }
+//         }
+
+//         while (columnNumber != 0) {
+//             int last_digit = columnNumber % 26;
+
+//             if (last_digit == 0) {
+//                 ans.push_back('Z');  // Special case for remainder 0
+//                 columnNumber = (columnNumber / 26) - 1;
+//             } else {
+//                 ans.push_back(hashh[last_digit]);
+//                 columnNumber /= 26;
+//             }
+//         }
+
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
