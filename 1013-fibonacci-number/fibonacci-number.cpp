ccb241859810b057c0001f7eb148dc783c829dcd
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-01 19:23:19 +0530

    Time: 2675 ms (7.99%) | Memory: 771.1 MB (8.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -5,8 +5,10 @@ public:
         if(n == 0) return 0;
         if(n == 1) return 1;
 
+        vector<int> dp(n+1,-1);
+
         int partialans_1 = fib(n-1);
         int partialans_2 = fib(n-2);
-        return (partialans_1 + partialans_2);
+        return dp[n] = (partialans_1 + partialans_2);
     }
 };
