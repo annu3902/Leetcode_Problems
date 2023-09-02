b88ab72c30262179d8ebfb86b1369c0949ed6549
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-02 08:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (59.77%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n == 0) return 1;
+        int temp = n;
+        n = abs(n);
+
+        double partialans = myPow(x, n/2);
+        double result = (temp >= 0) ? (partialans * partialans) : (1/partialans * 1/partialans);
+
+        if(n % 2 == 1) {
+            // double check = (result >= 1 ) ? (result*x) : (1/x * result);
+            if(temp < 0){
+                return (1/x * result);
+            }else{
+                return (x * result);
+            }
+            // return check;
+        }
+
+        return result;
     }
 };
