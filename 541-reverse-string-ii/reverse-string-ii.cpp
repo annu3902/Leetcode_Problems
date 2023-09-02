ca60adb4ee8eb31d8d11d4894eec8b585de233b7
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-02 11:22:31 +0530

    Time: 7 ms (24.70%) | Memory: 7.2 MB (94.58%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -1,59 +1,59 @@
-// class Solution {
-// public:
-//     void reverseHelper(int left, int right, string &s){
-//         if(left >= right) return;
-        
-//         swap(s[left], s[right]);
-//         reverseHelper(left+1, right-1, s);
-//     }
-
-//     string reverseStr(string s, int k) {
-//         int n = s.size();
-//         if(n < k){
-//             reverse(s.begin(), s.end());
-//             return s;
-//         }
-
-//        int x = 2*k;
-//        int total_grps = ceil(n/x);
-//        int left = 0;
-//        int right = k-1;
-
-//         for(int i =0; i<=total_grps; i++){
-//             reverseHelper(left, right, s);
-//             left = left + 2*k;
-//             right = right + 2*k;
-//         }
-
-//         return s;
-//     }
-// };
-
 class Solution {
 public:
-    void reverseHelper(int left, int right, string &s) {
-        if (left >= right) return;
-
+    void reverseHelper(int left, int right, string &s){
+        if(left >= right) return;
+        
         swap(s[left], s[right]);
-        reverseHelper(left + 1, right - 1, s);
+        reverseHelper(left+1, right-1, s);
     }
 
     string reverseStr(string s, int k) {
         int n = s.size();
-        if (n < k) {
+        if(n < k){
             reverse(s.begin(), s.end());
             return s;
         }
 
-        int x = 2 * k;
-        int total_grps = n / x;
-        
-        for (int i = 0; i <= total_grps; i++) {
-            int left = i * x;
-            int right = min(left + k - 1, n - 1);
+       int x = 2*k;
+       int total_grps = ceil(n/x);
+       int left = 0;
+       int right = k-1;
+
+        for(int i =0; i<=total_grps; i++){
             reverseHelper(left, right, s);
+            left = left + 2*k;
+            right = min(left + k -1, n-1);
         }
 
         return s;
     }
 };
+
+// class Solution {
+// public:
+//     void reverseHelper(int left, int right, string &s) {
+//         if (left >= right) return;
+
+//         swap(s[left], s[right]);
+//         reverseHelper(left + 1, right - 1, s);
+//     }
+
+//     string reverseStr(string s, int k) {
+//         int n = s.size();
+//         if (n < k) {
+//             reverse(s.begin(), s.end());
+//             return s;
+//         }
+
+//         int x = 2 * k;
+//         int total_grps = n / x;
+        
+//         for (int i = 0; i <= total_grps; i++) {
+//             int left = i * x;
+//             int right = min(left + k - 1, n - 1);
+//             reverseHelper(left, right, s);
+//         }
+
+//         return s;
+//     }
+// };
