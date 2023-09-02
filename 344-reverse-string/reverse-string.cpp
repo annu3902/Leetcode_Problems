7d6067a191ca3022d69408c82060818387bca5f5
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-02 10:33:51 +0530

    Time: 20 ms (53.68%) | Memory: 23.3 MB (31.62%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,12 +4,12 @@ public:
         // Writting Base Condition 
         if(left >= right) return;
 
-        // Call recursion to do later 
-        reverseStringHelper(left+1, right-1, s);
-        
         // Do small task - 2
         swap(s[left], s[right]);
 
+        // Call recursion to do later 
+        reverseStringHelper(left+1, right-1, s);
+        
 
     }
 
