9f8afd70ef0b9d7f24fdbfdb6d7dcdbaa3edaa13
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-05 07:58:22 +0530

    Time: 5 ms (37.69%) | Memory: 9.9 MB (29.46%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    vector<vector<int>> helper(vector<int>& nums, int i, set <vector<int>> st){
+        vector<vector<int>> finalans;
+
+        // Writting the Base Condition - 1
+        if(i == nums.size()){
+            return {{}};
+        }
+
+        // Ask recursion to produce the powerset for remaining (i+1) elements - 2
+        
+        vector<vector<int>> partialans  = helper(nums, i+1, st); 
+        for(vector<int> x : partialans){
+            sort(x.begin(), x.end());
+            if(st.find(x) == st.end()){
+                st.insert(x);
+                finalans.push_back(x);
+            }
+        }
+        // Do your small task - 3
+        for(vector<int> x : partialans){
+            x.push_back(nums[i]);
+            sort(x.begin(), x.end());
+            if(st.find(x) == st.end()){
+                st.insert(x);
+                finalans.push_back(x);
+            }
+        }
+
+        return finalans;
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> st;
+        return helper(nums, 0, st);
+    }
+};
