66362ba276b389070ae0dfd8b2a279f73de48b30
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-05 07:16:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (47.86%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    vector<vector<int>> helper(vector<int>& nums, int i){
+        vector<vector<int>> finalans;
+
+        // Writting the Base Condition
+        if(i == nums.size()){
+            return {{}};
+        }
+
+        // Ask recursion to generrate the powerset for remaining (i+1)elements
+        vector<vector<int>> partialans = helper(nums, i+1);
+        for(vector<int> x : partialans){
+            finalans.push_back(x);
+        }
+
+        // Do my small task
+        for(vector<int> x : partialans){
+            x.push_back(nums[i]);
+            finalans.push_back(x);
+        }
+        return finalans;
+
+    }   
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        return helper(nums, 0);
+    }
+};
