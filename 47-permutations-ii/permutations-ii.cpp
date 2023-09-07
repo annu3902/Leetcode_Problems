82dff07c4b9c429e90ef06f1ef9cc41341d7d9d7
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-08 02:04:26 +0530

    Time: 762 ms (5.03%) | Memory: 27.8 MB (15.44%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,39 +1,52 @@
+// class Solution {
+// public:
+//     void recursion(vector<int> num, int i, int n, vector<vector<int> > &result) {
+//         if(i == n-1){
+//             result.push_back(num);
+//             return;
+//         }
+
+//         for(int k = i; k < n; k++){
+//             if(k != i && num[k] == num[i]) continue;
+
+//             swap(num[k], num[i]);
+//             recursion(num, i+1, n, result);
+//         }
+//     }
+//     vector<vector<int> > permuteUnique(vector<int> &num) {
+//         sort(num.begin(), num.end());
+
+//         int n = num.size();
+
+//         vector<vector<int>> result;
+//         recursion(num, 0, n, result);
+        
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
-    void permutation_generator(vector<int>& nums, int pos, vector<vector<int>>& ans){
-        // Base Condition -1
-        if(pos == nums.size()){
-            // if(st.find(nums) == st.end()){
-                ans.push_back(nums);
-            // }
+    void permutation_generator(vector<int> nums, int pos, vector<vector<int>>& ans){
+
+        if(pos == nums.size()-1){
+            vector<vector<int>> :: iterator it;
+            it = find(ans.begin(),ans.end(),nums);
+            if(it==ans.end())   ans.push_back(nums);
             return;
         }
 
-        unordered_set<int> st;
         for(int i=pos; i<nums.size(); i++){
-
-            // if(i != pos && (nums[i] == nums[i-1] || nums[i] == nums[pos])){
-            //     continue;
-            // }
-
-            // else if(i != pos && nums[i] == nums[pos]){
-            //     continue;
-            // }
-
-            if(st.count(nums[i])) continue;
-
-            st.insert(nums[i]);
             swap(nums[i], nums[pos]);
             permutation_generator(nums, pos+1, ans);
-            // Backtracking   
-            swap(nums[i], nums[pos]);
+            // swap(nums)
         }
 
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
-        set<vector<int>> st;
 
         sort(nums.begin(), nums.end());
         permutation_generator(nums, 0, ans);
