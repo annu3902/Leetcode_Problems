a59f0139bb28cfb44618a4092b922bebc6046bb8
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-09-10 16:38:40 +0530

    Time: 10 ms (12.08%) | Memory: 9.9 MB (29.68%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
 
-    vector<vector<int>> helper(vector<int>& nums, int i, set <vector<int>> st){
-        vector<vector<int>> finalans;
-        sort(nums.begin(), nums.end());
+    vector<vector<int>> helper(vector<int>& nums, int pos, set<vector<int>> st){
 
-        // Writting the Base Condition - 1
-        if(i == nums.size()){
+        // 
+        if(pos == nums.size()){
             return {{}};
         }
 
-        // Ask recursion to produce the powerset for remaining (i+1) elements - 2
-        
-        vector<vector<int>> partialans  = helper(nums, i+1, st); 
-        for(vector<int> x : partialans){
-            // sort(x.begin(), x.end());
-            if(st.find(x) == st.end()){
-                st.insert(x);
-                finalans.push_back(x);
-            }
-        }
-        // Do your small task - 3
-        for(vector<int> x : partialans){
-            x.push_back(nums[i]);
-            // sort(x.begin(), x.end());
-            if(st.find(x) == st.end()){
-                st.insert(x);
-                finalans.push_back(x);
-            }
-        }
 
-        return finalans;
+        // calling recursion to produce the powerset for remaining
+       vector<vector<int>> partialans = helper(nums, pos+1,st);
+       vector<vector<int>> finalans;
+
+        // Saving the ans 
+       for(vector<int> x : partialans){
+           if(st.find(x) == st.end()){
+               st.insert(x);
+               finalans.push_back(x);
+           }
+       }    
+
+        // Doing my own task
+       for(vector<int> x: partialans){
+           x.push_back(nums[pos]);
+           sort(x.begin(), x.end());
+
+           if(st.find(x) == st.end()){
+               st.insert(x);
+               finalans.push_back(x);
+           }
+       }
+       return finalans;
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+
         set<vector<int>> st;
         return helper(nums, 0, st);
+
     }
 };
