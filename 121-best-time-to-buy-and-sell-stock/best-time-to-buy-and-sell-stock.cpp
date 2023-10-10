d7eefa3179350b50b6bfb9c50b503a7796b8b53d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-10-10 18:05:27 +0530

    Time: 103 ms (24.44%) | Memory: 93.6 MB (38.57%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,56 +1,19 @@
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int mini = prices[0];
-//         int profit =0;
-//         for(int i=1; i<prices.size(); i++)
-//         {
-//             mini = min(mini, prices[i-1]);
-//             profit = max(profit, prices[i] - mini);
-//         }
-//         return profit;
-//     }
-// };
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // int n = prices.size();
-        // int maxi = 0;
-        // for(int i =0; i<n; i++)
-        // {
-        //     for(int j=i+1; j<n; j++){
-        //         if(prices[j] > prices[i]){
-        //             int diff = prices[j] - prices[i];
-        //             maxi = max(maxi, diff);
-        //         }
-        //     }
-        // }
-        // return maxi;
+        int mini = prices[0];
+        int profit = 0;
 
-        // int n =prices.size();
-        // int maxi =0;
-        // int mini = prices[0];
-        // for(int i=1; i<n; i++)
-        // {
-        //     mini = min(mini, prices[i]);
-        //     int diff = prices[i] - mini;
-        //     maxi = max(maxi, diff);
-        // }
-        // return maxi;
-
-        int n =prices.size();
-        int left = 0;
-        int max_profit = 0;
-
-        for(int right = 1; right<n; right++){
-            if(prices[right] < prices[left]){
-                left = right;
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i] < mini){
+                mini = prices[i];
             }
 
-            int diff = prices[right] - prices[left];
-            max_profit = max(max_profit, diff);
+            else if(prices[i] > mini){
+                int diff = prices[i] - mini;
+                profit = max(profit , diff);
+            }
         }
-        return max_profit;
+        return profit;
     }
 };
