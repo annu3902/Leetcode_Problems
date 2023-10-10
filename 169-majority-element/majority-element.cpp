25e0c7420b54768de4929f11c5c6e58cc6b2b05f
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-10-10 19:59:58 +0530

    Time: 12 ms (71.18%) | Memory: 19.9 MB (24.70%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,44 +1,82 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int n =nums.size();
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n =nums.size();
 
-        //*********** MOORE'S VOTING ALGORITHM **********//
+//         //*********** MOORE'S VOTING ALGORITHM **********//
 
-        int element = nums[0];
-        int cnt =1;
-        int i =1;
+//         int element = nums[0];
+//         int cnt =1;
+//         int i =1;
         
-        while(i < n){
+//         while(i < n){
+//             if(cnt > 0){
+//                 if(nums[i] == element){
+//                     cnt++;
+//                 }else{
+//                     cnt--;
+//                 }
+//             }
+
+//             else{
+//                 element = nums[i];
+//                 cnt++;
+//             }
+//             i++;
+//         }
+
+//         if(cnt > 0)
+//         {
+//             int count =0;
+//             for(int i=0; i<n; i++)
+//             {
+//                 if(nums[i] == element){
+//                     count ++;
+//                 }
+//             }
+
+//             if(count > n/2){
+//                 return element;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+class Solution{
+    public :
+    int majorityElement(vector<int>& nums){
+        int n = nums.size();
+
+        int cnt = 1;
+        int ele = nums[0];
+
+        for(int i = 1; i<n; i++){
             if(cnt > 0){
-                if(nums[i] == element){
+                if(nums[i] == ele)
                     cnt++;
-                }else{
+                else{
                     cnt--;
                 }
             }
-
             else{
-                element = nums[i];
+                ele = nums[i];
                 cnt++;
             }
-            i++;
         }
 
-        if(cnt > 0)
-        {
-            int count =0;
-            for(int i=0; i<n; i++)
-            {
-                if(nums[i] == element){
+        if(cnt > 0){
+            int count = 0;
+            for(int i =0; i<nums.size(); i++){
+                if(nums[i] == ele){
                     count ++;
                 }
             }
-
             if(count > n/2){
-                return element;
+                return ele;
             }
         }
         return -1;
     }
+
 };
