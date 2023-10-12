c9074bf61e48ae15deeb7f0ae25c74764296dbea
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-10-12 19:18:02 +0530

    Time: 4 ms (53.61%) | Memory: 9.2 MB (30.81%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
+vector<vector<int>> helper(vector<int> &nums, int pos, set<vector<int>> &st){
+        // Base Case
+        if(pos == nums.size()){
+            return {{}};
+        }
 
-void helper(vector<int>& arr, int pos, vector<int>& subset, vector<vector<int>> &ans){
-    if(pos == arr.size()){
-        ans.push_back(subset);
-        return;
+        // Call the recursion to form all the subset for remaining elements
+        vector<vector<int>> partialAns = helper(nums, pos + 1, st);
+        vector<vector<int>> finalAns;
+        // unordered_set<vector<int>> st; 
+        finalAns = partialAns;
+
+        // Doing my small job to add the pos element in the subset
+        for(vector<int> x : partialAns){
+            x.push_back(nums[pos]);
+            if(st.count(x) == 0){
+                st.insert(x);
+                finalAns.push_back(x);
+            }
+        }
+        return finalAns;
     }
 
-    // Doing my small task first to include or exclude the element
-
-        // Include the element
-        subset.push_back(arr[pos]);
-        helper(arr, pos+1, subset, ans);
-
-        // Exclude the element
-        subset.pop_back();
-        // Surpass the element till the new element is equal to the previous one
-        while(pos+1 < arr.size() && arr[pos] == arr[pos+1]){
-            pos++;
-        }
-        helper(arr, pos+1, subset, ans);
-}
-    vector<vector<int>> subsetsWithDup(vector<int>& arr) {
-    vector<int> subset;
-    vector<vector<int>> ans;
-    sort(arr.begin(), arr.end());
-
-    helper(arr, 0, subset, ans);
-    sort(ans.begin(), ans.end());
-    return ans;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        set<vector<int>> st;
+        return helper(nums, 0, st);
     }
 };
-
