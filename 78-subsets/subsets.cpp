89fcc51a4cb476a5e954df8017abb5498921247a
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-10-12 18:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (47.06%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
+    vector<vector<int>> helper(vector<int> &nums, int pos){
 
-    vector<vector<int>> helper(vector<int>& nums, int i){
-        vector<vector<int>> finalans;
-
-        // Writting the Base Condition
-        if(i == nums.size()){
+        if(pos == nums.size()){
             return {{}};
         }
 
-        // Ask recursion to generrate the powerset for remaining (i+1)elements
-        vector<vector<int>> partialans = helper(nums, i+1);
-        for(vector<int> x : partialans){
-            // sort(x.begin(), x.end());
-            finalans.push_back(x);
-        }
+        // Call Recursion to form the subsets of the rest
+        vector<vector<int>> partialAns = helper(nums, pos + 1);
+        vector<vector<int>> finalAns;
 
-        // Do my small task
-        for(vector<int> x : partialans){
-            x.push_back(nums[i]);
-            // sort(x.begin(), x.end());
-            finalans.push_back(x);
-        }
-        return finalans;
+        finalAns = partialAns;
 
-    }   
+        // Doing my small task of forming the subset for the pos element
+        for(vector<int> x: partialAns){
+            x.push_back(nums[pos]);
+            finalAns.push_back(x);
+        }
+        return finalAns;
+
+    }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         return helper(nums, 0);
