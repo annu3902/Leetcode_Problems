387f7a0232da2d0c44b4230b4f79301d7166ebf8
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-11-25 19:10:45 +0530

    Time: 3 ms (84.23%) | Memory: 10.5 MB (55.22%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        sort(nums2.begin(), nums2.end());
-        sort(nums1.begin(), nums1.end());
+        // vector<int> ans;
+        // sort(nums2.begin(), nums2.end());
+        // sort(nums1.begin(), nums1.end());
 
-        int n = nums1.size();
-        int m = nums2.size();
-        int i =0;
+        // int n = nums1.size();
+        // int m = nums2.size();
+        // int i =0;
         // int j =0;
 
         // while(i<n && j<m){
@@ -22,22 +22,40 @@ public:
         //     }
         // }
         // return ans;
-        vector<int> taken(n,0);
-        for(int j =0; j<m; j++){
-            for(int i =0; i<n; i++){
-                if(nums2[j] == nums1[i] && taken[i] == 0){
-                    ans.push_back(nums2[j]);
-                }
-                if(nums1[i] > nums2[j]) break;
-            }
-        }
+    //////////////////////////////////////////////////////////////////////////
+        // My Approach 2
+    //     vector<int> taken(n,0);
+    //     for(int j =0; j<m; j++){
+    //         for(int i =0; i<n; i++){
+    //             if(nums2[j] == nums1[i] && taken[i] == 0){
+    //                 ans.push_back(nums2[j]);
+    //             }
+    //             if(nums1[i] > nums2[j]) break;
+    //         }
+    //     }
 
-        unordered_set<int> st;
-        st.insert(ans.begin(), ans.end());
-        vector<int> ans2;
-        for(auto it : st){
-            ans2.push_back(it);
+    //     unordered_set<int> st;
+    //     st.insert(ans.begin(), ans.end());
+    //     vector<int> ans2;
+    //     for(auto it : st){
+    //         ans2.push_back(it);
+    //     }
+    //     return ans2;
+    // }
+
+    vector<int> hash(1000, 0);
+    vector<int> ans;
+
+    for(int i=0; i<nums1.size(); i++){
+        if(hash[nums1[i]] == 0) hash[nums1[i]]++;
+    }
+
+    for(int i=0; i<nums2.size(); i++){
+        if(hash[nums2[i]] == 1){
+            ans.push_back(nums2[i]);
+            hash[nums2[i]]--;
         }
-        return ans2;
+    }
+    return ans;
     }
 };
