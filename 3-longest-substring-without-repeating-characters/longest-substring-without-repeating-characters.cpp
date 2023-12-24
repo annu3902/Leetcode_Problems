d9ce8180fa128814a18dd23f23611f7f18dbb868
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2023-12-24 18:29:46 +0530

    Time: 39 ms (12.60%) | Memory: 14.1 MB (9.25%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,80 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+
+//         int n =s.size();
+//         int i =0;
+//         int j =0;
+//         int max_length = 0;
+//         set<char> st;
+
+//         for(j =0; j<n; j++){
+//             if(st.find(s[j]) == st.end()){
+//                 st.insert(s[j]);
+//             }
+
+//             else{
+//                 max_length = max(max_length, j-i);
+
+//                 while(st.find(s[j]) != st.end()){
+//                     st.erase(s[i]);
+//                     i++;
+//                 }
+//                 st.insert(s[j]);
+//             }
+//         }
+//         max_length = max(max_length, j-i);
+//         return max_length;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // map<char, int> mpp;
+        // int maxi = 0;
+        // int length = 0;
+        // int j = 0;
 
-        int n =s.size();
-        int i =0;
-        int j =0;
-        int max_length = 0;
-        set<char> st;
+        // for (int i = 0; i < s.size(); i++) {
+        //     if (mpp.find(s[i]) == mpp.end()) {
+        //         mpp[s[i]]++;
+        //         length++;
+        //     } 
+        //     else {
+        //         maxi = max(maxi, length);
+        //         while (mpp[s[i]] != 0) {
+        //             mpp.erase(s[j]);
+        //             j++;
+        //             length--;
+        //         }
+        //         // cout<<length<<endl;
+        //         mpp[s[i]]++;
+        //     }
+        // }
+        // cout << length;
+        // maxi = max(maxi, length);
+        // return maxi;
+        map<char,int> mpp;
+        int i=0;
+        int j=0;
+        int maxi = 0;
 
-        for(j =0; j<n; j++){
-            if(st.find(s[j]) == st.end()){
-                st.insert(s[j]);
+        for(; i<s.size(); i++){
+            if(mpp.find(s[i]) == mpp.end()){
+                mpp[s[i]]++;
             }
-            
             else{
-                max_length = max(max_length, j-i);
-
-                while(st.find(s[j]) != st.end()){
-                    st.erase(s[i]);
-                    i++;
+                maxi = max(maxi, i-j);
+                while(mpp.count(s[i])){
+                    mpp.erase(s[j]);
+                    j++;
                 }
-                st.insert(s[j]);
+                mpp[s[i]]++;
             }
         }
-        max_length = max(max_length, j-i);
-        return max_length;
+        cout<<i<<" "<<j<<endl;
+        maxi = max(maxi, i - j);
+        return maxi;
     }
 };
