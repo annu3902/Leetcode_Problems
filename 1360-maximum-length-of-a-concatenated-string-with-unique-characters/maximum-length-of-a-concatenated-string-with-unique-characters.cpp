3deb192023c579dd7958f1ba5294ecd3115d2439
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-01-24 17:36:21 +0530

    Time: 485 ms (13.46%) | Memory: 235.4 MB (10.75%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -17,8 +17,32 @@ public:
         return maxi;
     }
 
+    // TIME COMPLEXITY = 2^N * N * W; 
+            // W -> Length of each word
+            // N -> Size of the arr
+    // Space Complexity = 2*(N * W)
+
+    // Backtracking
+    int solve2(vector<string> arr, int i, string &temp){
+        unordered_set<char> st(temp.begin(), temp.end());
+        if(temp.size() != st.size()) return 0;
+        // Base Case
+        if(i >= arr.size()) return temp.size();
+
+        // Pick
+        temp += arr[i];
+        int len1 = solve(arr, i + 1, temp);
+        
+        // Not Pick
+        for(char ch : arr[i]){
+            temp.pop_back();
+        }
+        int len2 = solve(arr, i + 1, temp);
+
+        return max({len1, len2});
+    }
     int maxLength(vector<string>& arr) {
         string temp = "";
-        return solve(arr, 0, temp);
+        return solve2(arr, 0, temp);
     }
 };
