8c224059cb8116c9bf4cb02dfa4b609d9ece84bd
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-01-27 17:26:58 +0530

    Time: 59 ms (14.56%) | Memory: 88.2 MB (5.43%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,87 +1,39 @@
-// class Solution {
-// public:
-//     int threeSumClosest(vector<int>& nums, int target) {
-//         int n =nums.size();
-//         int sum = 0;
-//         int mini_dist = INT_MAX;
-//         int ans = 0;
-        // for(int i =0; i<=n-3; i++){
-        //     // same set of i;
-        //     if(i > 0 && nums[i] == nums[i-1]){
-        //         continue;
-        //     }
-        //     sum = nums[i] + nums[n-1];
-        //     for(int j =i+1; j<=n-2; j++){
-        //         if(j > i+1 && nums[j] == nums[j-1]){
-        //             continue;
-        //         }
-        //         else if(j==i+1){
-        //             sum += nums[j];
-        //         }
-        //         else{
-        //             int diff = nums[j] - nums[j-1];
-        //             sum += diff;
-        //         }
-        //         int gap = abs(target - sum);
-        //         if(gap > mini_dist){
-        //             break;
-        //         }
-        //         else if(gap < mini_dist){
-        //             // Ans Case
-        //             mini_dist = gap;
-        //             ans = sum;
-        //         }
-        //         else if(gap == mini_dist && sum == target){
-        //             mini_dist = gap;
-        //             ans = sum;
-        //         }
-        //     }
-        // }
-        // return ans;
-        
-//     }
-// };
-
-
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-
-        int n =nums.size();
-        int mini =INT_MAX;
-        int ans = 0;
-
+        int n = nums.size();
         sort(nums.begin(), nums.end());
+        vector<pair<int, int>> arr;
 
-        for(int i =0; i<n; i++){
+        for(int i = 0; i < n; i++){
+            int left = i + 1; int right = n - 1;
 
-            // Optimization for not checking for duplicates
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-
-            int j = i+1;
-            int k = n-1;
-
-            while(j < k){
-
-                int sum = nums[i] + nums[j] + nums[k];
-                int diff = abs(target - sum);
-
-                if(diff < mini){
-                    mini = diff;
-                    ans = sum;
-                }
-
-                if(sum < target){
-                    j++;
-                }
-                
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == target) return target;
+                else if(sum < target){
+                    int temp = abs(sum - target);
+                    arr.push_back({sum, temp});
+                    left++;
+                }   
                 else{
-                    k--;
+                    int temp = abs(sum - target);
+                    arr.push_back({sum, temp});
+                    right--;
                 }
             }
         }
-        return ans;
+
+        int comp = INT_MAX;
+        int res;
+        for(pair<int, int> p : arr){
+            int firstEle = p.first;
+            int secondEle = p.second;
+            if(secondEle < comp){
+                comp = secondEle;
+                res = firstEle;
+            }
+        }
+        return res;
     }
 };
