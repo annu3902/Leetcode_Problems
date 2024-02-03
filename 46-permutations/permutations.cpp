6f305be57b96694b1f98b1ba280ba850c0d1df81
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-03 11:28:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (30.72%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, int ind, vector<vector<int>>& ans){
-        int n = nums.size();
-
-        // Base Condition -1)
-        if(ind == nums.size()){
+    void solve(vector<int> &nums, int pos, vector<vector<int>> &ans){
+        int endPos = nums.size();
+        // Base Case
+        if(pos == endPos) {
             ans.push_back(nums);
             return;
-        } 
+        }
 
-        // To do small task first -2)
-        for(int i=ind; i<n; i++){
-            swap(nums[i], nums[ind]);
-            // Call recursion to do the remaining task
-            solve(nums, ind+1, ans);
-
-            // Backtracking
-            swap(nums[i], nums[ind]);
+        // Processing
+        for(int i = pos; i < endPos; i++){
+            // Bring every element from position to the frist place
+            swap(nums[pos], nums[i]);
+            solve(nums, pos + 1, ans);
+            swap(nums[pos], nums[i]);  // Backtracking
         }
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
-        solve(nums, 0, ans);
-        return ans;
+       sort(nums.begin(), nums.end()); // --> To return the answer in sorted order
+       vector<vector<int>> ans;
+       solve(nums, 0, ans); 
+       return ans; 
     }
 };
