ef217c6e074042e704ae3eee52e9841a8528d1ac
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-07 01:02:07 +0530

    Time: 87 ms (5.05%) | Memory: 12.6 MB (31.03%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,82 +1,44 @@
 class Solution {
+private:
+    void solve(int ind, vector<int>& candidates, vector<int>& temp,
+               vector<vector<int>>& ans, int sum, int target){
+                   // Base Case;
+                   if(sum > target) {
+                       for(int x : temp){
+                           cout<<x<<" ";
+                       }
+                       cout<<endl;
+                       return;
+                   }
+                   if(sum == target){
+                       for(int x : temp){
+                           cout<<x<<" ";
+                       }
+                       cout<<endl;
+                        ans.push_back(temp);
+                       return;
+                   }
+
+                   // Processing
+                    for(int i = ind; i < candidates.size(); i++){
+                        // while(i < candidates.size() && i > ind && candidates[i] == candidates[i-1]){
+                        //     i++;
+                        // }
+                        if(i != ind && candidates[i] == candidates[i-1]) continue;
+                        temp.push_back(candidates[i]);
+                        sum += candidates[i];
+                        solve(i+1, candidates, temp, ans, sum, target);
+                        sum -= candidates[i];
+                        temp.pop_back();
+                    }
+               } 
 public:
-void helper(vector<int> &nums, int target, int ind, vector<int> &subset, vector<vector<int>> &ans){
-    if(target == 0){
-        // vector<vector<int>> :: iterator it;
-        // it = find(ans.begin(), ans.end(), subset);
-        // if(it == ans.end())
-        ans.push_back(subset);
-        return;
-    }
-
-    if(ind >= nums.size() || target< nums[ind]) {
-        return;
-    }
-
-    // for(int i = ind; i<nums.size();i++){
-    //     if(i !=ind && nums[i] == nums[i-1]) continue;
-    //     subset.push_back(nums[i]);
-    //     helper(nums, target - nums[i], i + 1, subset, ans);
-    //     subset.pop_back();
-    // }
-
-    subset.push_back(nums[ind]);
-    helper(nums, target - nums[ind], ind + 1, subset, ans);
-
-    subset.pop_back();
-    while(ind + 1 < nums.size() && nums[ind] == nums[ind + 1]){
-        ind++;
-    }
-    helper(nums, target, ind+1, subset, ans);
-
-}
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<int> subset;
+    vector<vector<int>> combinationSum2(vector<int>& candidates,
+                                              int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> temp;
         vector<vector<int>> ans;
-        helper(nums, target, 0, subset, ans);
+        solve(0, candidates, temp, ans, 0, target);
         return ans;
     }
 };
-
-// class Solution {
-// public:
-// void helper(vector<int> &nums, int target, int ind, vector<int> &subset, vector<vector<int>> &ans){
-//     if(target == 0){
-//         ans.push_back(subset);
-//         return;
-//     }
-
-//     if(ind >= nums.size() || target< nums[ind]) {
-//         return;
-//     }
-
-//     // subset.push_back(nums[ind]);
-//     for(int i = ind; i<nums.size();i++){
-//         if(i !=ind && nums[i] == nums[i-1]) continue;
-//         subset.push_back(nums[i]);
-//         helper(nums, target - nums[i], i + 1, subset, ans);
-//         subset.pop_back();
-//     }
-// }
-//     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-//         sort(nums.begin(), nums.end());
-//         vector<int> subset;
-//         vector<vector<int>> ans;
-//         helper(nums, target, 0, subset, ans);
-//         return ans;
-//     }
-// };
-
-// class Solution{
-//     vector<vector<int>> combinationSum2(vector<int>& nums, int target){
-//         sort(nums.begin(), nums.end());
-
-//         for(int i = nums.size() -1; i >= 0;i--){
-//             if(i != nums.size() -1 && nums[i] == nums[i-1]) continue;
-//             subset.push_back(nums[i]);
-//             helper(nums, target - nums[i], i + 1, subset, ans);
-//             subset.pop_back();
-//         }
-//     }
-// };
