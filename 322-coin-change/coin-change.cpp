3d8abf4c2e6130e2a38fa7641a8343604e3d416b
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-10 23:07:55 +0530

    Time: 52 ms (65.14%) | Memory: 47.8 MB (17.15%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,46 +1,47 @@
 class Solution {
 private:
-    int f(int amount, vector<int> &coins, vector<int> &dp){
+// Dp Memoization
+    int f(int amount, vector<int> &coins, vector<vector<int>> &dp,int index){
         int n = coins.size();
-        if(amount < 0) return INT_MAX;
+       
         if(amount == 0) return 0;
+        if(index>=coins.size()) return 1e9;
 
-        if(dp[amount] != -1) return dp[amount];
+        if(dp[index][amount] != -1) return dp[index][amount];
 
-        int mini = INT_MAX;
-        for(int i =0; i<n; i++){
-            int ans = f(amount-coins[i], coins, dp);
-            mini = min(mini, ans);
-        }
-            if(mini != INT_MAX){
-                mini += 1;
-            }
-        return dp[amount] = mini;
+        // int pick = INT_MAX;
+        int pick = 1e9;
+        if(amount>=coins[index]) pick = 1 + f(amount - coins[index] , coins , dp,index);
+        int notpick = f(amount , coins , dp , index+1);
+        int mini = min(pick, notpick);
+        
+        return dp[index][amount] = min(pick , notpick);
     }
 
-    int fTab(vector<int> &coins, int amount){
-        int n = coins.size();
-        vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0;
+// Dp Tabulation
+    // int fTab(vector<int> &coins, int amount){
+    //     int n = coins.size();
+    //     vector<int> dp(amount + 1, INT_MAX);
+    //     dp[0] = 0;
 
-        for(int i = 1; i <= amount; i++){
-            for(int x: coins){
-                if(i - x >= 0 && dp[i - x] != INT_MAX)
-                dp[i] = min(dp[i - x], dp[i]);
-            }
-            if(dp[i] != INT_MAX){
-                dp[i] += 1;
-            }
-        }
+    //     for(int i = 1; i < dp.size(); i++){
+    //         for(int x: coins){
+    //             if(i - x >= 0 && dp[i - x] != INT_MAX)
+    //             dp[i] = min(dp[i - x], dp[i]);
+    //         }
+    //         if(dp[i] != INT_MAX){
+    //             dp[i] += 1;
+    //         }
+    //     }
 
-        return dp[amount];
-    }
+    //     return dp[amount];
+    // }
 
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // vector<int> dp(amount+1, -1);
-        int ans = fTab(coins, amount);
-        if(ans == INT_MAX) return -1;
+        vector<vector<int>> dp(coins.size(), vector<int>(amount+1,-1));
+        int ans = f(amount, coins ,dp, 0);
+        if(ans == 1e9) return -1;
         return ans;
     }
 };
