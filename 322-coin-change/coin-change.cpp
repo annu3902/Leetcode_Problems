64334273b21375eda7aa7dad6295c078c3d6f4fc
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-10 19:06:51 +0530

    Time: 23 ms (89.98%) | Memory: 16.1 MB (48.08%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,45 @@
 class Solution {
-public:
+private:
     int f(int amount, vector<int> &coins, vector<int> &dp){
-        int totalCoins = coins.size();
-        if(amount == 0) return 0;
+        int n = coins.size();
         if(amount < 0) return INT_MAX;
+        if(amount == 0) return 0;
 
         if(dp[amount] != -1) return dp[amount];
 
         int mini = INT_MAX;
-
-        for(int i =0; i <totalCoins; i++){
+        for(int i =0; i<n; i++){
             int ans = f(amount-coins[i], coins, dp);
-            if(ans != INT_MAX){
-                mini = min(mini , ans+1);
-            }
+            mini = min(mini, ans);
         }
+            if(mini != INT_MAX){
+                mini += 1;
+            }
         return dp[amount] = mini;
     }
 
+    int fTab(vector<int> &coins, int amount){
+        int n = coins.size();
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+
+        for(int i = 1; i <= amount; i++){
+            for(int x: coins){
+                if(i - x >= 0 && dp[i - x] != INT_MAX)
+                dp[i] = min(dp[i - x], dp[i]);
+            }
+            if(dp[i] != INT_MAX){
+                dp[i] += 1;
+            }
+        }
+
+        return dp[amount];
+    }
+
+public:
     int coinChange(vector<int>& coins, int amount) {
-        int totalCoins = coins.size();
-        // dp[amount] --> minimum number of coins to produce amount
-        vector<int> dp(amount+1 , -1);
-        int ans = f(amount, coins, dp);
+        // vector<int> dp(amount+1, -1);
+        int ans = fTab(coins, amount);
         if(ans == INT_MAX) return -1;
         return ans;
     }
