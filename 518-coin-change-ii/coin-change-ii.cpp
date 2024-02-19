31d1cf1bab2222dd3faf057cdd1a1b9da1436a42
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-19 19:21:34 +0530

    Time: 15 ms (53.17%) | Memory: 19.9 MB (24.62%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,79 +1,27 @@
 class Solution {
 public:
-
-    int solve(int amount , vector<int> & coins , int index,vector<vector<int>> &dp){
-        if(amount==0) return 1;
-        if(index>=coins.size()) return 0;
-        if(dp[index][amount]!=-1) return dp[index][amount];
-
-        int pick = 0;
-        if(amount>=coins[index]) pick = solve(amount-coins[index] , coins , index,dp);
-
-        int notpick = solve(amount , coins , index+1,dp);
-
-        return dp[index][amount] = pick + notpick;
-    }
-
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(amount+1 , vector<int>(n+1,0));
-        
-        for(int i=0;i<n;i++){
-            dp[0][i]=1;
+        int n=coins.size();
+        // STATE -> NUMBER OF WAYS TO PRODUCE SUM, FROM THE ith INDEX
+        vector<vector<int>> dp(n+1, vector<int> (amount+1)); // No of ways to produce sum from 0 to amount by taking the coins form 0th index till the nth index 
+
+        for(int i=0; i<n; ++i){
+            dp[i][0]=1; // No of ways to produce sum =0 from any index=1
         }
 
-        int k = amount;
-        for(int amount = 1;amount<=k;amount++){
-            for(int index=n-1;index>=0;index--){
-                int pick = 0;
-                if(amount>=coins[index]) pick = dp[amount-coins[index]][index];
-
-                int notpick = dp[amount][index+1];
-
-                dp[amount][index] = pick + notpick;
+        // Since the current state dp[i][sum] depends upon the value of dp[i+1][sum] so the limit goes from (n-1) to 0;
+        for(int i=n-1; i>=0; i--){
+            // dp[i][sum] = dp[i+1][sum] + dp[i][sum-ci];
+            for(int sum=1; sum<=amount; sum++){
+                // Skipping the ith index
+                int skip = dp[i+1][sum];
+                // Picking the ith index
+                int pick = 0; // if {sum < coins[i]} --> We can not make the sum.
+                if(sum >= coins[i])
+                pick = dp[i][sum-coins[i]];
+                dp[i][sum] = skip+pick; // dp[i][sum] = dp[i+1][sum]+dp[i][sum-ci] :{sum >= ci}
             }
         }
-        return dp[k][0];
+        return dp[0][amount];
     }
 };
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int change(int amount, vector<int>& coins) {
-//         int n = coins.size();
-//         vector<int> dp(amount + 1, 0);
-//         dp[0] = 1;
-
-//         for(int i = 1; i <= amount; ++i){
-//             int ways = 0;
-//             for(int j = 0; j < coins.size(); j++){
-//                 if(i-coins[j] >= 0 && dp[i-coins[j]] > 0)
-//                     ways ++;
-//             }
-//             dp[i] = ways;
-//         }
-//         return dp[amount];
-//         // vector<int> dp(amount+1, -1);
-//         // return f(amount, coins, dp, 0);
-//     }
-// };
-
-// class Solution {
-// public:
-//     int change(int amount, vector<int>& coins) {
-//         vector<int> dp(amount + 1, 0);
-//         dp[0] = 1;
-
-//         for(int j = 0; j < coins.size(); j++){
-//             for(int i = coins[j]; i <= amount; ++i){
-//                 dp[i] += dp[i - coins[j]];
-//             }
-//         }
-//         return dp[amount];
-//     }
-// };
