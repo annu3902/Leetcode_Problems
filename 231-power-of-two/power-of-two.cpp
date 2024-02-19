da1be0bc631a4fce31471067f0fbc87e9015cc99
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-20 01:23:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (19.56%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <=0) return false;
-
-        if((n & (n-1)) == 0) return true;
+        if(n <= 0) return false;
+        double x = log2(n);
+        int y = log2(n);
+        if(x == y) return true;
         return false;
     }
 };
-
-// class Solution {
-// public:
-//     bool isPowerOfTwo(int n) {
-//         if (n <= 0) {
-//             return false;
-//         }
-        
-//         return (n & (n - 1)) == 0;
-//     }
-// };
