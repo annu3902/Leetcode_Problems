47a24f6e778a16845191dac8bb719aee0da773cf
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-20 20:12:45 +0530

    Time: 18 ms (33.43%) | Memory: 21.1 MB (5.76%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    bool linearSearch(vector<int> &nums, int key){
-        for(int x : nums){
-            if(x == key) return true;
-        }
-        return false;
-    }
-
     int missingNumber(vector<int>& nums) {
-        for(int i =0; i<=nums.size(); i++){
-            if(linearSearch(nums, i) == 0) return i;
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n; ++i){
+            if(nums[i] != i) return i;
         }
-        return -1;
-    }
+        return n;
+    }   
 };
