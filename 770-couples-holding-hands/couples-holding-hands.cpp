9b9804df48feee1ff21fa7d73e82b03350a5c33f
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-20 22:21:38 +0530

    Time: 4 ms (32.13%) | Memory: 9.4 MB (5.58%) - LeetSync

--- 770-couples-holding-hands/couples-holding-hands.cpp
+++ 770-couples-holding-hands/couples-holding-hands.cpp
@@ -16,26 +16,15 @@ public:
         int cntSwaps=0;
         for(int i=0; i<n; i=i+2){
             int first=row[i];
-            int second;
-            if(first & 1) second = first-1;
-            else second = first+1;
             // int second = first + ((first & 1) ? -1 : 1);
+            int second = first^1;
             if(row[i+1] != second){
                 cntSwaps++;
                 int temp = mpp[second];
                 mpp[row[i+1]] = temp;
                 mpp[second]=i+1;
-                // cout<<row[i+1]<<" "<<second<<endl;
                 swap(row[temp], row[i+1]);
-                for(int x : row){
-                    cout<<x<<" ";
-                }
-                cout<<endl;
             }
-        }
-        // cout<<endl;
-        for(int x : row){
-            cout<<x<<" ";
         }
 
         return cntSwaps;
