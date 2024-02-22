72adad1120b1891f750771947deceb7a5ffcc069
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-22 23:55:02 +0530

    Time: 5 ms (31.04%) | Memory: 9.2 MB (24.31%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,36 +1,60 @@
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        // FORMING A TABLE AND EVALUATE
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         // FORMING A TABLE AND EVALUATE
 
-        // vector<int> maxSumdp(n,0);
-        // maxSumdp[0]=nums[0];
+//         // vector<int> maxSumdp(n,0);
+//         // maxSumdp[0]=nums[0];
 
-        // for(int i=1; i<n; ++i){
-        //     int pick=nums[i];
-        //     if(i>=2){
-        //         pick+=maxSumdp[i-2];
-        //     }
-        //     int skip=maxSumdp[i-1];
-        //     maxSumdp[i] = max(skip, pick);
-        // }
-        // return maxSumdp[n-1];
+//         // for(int i=1; i<n; ++i){
+//         //     int pick=nums[i];
+//         //     if(i>=2){
+//         //         pick+=maxSumdp[i-2];
+//         //     }
+//         //     int skip=maxSumdp[i-1];
+//         //     maxSumdp[i] = max(skip, pick);
+//         // }
+//         // return maxSumdp[n-1];
         
-        // SPACE OPTIMIZATION
-        int dp_1=nums[0];
-        int dp_2=INT_MAX;
+//         // SPACE OPTIMIZATION
+//         int dp_1=nums[0];
+//         int dp_2=INT_MAX;
 
-        for(int i=1; i<n; ++i){
-            int pick = nums[i];
-            if(dp_2 != INT_MAX){
-                pick+=dp_2;
-            }
-            int skip = dp_1;
-            int ans = max(pick,skip);
-            dp_2=dp_1;
-            dp_1=ans;
-        }
-        return dp_1;
+//         for(int i=1; i<n; ++i){
+//             int pick = nums[i];
+//             if(dp_2 != INT_MAX){
+//                 pick+=dp_2;
+//             }
+//             int skip = dp_1;
+//             int ans = max(pick,skip);
+//             dp_2=dp_1;
+//             dp_1=ans;
+//         }
+//         return dp_1;
+//     }
+// };
+
+
+class Solution{
+public:
+    int f(int i, vector<int>&nums, vector<int> &dp){
+        int n=nums.size();
+        if(i==n) return dp[n] = 0;
+        if(dp[i] != -1) return dp[i];
+
+        int skip=f(i+1, nums, dp);
+        int pick=nums[i];
+        if(i+2<=n-1)
+        pick+=f(i+2, nums, dp);
+
+        dp[i] = max(skip, pick);
+        return dp[i];
+    }
+
+    int rob(vector<int>& nums){
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
+        return f(0, nums, dp);
     }
 };
