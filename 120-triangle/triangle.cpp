505aa5acfce354a6b7246aa0661f37498911c331
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-02-23 15:39:32 +0530

    Time: 19 ms (5.39%) | Memory: 11.4 MB (32.39%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        if(n<2) return triangle[0][0];
+        vector<vector<int>> minSumdp(n,vector<int>(n,INT_MAX));
+
+        // dp[k][i] -> minimum path sum till kth row and taking the ith index
+        minSumdp[0][0]=triangle[0][0];
+
+        for(int k=1; k<n; ++k){
+            for(int i=0; i<=k; ++i){
+                // dp[k][i]=dp[k-1][i];
+                int path1 = INT_MAX;
+                if(minSumdp[k-1][i] != INT_MAX)
+                path1=minSumdp[k-1][i];
+                else path1=minSumdp[k-1][i-1];
+                
+                int path2=INT_MAX;
+                if(i>=1)
+                    path2=minSumdp[k-1][i-1];
+                // cout<<path1<<" "<<path2<<endl;
+                minSumdp[k][i] = min(path1, path2) + triangle[k][i];
+                cout<<minSumdp[k][i]<<" "<<endl;
+            }
+            cout<<endl;
+        }
+
+        int miniPathSum = minSumdp[n-1][0];
+        for(int x : minSumdp[n-1]){
+            miniPathSum = min(miniPathSum, x);
+        }
+        return miniPathSum;
+    }
+};
