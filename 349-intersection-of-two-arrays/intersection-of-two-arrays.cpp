6a2fc2c556b677dac0f82284ddef475fb234a9e3
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-03-10 06:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (61.63%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,65 +1,25 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        // unordered_set<int> st1;
-        // unordered_set<int> st2;
-        // unordered_set<int> st3;
-
-        // for(int x : nums1){
-        //     st1.insert(x);
-        // }
-
-        // for(int x : nums2){
-        //     st2.insert(x);
-        // }
-
-        // for(int x : st1){
-        //     if(st2.count(x)){
-        //         st3.insert(x);
-        //     }
-        // }
-
-        // vector<int> ans;
-        // for(int x : st3){
-        //     ans.push_back(x);
-        // }
-        // return ans;
-
-        sort(nums1.begin(),nums1.end());
+        int n=nums1.size();
+        int m=nums2.size();
+        sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
-        unordered_set<int> st;
-        int i = 0, j =0;
-
-        while(i < nums1.size() && j < nums2.size()){
-            if(nums1[i] < nums2[j]) i++;
-            else if(nums2[j] < nums1[i]) j++;
-            else{
-                if(st.find(nums1[i]) == st.end()){
-                    st.insert(nums1[i]);
+        int i=0;
+        int j=0;
+        vector<int> ans;
+        while(i<n&&j<m){
+            if(nums1[i]<nums2[j]) i++;
+            else if(nums2[j]<nums1[i]) j++;
+            else if(nums1[i]==nums2[j]){
+                if(ans.empty()) ans.push_back(nums1[i]);
+                else if(!ans.empty()&&nums1[i]!=ans.back()){
+                    ans.push_back(nums1[i]);
                 }
                 i++;
                 j++;
             }
         }
-        for(auto x: st){
-            cout<<x<<" ";
-        }
-        vector<int>ans; 
-
-        for(int x : st){
-            ans.push_back(x);
-        }
         return ans;
-
-        // vector<int> v;
-        // vector<int> :: iterator it;
-        // for(int i=0;i<nums1.size();i++){
-        //     it = find(nums2.begin(), nums2.end(),nums1[i]);
-        //     if(it!=nums2.end()){
-        //         if(find(v.begin(),v.end(),nums1[i])==v.end()) v.push_back(nums1[i]);
-        //     }
-        // }
-
-        // return v;
     }
 };
