3b2e26fedb48d8e0e27996263d2cb2c5106b73c3
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-03-14 20:36:57 +0530

    Time: 4 ms (35.83%) | Memory: 7.1 MB (74.39%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,14 +1,10 @@
-int dp[1001] = { 0, 0 };
 class Solution {
 public:
-int pivotInteger(int n) {
-    if (dp[1] == 0)
-       for (int i = 1, j = 1; i <= 1000; ++i) {
-           int sum = i * (i + 1) / 2;
-           while (j * j < sum)
-               ++j;
-           dp[i] = j * j == sum ? j : -1;
-       }
-    return dp[n];
-}
+    int pivotInteger(int n) {
+        int num = ((n*n)+n)/2;
+        double val1 = sqrt(num);
+        int val2 = sqrt(num);
+        int ans = (val1==val2) ? val2 : -1;
+        return ans;
+    }
 };
