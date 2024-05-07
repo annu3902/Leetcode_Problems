b39c4238965b050c3b685c3f43ce97dfbaee5e98
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-07 10:03:28 +0530

    Time: 311 ms (14.88%) | Memory: 176.5 MB (13.55%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -1,25 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+
+    ListNode* insertAtHead(ListNode* &curr, int value){
+        ListNode* temp = new ListNode(value);
+        temp->next = curr;
+        curr=temp;
+        return curr;
+    }
+
     ListNode* removeNodes(ListNode* head) {
-        ListNode* cur = head;
-        stack<ListNode*> stack;
-        
-        while (cur != nullptr) {
-            while (!stack.empty() && stack.top()->val < cur->val) {
-                stack.pop();
+        stack<int> st;
+        // Inserting nodes to stack;
+        ListNode* temp = head;
+        while(temp->next){
+            st.push(temp->val);
+            temp=temp->next;
+        }
+
+        ListNode* curr = new ListNode(temp->val); 
+        int maxi = curr->val;
+        while(!st.empty()){
+            if(maxi <= st.top()){
+                maxi = st.top();
+                insertAtHead(curr, maxi);
             }
-            stack.push(cur);
-            cur = cur->next;
+            st.pop();
         }
-        
-        ListNode* nxt = nullptr;
-        while (!stack.empty()) {
-            cur = stack.top();
-            stack.pop();
-            cur->next = nxt;
-            nxt = cur;
-        }
-        
-        return cur;
+        return curr;
     }
 };
