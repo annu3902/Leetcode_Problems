cd6cb24a1d46663cbe9acad71b5440d2360d2a32
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-08 02:56:58 +0530

    Time: 182 ms (46.05%) | Memory: 121.1 MB (43.64%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // To compute twice the value of each node's value and propagate carry
+    int twiceOfVal(ListNode* head){
+        // Base case: if head is null, return 0
+        if (!head) return 0;
+        
+        // Double the value of current node and add the result of next nodes
+        int doubledValue = head->val * 2 + twiceOfVal(head->next);
+        
+        // Update current node's value with the units digit of the result
+        head->val = doubledValue % 10;
+        
+        // Return the carry (tens digit of the result)
+        return doubledValue / 10;
+    }
+    
+    ListNode* doubleIt(ListNode* head) {
+        int carry = twiceOfVal(head);
+        
+        // If there's a carry, insert a new node at the beginning with the carry value
+        if (carry) 
+            head = new ListNode(carry, head);
+        
+        return head;
+    }
+};
