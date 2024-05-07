98c2053d5041fb2d074a5451a7c4a12e75863ff3
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-07 13:10:58 +0530

    Time: 252 ms (88.75%) | Memory: 164.4 MB (65.50%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -11,31 +11,19 @@
 class Solution {
 public:
 
-    ListNode* insertAtHead(ListNode* &curr, int value){
-        ListNode* temp = new ListNode(value);
-        temp->next = curr;
-        curr=temp;
-        return curr;
+    ListNode* solve(ListNode* head){
+        if(!head->next) return head;
+        ListNode* nextNode = solve(head->next);
+        if(head->val < nextNode->val)
+            // delete(head);
+            head = nextNode;
+        else{
+            head->next = nextNode;
+        }
+        return head;
     }
 
     ListNode* removeNodes(ListNode* head) {
-        stack<int> st;
-        // Inserting nodes to stack;
-        ListNode* temp = head;
-        while(temp->next){
-            st.push(temp->val);
-            temp=temp->next;
-        }
-
-        ListNode* curr = new ListNode(temp->val); 
-        int maxi = curr->val;
-        while(!st.empty()){
-            if(maxi <= st.top()){
-                maxi = st.top();
-                insertAtHead(curr, maxi);
-            }
-            st.pop();
-        }
-        return curr;
+        return solve(head);
     }
 };
