64c726c506fbdf484ae52d0c8c8c7854cdee82e5
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-14 21:26:46 +0530

    Time: 122 ms (59.42%) | Memory: 73.6 MB (20.52%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -11,25 +11,57 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        map<int, int> freq;
+        if(head==NULL || head->next==NULL) return head; // Base Case
+        // Fiding mid
+        ListNode* mid = middleOfLL(head);
 
-        ListNode* temp=head;
-        while(temp!=NULL){
-            freq[temp->val]++;
-            temp=temp->next;
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = NULL;
+
+        // sORT BOTH THE LIST   
+        left = sortList(left);
+        right = sortList(right);
+
+        // MERGE THE LIST
+        ListNode* result = merge2LL(left, right);
+        return result;
+    }
+
+    ListNode* middleOfLL(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-
-        temp=head;
-        for(pair<int,int> it : freq){
-            int first = it.first;
-            int second = it.second;
-            cout<<first<<" "<<second<<" ";
-            while(second){
-                temp->val=first;
-                second--;
-                temp=temp->next;
+        return slow;
+    }
+    
+    ListNode* merge2LL(ListNode* left, ListNode* right){
+        ListNode* dummy = new ListNode();
+        ListNode* ans = dummy;
+        
+        while(left!=NULL && right!=NULL){
+            if(left->val <= right->val){
+                dummy->next = left;
+                left = left->next;
             }
+            else{
+                dummy->next = right;
+                right = right->next;
+            }
+            dummy = dummy->next;
         }
-        return head;
+
+        if(!left && right){
+            dummy->next = right;
+        }
+        else if(!right && left){
+            dummy->next = left;
+        }
+        
+        return ans->next;
     }
 };
