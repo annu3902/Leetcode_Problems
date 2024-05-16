1729bf37e5194597c1e26a1d88275f34db175f03
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-16 20:30:31 +0530

    Time: 15 ms (81.66%) | Memory: 16.4 MB (40.04%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,5 +1,7 @@
-class Solution {
-public:
+// class Solution {
+// public:
+    /*  Checking Bipartite using DFS
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         
@@ -34,4 +36,51 @@ public:
 
         return true;
     }
+};
+
+*/
+
+
+class Solution{
+public:
+    bool isBipartite(vector<vector<int>>& graph){
+
+        int n=graph.size();
+        vector<int> color(n, -1);
+
+        for(int node=0; node<n; node++){
+            if(color[node] == -1 && bfsBipartite(node, graph, color, 0) == false){
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    bool bfsBipartite(int node, vector<vector<int>> &graph, vector<int> &color, int currColor){
+
+        color[node] = currColor;
+        queue<pair<int, int>> q;
+        q.push({node, currColor});
+
+        while(!q.empty()){
+            pair<int, int> frontNode = q.front();
+            int u = frontNode.first;
+            int clr = frontNode.second;
+            q.pop();
+
+            for(auto &v : graph[u]){
+                if(color[v] == clr) return false;
+
+                if(color[v] == -1){
+                    color[v] = 1-clr;
+                    q.push({v, color[v]});
+                }
+            }
+        }
+        return true;
+
+    }
+
 };
