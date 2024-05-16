a4077cdcce3f2b80c829c947fae28c5a1cb2413f
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-16 13:28:52 +0530

    Time: 19 ms (37.53%) | Memory: 19.3 MB (13.39%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -2,47 +2,56 @@ class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
         
-        int m=prerequisites.size();
-        vector<int> inDegree(n, 0);
+        int m =prerequisites.size();
         unordered_map<int, vector<int>> adj;
 
+        // {a,b} = b -> a
         for(int i=0; i<m; i++){
             int a = prerequisites[i][0];
             int b = prerequisites[i][1];
 
             adj[b].push_back(a);
-            // inDegree Calculation
-            inDegree[a]++;
         }
 
-        // Kahn's algo
-        queue<int> q;
-        for(int u=0; u<n; u++){
-            if(inDegree[u] == 0){
-                q.push(u);
-            }
-        }
-        
+        // 2. toposort check
         vector<int> result;
+        stack<int> st;
         vector<bool> visited(n, false);
+        vector<bool> inRecurssion(n, false);
+
         int cnt=0;
-
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-            result.push_back(u);
-            visited[u] = true;
-            cnt++;
-
-            for(auto &v : adj[u]){
-                inDegree[v]--;
-                if(!inDegree[v]) q.push(v);
+        for(int node=0; node<n; node++){
+            if(!visited[node] && dfs(node, adj, visited, st, inRecurssion)){
+                return {};
             }
         }
 
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
 
-        if(cnt != n) return{};
         return result;
     }
 
+    bool dfs(int u, unordered_map<int, vector<int>> &adj, vector<bool> &visited, stack<int> &st, vector<bool> &inRecurssion){
+
+        visited[u] = true;
+        inRecurssion[u] = true;
+        
+        for(auto &v : adj[u]){
+            if(!visited[v]){ 
+                if(dfs(v, adj, visited, st, inRecurssion)){
+                    return true;
+                }       
+            }
+            if(inRecurssion[v]){
+                return true;
+            }
+        }
+
+        st.push(u);
+        inRecurssion[u] = false;
+        return false;
+    }
 };
