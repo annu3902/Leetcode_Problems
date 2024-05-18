18078a9d8a888298a90535298fdb1d22ed455a33
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-19 00:29:06 +0530

    Time: 17 ms (6.76%) | Memory: 9.7 MB (32.79%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,47 +3,36 @@ public:
     bool isAnagram(string s, string t) {
         int n = s.size();
         int m = t.size();
+        if(n != m) return false;
+
         map<char, int> mpp1;
         map<char, int> mpp2;
 
-        if(n != m) return false;
-
         for(int i = 0; i < n; i++){
-            char sh = s[i];
-            char th  = t[i];
-            mpp1[sh]++;
-            mpp2[th]++;
+            mpp1[s[i]]++;
+            mpp2[t[i]]++;
         }
 
-        // auto it1 = mpp1.begin();
-        // auto it2 = mpp2.begin();
+        auto it1 = mpp1.begin();
+        auto it2 = mpp2.begin();
 
-        // for(; it1 != mpp1.end(); it1++){
+        for(; it1 != mpp1.end(); it1++){
 
-        //     pair<char, int> p1 = *it1;
-        //     pair<char, int> p2 = *it2;
-        //     char ch1 = p1.first;
-        //     int cnt1 = p1.second;
-        //     char ch2 = p2.first;
-        //     int cnt2 = p2.second;
-
-        //     if(ch1 != ch2 ) return false;
-        //     else if(cnt1 != cnt2) return false;
-        //     // else if(ch1 != ch2) return false;
-        // }
-        auto it = mpp2.begin();
-        for(pair<char, int> p : mpp1){
-            char ch = p.first;
-            int cnt = p.second;
-
-            pair<char, int> p2 = *it;
+            pair<char, int> p1 = *it1;
+            pair<char, int> p2 = *it2;
+            char ch1 = p1.first;
+            int cnt1 = p1.second;
             char ch2 = p2.first;
             int cnt2 = p2.second;
 
-            if(ch != ch2) return false;
-            else if (ch == ch2 && cnt != cnt2) return false;
-            it++;
+            cout<<ch1<<" "<<ch2<<endl;
+
+            if(ch1 != ch2 ) return false;
+            else if(cnt1 != cnt2) return false;
+            // else if(ch1 != ch2) return false;
+            it2++;
         }
         return true;
+
     }
 };
