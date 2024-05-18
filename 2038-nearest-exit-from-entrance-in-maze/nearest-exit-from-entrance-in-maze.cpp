c59ddd5c54af10f3799775a7211746093a0f8c88
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-18 11:39:13 +0530

    Time: 153 ms (20.85%) | Memory: 52.1 MB (16.04%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -4,8 +4,8 @@ public:
         int m = maze.size();
         int n = maze[0].size();
 
-        queue<vector<int>> q;
-        q.push(entrance);
+        queue<pair<int, int>> q;
+        q.push({entrance[0], entrance[1]});
         maze[entrance[0]][entrance[1]] = '+';
         vector<vector<int>> directions = {{-1,0}, {1, 0}, {0, -1}, {0, 1}};
         int level = 0;
@@ -16,12 +16,12 @@ public:
 
             while(size--){
 
-                vector<int> front = q.front();
+                pair<int, int> front = q.front();
                 q.pop();
-                
+
                 for(vector<int> dir : directions){
-                    int i = front[0] + dir[0];
-                    int j = front[1] + dir[1];
+                    int i = front.first + dir[0];
+                    int j = front.second + dir[1];
 
                     if(i >= 0 && i<=m-1 && j >= 0 && j <= n-1 && maze[i][j] == '.'){
                         if(i==0 || i==m-1 || j==0 || j==n-1){
