42a2a2807765a8da02ecb09cc2a3b05af54c6aa5
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-19 00:16:40 +0530

    Time: 87 ms (75.19%) | Memory: 73 MB (63.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,38 +1,36 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;    
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(st.find(nums[i]) == st.end()){
+                st.insert(nums[i]);
+            }
+            else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
 // class Solution {
 // public:
 //     bool containsDuplicate(vector<int>& nums) {
-//         unordered_map<int,int> mpp;
-        
-//         for(int i=0; i<nums.size(); i++)
-//         {
-//             mpp[nums[i]]++;
-//         }
+//         sort(nums.begin(), nums.end());
 
-//         for(auto it : mpp)
+//         int i=0;
+//         for(int j=1; j<nums.size(); j++)
 //         {
-//             if(it.second >= 2){
+//             if(nums[i] != nums[j]){
+//                 i=j;
+//             }
+//             else{
 //                 return true;
-//                 break;
 //             }
 //         }
 //         return false;
 //     }
 // };
-
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int i=0;
-        for(int j=1; j<nums.size(); j++)
-        {
-            if(nums[i] != nums[j]){
-                i=j;
-            }
-            else{
-                return true;
-            }
-        }
-        return false;
-    }
-};
