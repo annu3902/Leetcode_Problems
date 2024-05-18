a3482cfa26a4771af13eda50bc929f1dab18ea78
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-18 12:46:49 +0530

    Time: 1035 ms (31.53%) | Memory: 358.4 MB (8.36%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,31 +1,39 @@
-// Approach 1: BFS based approach
-// ==============================
-
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        vector<vector<int>> graph(n);
-        // Build the graph
-        for(int i=0; i<edges.size(); i++) {
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
-        }
-        
-        // BFS
-        queue<int> q;
-        vector<int> visited(n, 0);
-        q.push(start);
-        visited[start] = 1;
-        while(!q.empty()){
-            int top = q.front();
-            q.pop();
-            if(top == end)
-                return true;
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
 
-            for(int i=0; i<graph[top].size(); i++){
-                if(visited[graph[top][i]] == 0){
-                    q.push(graph[top][i]);
-                    visited[graph[top][i]] = 1;
+        // 1. graph formation
+        unordered_map<int, vector<int>> adj;
+
+        for(int i=0; i<edges.size(); i++){
+            // i=0;
+            // edges[0] = {0,1}
+            // 0 - 1
+            // 1 - 0
+            vector<int> vec = edges[i];
+            int u = vec[0];
+            int v = vec[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+
+        while(!q.empty()){
+
+            int u = q.front();
+            q.pop();
+            if(u == destination) return true;
+
+            for(auto &v : adj[u]){
+                if(!visited[v]){
+                    visited[v] = true;
+                    q.push(v);
                 }
             }
         }
