ffba69d0ecc6a5c0ed7fe579f510c8a023a1842d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-18 16:30:35 +0530

    Time: 185 ms (9.16%) | Memory: 70.7 MB (45.64%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,52 +1,59 @@
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& edges) {
-
-        // Graph formation
+        
         unordered_map<int, vector<int>> adj;
+
         for(int i=0; i<edges.size(); i++){
-                // edges[0] = {1,2}
-                // 1 -> 2;
+            // edges[0] = {1,2}
             int u = edges[i][0];
             int v = edges[i][1];
+
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
-        vector<int> color (n+1,-1);
-        for(auto & it : adj){
-            cout<<it.first<<" -> "; 
-            for(int &x: it.second){
-                cout<<x<<" ";
-            } 
-            cout<<endl;      
+
+        queue<pair<int, int>> q;
+        vector<int> color(n+1, -1);
+
+
+    for(int i=1; i<=n; i++){
+        if(color[i] == -1){
+            q.push({i,0});
         }
 
-        for(int i=1; i<=n; i++){
+        while(!q.empty()){
+            int size = q.size();
 
-            if(color[i] == -1 && dfs(i, adj, color, 0) == false){
-                return false;                
-            }
-        }
+            // while(size--){
 
-        return true;
-    }
+                pair<int,int> front = q.front();
+                int u = front.first;
+                int currColor = front.second;
 
+                color[u] = currColor;
+                q.pop();
+                cout<<u<<" ";
 
-    bool dfs(int node, unordered_map<int, vector<int>> &adj, vector<int> &color, int currColor){
+                currColor = 1 - color[u];
 
-            color[node] = currColor;
+                for(auto &v: adj[u]){
+                    if(color[v] == color[u]) return false;
 
-            currColor = 1- currColor;
+                    else if(color[v] == -1){
 
-            for(auto &v : adj[node]){
-                if(color[v] == color[node]) return false;
+                        q.push({v, currColor});
+                        color[v] = currColor;
 
-                else if(color[v] == -1 && dfs(v, adj, color, currColor) == false){
-                    return false;
+                    }
+
                 }
+            // }
 
-            }
-        return true;
+        }
+    }
+        return true;   
+
     }
 };
