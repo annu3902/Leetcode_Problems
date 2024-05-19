6f6e40aec37cfba830048a72f93fa4493258f348
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-19 21:31:05 +0530

    Time: 708 ms (5.20%) | Memory: 137.7 MB (11.01%) - LeetSync

--- 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -1,38 +1,99 @@
 class Solution {
 public:
-    vector<vector<int>> v;
-    vector<int> counter, res;
-        
-    void dfs(int i, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            dfs(u, i);
-            counter[i] += counter[u];
-            res[i] += res[u] + counter[u];
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        if(n == 1) return {0};
+
+        // Graph formation
+        unordered_map<int, vector<int>> adj;
+        for(vector<int> edge : edges){
+            int u =edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        counter[i] += 1;
+
+        vector<bool> visited(n, false);
+        unordered_map<int,int> parent;
+        queue<int> q;
+
+        q.push(0);
+        visited[0] = true;
+        parent[0] = -1;
+        int sum=0;
+
+        int level = 0;
+
+        // Level order traversal
+        while(!q.empty()){  
+            int size = q.size();
+            while(size--){
+                int u = q.front();
+                sum += level;
+                q.pop();
+
+                for(auto &v : adj[u]){
+                    if(!visited[v]){
+                        q.push(v);
+                        visited[v] = true;
+                        parent[v] = u;
+                    }
+                }
+            }
+            level++;
+        }
+
+        vector<int> result(n, -1);
+        result[0] = sum;
+
+        vector<int> children(n, 0);
+        children[0] = dfsChild(0, -1, adj, children);
+
+        queue<pair<int, int>> que;
+        fill(visited.begin(), visited.end(), false);
+
+        que.push(make_pair(0, -1));
+
+        while(!que.empty()){
+            int size = que.size();
+
+            while(size--){
+
+                int child = que.front().first;
+                int parent = que.front().second;
+                int ch = children[child];
+                que.pop();
+
+                for(auto &v : adj[child]){
+
+                    if(!visited[v]){
+                        que.push({v, child});
+                        visited[v] = true;
+                    }
+
+                }
+
+                if(parent != -1 && result[child] == -1){
+                    cout<<child<<" "<<parent<<" "<<result[parent]<<endl;
+                    result[child] = result[parent] - ch + (n - ch);
+                }
+                
+            }
+        }
+
+        return result;
+
     }
 
-    void dfs2(int i, int n, int p = -1) {
-        for(auto u : v[i]) {
-            if(u == p) continue;
-            res[u] = res[i] - counter[u] + n - counter[u];
-            dfs2(u, n, i);
+    int dfsChild(int node, int parent, unordered_map<int, vector<int>> &adj, vector<int> &children){
+        children[node] = 1;
+        int totalNode = 1;
+
+        for(auto &v : adj[node]){
+            if(v == parent) continue;
+
+            totalNode += dfsChild(v, node, adj, children);
         }
-    }
-    
-    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        v.resize(n);
-        for(int i = 0; i < n - 1; i++) {
-            int a = edges[i][0];
-            int b = edges[i][1];
-            v[a].push_back(b);
-            v[b].push_back(a);
-        }
-        res.resize(n);
-        counter.resize(n);
-        dfs(0);
-        dfs2(0, n);
-        return res;
+        return children[node] = totalNode;
     }
 };
