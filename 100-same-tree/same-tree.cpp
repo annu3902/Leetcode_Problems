521edea85d4d9a86a4fd5e3cb3999b53bcabc46c
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-24 02:18:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (97.19%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -9,34 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//
-// Algorithm for the recursion:
-// 1)
-// If one of the node is NULL then return the equality result of p an q.
-// This boils down to if both are NULL then return true, 
-// but if one of them is NULL but not the other one then return false
-// 2)
-// At this point both root nodes represent valid pointers.
-// Return true if the root nodes have same value and 
-// the left tree of the roots are same (recursion)
-// and the right tree of the roots are same (recursion). 
-// Otherwise return false. 
-//
-
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q)
-    {
-        stack<pair<TreeNode*, TreeNode*>> st;
-        st.push({p, q});
-        while (!st.empty()) {
-            auto [node1, node2] = st.top();
-            st.pop();
-            if (!node1 && !node2) continue;
-            if (!node1 || !node2 || node1->val != node2->val) return false;
-            st.push({node1->left, node2->left});
-            st.push({node1->right, node2->right});
-        }
-        return true;
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL && q == NULL) return true;
+        else if(p == NULL && q != NULL) return false;
+        else if(p != NULL && q == NULL) return false;
+
+        bool leftPart = isSameTree(p->left, q->left);
+        bool rightPart = isSameTree(p->right, q->right);
+
+        if(leftPart == true && rightPart == true && p->val == q->val) return true;
+
+        return false;
     }
 };
