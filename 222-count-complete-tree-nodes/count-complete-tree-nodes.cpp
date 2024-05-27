506aaf6077dfcac1a39ac19fbd64466009f3a917
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-27 12:55:02 +0530

    Time: 24 ms (54.50%) | Memory: 29 MB (96.64%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,30 +11,48 @@
  */
 class Solution {
 public:
+
+    int power(int x , int n){
+        int t = abs (n);
+
+        if(t == 0) return 1;
+        else if(t == 1) return x;
+
+        long long ans = power(x, n/2);
+            ans *= ans;
+
+        if(t & 1) ans *= x;
+
+        if(n >= 0) return ans;
+
+        return 1/ans;
+    }
+
     int countNodes(TreeNode* root) {
+        
+        if(!root) return 0;
+
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        if(leftHeight == rightHeight) return power(2,leftHeight) - 1;
+
+        int countLeftNodes = countNodes(root->left);
+        int countRightNodes = countNodes(root->right);
+
+        return countLeftNodes + countRightNodes + 1;
+    }
+
+    int getLeftHeight(TreeNode* root){
 
         if(root == NULL) return 0;
-
-        queue<TreeNode*> q;
-        q.push(root);
-        int cnt=0;
-
-        while(!q.empty()){
-            int size = q.size();
-
-            while(size--){
-
-                TreeNode* node = q.front();
-                q.pop();
-                cnt++;
-
-               if(node->left) q.push(node->left);
-               if(node->right) q.push(node->right);
-
-            }
-
-        }
-        return cnt;
-
+        return (1 + getLeftHeight(root->left));
     }
+
+    int getRightHeight(TreeNode* root){
+
+        if(root == NULL) return 0;
+        return (1 + getRightHeight(root->right));
+    }
+
 };
