53cdd24f179fb4c9167688302e10904e97fc96c9
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-30 23:41:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (59.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     bool isValid(string s) {
+        
         stack<char> st;
-        for(int i = 0; i < s.size(); i++){
-            char ch = s[i];
+
+        for(char ch : s){
             if(ch == '(' || ch == '{' || ch == '['){
                 st.push(ch);
             }
-            else if(!st.empty()){
-                char sh = st.top();
-                if(sh == '(' && ch == ')'){
-                    st.pop();
-                }
-                else if(sh == '{' && ch == '}'){
-                    st.pop();
-                }
-                else if(sh == '[' && ch == ']'){
-                    st.pop();
-                }
+
+            else if(ch == ')'){
+                if(st.empty() || st.top() != '(') return false;
                 else{
-                    return false;
-                }
-                continue;
-            }   
-            else{
-                return false;
+                    st.pop();
+                } 
             }
+
+            else if(ch == '}'){
+                if(st.empty() || st.top() != '{') return false;
+                else{
+                    st.pop();
+                } 
+
+            }
+
+            else if(ch == ']'){
+                if(st.empty() || st.top() != '[') return false;
+                else{
+                    st.pop();
+                } 
+            }
+
         }
-        if(!st.empty()) return false;
-        return true;
+
+        return st.empty();
     }
 };
