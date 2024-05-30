20a3559d861e2b75407631f999efe70288b6b1b9
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-05-31 02:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (28.35%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    string addOne(string &s){
-        int n = s.size();
-        int i = n-1;
-
-        while(i >= 0 && s[i] != '0'){
-            s[i] = '0';
-            i--;
-        }
-
-        if(i < 0){
-            s = '1' + s;
-        }
-        else{
-            s[i] = '1';
-        }
-        return s;
-    }
-
     int numSteps(string s) {
-        int n =s.size();
-        int ans = 0;
-    
+        int n = s.size();
+        int count = 0;
+        
         while(s.size() > 1){
-            int n =s.size();
-            
-            if(s[n-1] == '1'){
-                addOne(s);
+
+            int n = s.size();
+            int lastIndex = n-1;
+
+            if(s[lastIndex] == '1'){
+                // No. is odd
+                int i=lastIndex;
+                while(i >= 0 && s[i] == '1'){
+                    s[i] = '0';
+                    i--;
+                }
+
+                if(i < 0){
+                    s = '1' + s;
+                }
+                else{
+                    s[i] = '1';
+                }
+                count++;
             }
 
-            else{
+            else if(s[lastIndex] == '0'){
                 s.pop_back();
+                count++;
             }
-
-            ans++;
         }
-        return ans;
+        return count;
+
     }
 };
