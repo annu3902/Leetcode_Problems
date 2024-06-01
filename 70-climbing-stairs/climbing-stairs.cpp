5b592c5f9ad88cad2f2135453a35ea4aabbfdf28
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-01 20:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (11.48%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,32 +1,16 @@
-// int value(int destination, vector<int> &dp){
-//     // Base Case
-//     if (destination == 0) {
-//         return 1;
-//     }
-//     if(destination<0) return 0;
-
-//     // Recurrence Relation
-//     if(dp[destination] != -1) return dp[destination];
-//     int tot = value(destination - 1,dp) + value(destination - 2, dp);
-//     return dp[destination] = tot;
-// }
-
 class Solution {
 public:
     int climbStairs(int n) {
-        // vector<int> dp(n + 1 , -1);
-        // return value(n , dp);
+        vector<int> dp(n+1, -1);
 
-        int dp0 = 1;
-        int dp1 = 1;
-        int totalWays = 1;
+        // dp[i] -> No. of ways to reach to the ith step
+        dp[0] = 1;
+        dp[1] = 1;
 
-        for(int i = 2; i<=n; i++){
-            totalWays = dp0 + dp1;
-            dp0 = dp1;
-            dp1 = totalWays;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
 
-        return totalWays;
+        return dp[n];
     }
 };
