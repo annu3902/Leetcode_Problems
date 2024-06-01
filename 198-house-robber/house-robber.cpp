7b85dd142005ffd8b4f84a2bf7778fb7aa0260ac
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-01 22:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (10.56%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,23 +4,39 @@ public:
         int n = nums.size();
         vector<int> dp(n, -1);
 
-        if(n == 1){
-            return (nums[0]);
-        }
+        return solve(0, nums, dp, n);
 
-        // dp[i] -> ith index tak ka collection
+        // if(n == 1){
+        //     return (nums[0]);
+        // }
+
+        // // dp[i] -> ith index tak ka collection
         
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
 
-        for(int i=2; i<n; i++){
+        // for(int i=2; i<n; i++){
 
-            int op1 = dp[i-2] + nums[i];
-            int op2 = dp[i-1];
+        //     int op1 = dp[i-2] + nums[i];
+        //     int op2 = dp[i-1];
  
-            dp[i] = max(op1, op2);
+        //     dp[i] = max(op1, op2);
+        // }
+
+        // return dp[n-1];
+    }
+
+    int solve(int index, vector<int> &nums, vector<int> &dp, int n){
+
+        if(index >= n){
+            return 0;
         }
 
-        return dp[n-1];
+        else if(dp[index] != -1) return dp[index];
+        int op1 = nums[index] + solve(index + 2, nums, dp, n);
+        int op2 = solve(index + 1, nums, dp, n);
+
+        dp[index] = max(op1, op2);
+        return dp[index];
     }
 };
