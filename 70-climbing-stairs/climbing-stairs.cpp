2ceaec812954de2ef350c9c771d856589a39de00
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-01 20:50:03 +0530

    Time: 3 ms (29.30%) | Memory: 7.3 MB (31.94%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,14 +2,26 @@ class Solution {
 public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
+        solve(n, dp);
+        return dp[n];
 
         // dp[i] -> No. of ways to reach to the ith step
-        dp[0] = 1;
-        dp[1] = 1;
+        // dp[0] = 1;
+        // dp[1] = 1;
 
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
+        // for(int i=2; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+
+        return dp[n];
+    }
+
+    int solve(int n, vector<int> & dp){
+        if(n==0 || n==1) return dp[n] = 1;
+
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
 
         return dp[n];
     }
