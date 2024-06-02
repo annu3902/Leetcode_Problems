c0a106091166d39ae60f3c46da55eb12932b2734
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-03 00:35:42 +0530

    Time: 16 ms (68.47%) | Memory: 27 MB (21.89%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    void reverseStringHelper(int left, int right,vector<char> &s){
-        // Writting Base Condition 
-        if(left >= right) return;
-
-        // Do small task - 2
-        swap(s[left], s[right]);
-
-        // Call recursion to do later 
-        reverseStringHelper(left+1, right-1, s);
-        
-
-    }
-
     void reverseString(vector<char>& s) {
-        reverseStringHelper(0, s.size()-1, s);
+        int n = s.size();
+        int i=0; int j = n-1;
+
+        while(i <= j){
+            swap(s[i++], s[j--]);
+        }
     }
 };
