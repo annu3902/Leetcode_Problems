e17b821ccc0e0fe86994e7915c9494820ce5d395
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-02 23:34:12 +0530

    Time: 4 ms (32.96%) | Memory: 9.3 MB (67.07%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,63 +1,28 @@
 class Solution {
-private:
-    // Space optimized solution of robbing 1
-    int maxSum(vector<int> nums){
-        int n=nums.size();
-        if(n==1) return nums[0];
-        int dp0=nums[0];
-        int dp1=max(nums[0], nums[1]);
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
 
-        for(int i=2; i<n; ++i){
-            int skip=dp1;
-            int pick=nums[i]+dp0;
-            int ans = max(pick, skip);
-            dp0=dp1;
-            dp1=ans;
-        }
-        return dp1;
+        if(n == 1) return nums[0];
+        if(n == 2) return (max(nums[0], nums[1]));
+         
+        vector<int> dp1(n+1, 0);
+        vector<int> dp2(n+1, 0);
+
+        dynamicProgramming(nums, dp1, 0, n-2);
+        dynamicProgramming(nums, dp2, 1, n-1);
+
+        return max(dp1[n-2], dp2[n-1]);
+
     }
 
+    void dynamicProgramming(vector<int> &nums, vector<int> &dp, int start, int end){
+        int n = nums.size();
+        dp[start] = nums[start];
+        if(start != end)dp[start + 1] = max(nums[start], nums[start + 1]);
 
-public:
-    // int rob(vector<int>& nums) {
-    //     int n=nums.size();
-    //     if(n==1) return nums[0];
-    //     vector<int> maxSumdp1(n-1,0);
-        
-    //     maxSumdp1[0]=nums[0];
-    //     for(int i=1; i<n-1; ++i){
-    //         int skip=maxSumdp1[i-1];
-    //         int pick=nums[i];
-    //         if(i>=2) pick += maxSumdp1[i-2];
-    //         maxSumdp1[i] = max(skip,pick);
-    //     }
-    //     // cout<<"Hii"<<endl;
-    //     vector<int> maxSumdp2(n+1,0);
-    //     maxSumdp2[1]=nums[1];
-    //     for(int i=2; i<n; ++i){
-    //         int skip=maxSumdp2[i-1];
-    //         int pick=nums[i];
-    //         if(i>=2) pick+=maxSumdp2[i-2];
-    //         maxSumdp2[i]=max(skip, pick);
-    //     }
-    //     return max(maxSumdp1[n-2], maxSumdp2[n-1]);
-
-
-        // Space Optimization
-        int rob(vector<int>&nums){
-            int n=nums.size();
-            if(n<2) return nums[0];
-            vector<int> skipLast(n-1);
-            vector<int> skipFirst(n-1);
-
-            for(int i=0; i<n-1; ++i){
-                skipLast[i]=nums[i];
-                skipFirst[i]=nums[i+1];
-            }
-
-            int max1=maxSum(skipLast);
-            int max2=maxSum(skipFirst);
-
-            return max(max1, max2);
+        for(int i=start +2; i<=end; i++){
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
         }
+    }
 };
