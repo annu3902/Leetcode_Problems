ab59da08d8bd6958b5ac0be8cdd974f19d65bfff
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-02 23:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (28.29%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -4,25 +4,25 @@ public:
         int n = nums.size();
 
         if(n == 1) return nums[0];
-        if(n == 2) return (max(nums[0], nums[1]));
-         
-        vector<int> dp1(n+1, 0);
-        vector<int> dp2(n+1, 0);
+        else if(n == 2) return max(nums[0], nums[1]);
 
-        dynamicProgramming(nums, dp1, 0, n-2);
-        dynamicProgramming(nums, dp2, 1, n-1);
+        vector<int> dp1 (n+1, -1);
+        vector<int> dp2 (n+1, -1);
 
-        return max(dp1[n-2], dp2[n-1]);
+        int ans1 = solve(nums, 0, n-2, dp1);
+        int ans2 = solve(nums, 1, n-1, dp2);
 
+        return max(ans1, ans2);
     }
 
-    void dynamicProgramming(vector<int> &nums, vector<int> &dp, int start, int end){
-        int n = nums.size();
-        dp[start] = nums[start];
-        if(start != end)dp[start + 1] = max(nums[start], nums[start + 1]);
+    int solve(vector<int> &nums, int start, int end, vector<int> &dp){
 
-        for(int i=start +2; i<=end; i++){
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
-        }
+        if(start > end) return 0;
+
+        if(dp[start] != -1) return dp[start];
+
+        dp[start] = max(nums[start] + solve(nums, start + 2, end, dp), solve(nums, start + 1, end, dp));
+
+        return dp[start];
     }
 };
