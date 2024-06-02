4ff1b2dcdaa407dfb8c157b770b5699e1605d119
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-02 17:52:50 +0530

    Time: 774 ms (16.67%) | Memory: 100.6 MB (66.67%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        int n=s.size();
+
+        vector<vector<int>> vec(26);
+        unordered_set<int> st;
+
+        for(int i = 0; i<n; i++){
+
+            if(s[i] != '*')vec[s[i] - 'a'].push_back(i);
+
+            else{
+                for(int k=0; k<26; k++){
+                    // 0 -> 'a'
+                    // 1 -> 'b'
+                    if(vec[k].empty()){
+                        continue;
+                    }
+                    else{
+                        cout<<"Hii ";
+                        st.insert(vec[k].back());   
+                        vec[k].pop_back();
+                        break;
+                    }
+                }
+            }
+        }
+
+        string ans="";
+
+        for(auto it : st){
+            cout<<it<<" ";
+        }
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '*') continue;
+
+            else if(st.count(i)) continue;
+
+            else{
+                ans += s[i];
+            }
+
+        }
+
+        return ans;
+        
+    }
+};
