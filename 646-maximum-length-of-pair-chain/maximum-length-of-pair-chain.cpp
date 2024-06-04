df846cc83d5f7bce16c60ab1b661030e8d537621
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-05 00:44:05 +0530

    Time: 227 ms (17.32%) | Memory: 30.6 MB (26.20%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
-    int findLongestChain(vector<vector<int>>& pairs) {
-        int n = pairs.size();
-        sort(pairs.begin(), pairs.end());
-        
-        vector<int> dp(n, 1);
+    int n;
+    int dp[1001][1001];
 
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(pairs[j][1] < pairs[i][0]){
-                    dp[i] = max(1 + dp[j], dp[i]);
-                }
-            }
+    int findLongestChain(vector<vector<int>>& pairs) {
+        n = pairs.size();
+        sort(pairs.begin(), pairs.end());
+        memset(dp, -1, sizeof(dp));
+
+        return solve(pairs, 0, -1);
+        
+        // vector<int> dp(n, 1); // dp[i] => Length of longest increasing subsequence ending at index i
+
+        // for(int i=1; i<n; i++){
+        //     for(int j=0; j<i; j++){
+        //         if(pairs[j][1] < pairs[i][0]){
+        //             dp[i] = max(1 + dp[j], dp[i]);
+        //         }
+        //     }
+        // }
+
+        // return *max_element(dp.begin(), dp.end());
+    }
+
+    int solve(vector<vector<int>> &pairs, int currIndex, int prevIndex){
+
+        if(currIndex == pairs.size()) return 0;
+
+        if(prevIndex != -1 && dp[currIndex][prevIndex] != -1) return dp[currIndex][prevIndex];
+
+        int skip = solve(pairs, currIndex+1, prevIndex);
+
+        int pick = 0;
+
+        if(prevIndex == -1 || pairs[currIndex][0] > pairs[prevIndex][1]){
+            pick = 1 + solve(pairs, currIndex+1, currIndex);
         }
 
-        return *max_element(dp.begin(), dp.end());
+        if(prevIndex != -1) dp[currIndex][prevIndex] = max(skip, pick);
 
-
+        return  max(skip, pick);
     }
 };
