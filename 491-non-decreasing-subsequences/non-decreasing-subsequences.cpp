902b44941e99ecf7780349ad4d2c0a84181a036e
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-06 01:19:38 +0530

    Time: 18 ms (92.67%) | Memory: 21.2 MB (89.95%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-
+vector<vector<int>> ans;
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        if(n == 1) return {};
+        int n=nums.size();
         vector<int> temp;
-        solve(nums, 0, -1, temp, ans);
+        solve(nums, 0, temp);
         return ans;
     }
 
-    void solve(vector<int> &nums, int currIndex, int prevIndex, vector<int> temp, vector<vector<int>> &ans){
-
+    void solve(vector<int> &nums, int currIndex, vector<int> &temp){
         if(currIndex >= nums.size()){
             if(temp.size() >= 2){
-                auto it = find(ans.begin(), ans.end(), temp);
-                if(it == ans.end()){
-                    for(auto i : temp) cout<<i<<" ";
-                    cout<<endl; 
-                    ans.push_back(temp);
-                }
+                ans.push_back(temp);
             }
             return;
         }
 
-        // Skip
-        solve(nums, currIndex+1, prevIndex, temp, ans);
-        // Pick
-        if(prevIndex == -1 || nums[currIndex] >= nums[prevIndex]){
+        // pick
+        if(currIndex == 0 || temp.empty() || temp.back() <= nums[currIndex]){
             temp.push_back(nums[currIndex]);
-            solve(nums, currIndex+1, currIndex, temp, ans);
+            solve(nums, currIndex+1, temp);
+            temp.pop_back();
         }
+
+        // skip
+        if(temp.empty() || temp.back() != nums[currIndex]){
+            solve(nums, currIndex+1, temp);
+        }
+        
+        return;
+
     }
 };
