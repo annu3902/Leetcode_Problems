998d745bab0472f646ea0fb60b7cbc7f18a5ae00
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-06 02:54:15 +0530

    Time: 13 ms (95.27%) | Memory: 11.8 MB (83.52%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s1, string s2) {
-        int n = s1.size();
-        int m = s2.size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+    int t[1001][1001];
+    int solve(string &text1, int i, int j, string& text2){
 
-        return lcs(s1, 0, s2, 0, dp);
+        if(i >= text1.size() && j >= text2.size()) return 0;
+        else if(i >= text1.size() && j < text2.size()) return 0;
+        else if(i < text1.size() && j >= text2.size()) return 0;
+
+        else if(t[i][j] != -1) return t[i][j];
+
+        if(text1[i] == text2[j]){
+            return 1 + solve(text1, i+1, j+1, text2);
+        }
+        
+        int op1 = solve(text1, i+1, j, text2);
+        int op2 = solve(text1, i, j+1, text2);
+
+        return t[i][j] = max(op1, op2);
     }
 
-    int lcs(string& s1, int i, string& s2, int j, vector<vector<int>> &dp){
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
 
-        if(i == s1.size() || j == s2.size()) return 0;
+        memset(t, 0, sizeof(t));
+        for(int j=0; j<n; j++) t[0][j] = 0;
+        for(int i=0; i<m; i++) t[i][0] = 0;
+        // return solve(text1, 0, 0, text2);
+        // t[i][j] -> Length of longest common subsequence ending at index i,j
+        for(int i=1; i<=m; i++){
 
-        if(dp[i][j] != -1) return dp[i][j];
+            for(int j=1; j<=n; j++){
+                if(text1[i-1] == text2[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j] , t[i][j-1]);
+                }
+            }
 
-        int cnt = 0;
-        if(s1[i] == s2[j]){
-           cnt = 1 + lcs(s1, i+1, s2, j+1, dp);
-           return dp[i][j] = cnt;
         }
 
-        return dp[i][j] = max(lcs(s1, i+1, s2, j, dp), lcs(s1, i, s2, j+1, dp));
-    }
+        return t[m][n];
 
+    }
 };
