3153731edb3f40eb9bd05bad08d21e2c7d9a2986
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-05 22:14:43 +0530

    Time: 2276 ms (5.07%) | Memory: 78.5 MB (10.84%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -2,36 +2,34 @@ class Solution {
 public:
 
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-    vector<vector<int>> ans;
-    set<vector<int>> st;
+        vector<vector<int>> ans;
         int n = nums.size();
         if(n == 1) return {};
         vector<int> temp;
-        solve(nums, 0, -1, temp, st);
-
-        for(auto it : st){
-            ans.push_back(it);
-        }
-
+        solve(nums, 0, -1, temp, ans);
         return ans;
     }
 
-    void solve(vector<int> &nums, int currIndex, int prevIndex, vector<int> temp, set<vector<int>> &st){
+    void solve(vector<int> &nums, int currIndex, int prevIndex, vector<int> temp, vector<vector<int>> &ans){
 
         if(currIndex >= nums.size()){
             if(temp.size() >= 2){
-                // ans.push_back(temp);
-                st.insert(temp);
+                auto it = find(ans.begin(), ans.end(), temp);
+                if(it == ans.end()){
+                    for(auto i : temp) cout<<i<<" ";
+                    cout<<endl; 
+                    ans.push_back(temp);
+                }
             }
             return;
         }
 
         // Skip
-        solve(nums, currIndex+1, prevIndex, temp,st);
+        solve(nums, currIndex+1, prevIndex, temp, ans);
         // Pick
         if(prevIndex == -1 || nums[currIndex] >= nums[prevIndex]){
             temp.push_back(nums[currIndex]);
-            solve(nums, currIndex+1, currIndex, temp, st);
+            solve(nums, currIndex+1, currIndex, temp, ans);
         }
     }
 };
