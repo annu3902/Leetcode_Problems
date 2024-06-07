586346f3ec80a65277b0a6302a37db0c46335645
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-08 01:06:40 +0530

    Time: 43 ms (80.18%) | Memory: 48.1 MB (79.93%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,68 @@
+class Trie {
+public:
+    class TrieNode{
+        public:
+        TrieNode* children[26];
+        bool isEnd;
+        TrieNode() {
+            this->isEnd = false;
+            for(int i=0; i<26; i++){
+                this->children[i] = NULL;
+            }  
+        };
+    };
+    
+    TrieNode* root;
+
+    Trie(){
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* crawler = root;
+
+        for(int i=0; i<word.size(); i++){
+            int index = word[i] - 'a';
+            if(crawler->children[index] == NULL){
+                crawler->children[index] = new TrieNode();
+            }
+            crawler = crawler->children[index];
+        }
+
+        crawler->isEnd = true;
+
+    }
+    
+    bool search(string word) {
+        TrieNode* crawler = root;
+
+        for(int i=0; i<word.size(); i++){
+            int index = word[i] - 'a';
+            if(crawler->children[index] == NULL) return false;
+
+            crawler = crawler->children[index];
+        }
+
+        return crawler->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* crawler = root;
+
+        for(int i=0; i<prefix.size(); i++){
+            int index= prefix[i] - 'a';
+            if(crawler->children[index] == NULL) return false;
+
+            crawler = crawler->children[index];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
