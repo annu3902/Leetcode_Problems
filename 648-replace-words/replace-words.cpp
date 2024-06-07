40bda4aaec00d36284aa83c8e8c2bc08db67f0b5
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-08 00:33:42 +0530

    Time: 55 ms (96.51%) | Memory: 76.5 MB (49.53%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,36 +1,81 @@
 class Solution {
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string> st(begin(dictionary), end(dictionary));
+    // Implementing the trie data structure
+    class trieNode{
+        public :
+        trieNode *children[26];
+        bool isEnd;
 
-        stringstream ss (sentence);
+        trieNode(){
+            this->isEnd = false;
+
+            for(int i=0; i<26; i++){
+                this->children[i] = NULL;
+            }
+        }
+    };
+
+    /*trieNode* getNode(){
+        trieNode* newNode = new trieNode();
+        newNode->isEnd = false;
+
+        for(int i=0; i<26; i++){
+            newNode->children[i] = NULL;
+        }
+
+        return trieNode;
+    }*/
+
+    trieNode* root;
+
+    void insert(string word){
+        trieNode* crawler = root;
+
+        for(int i=0; i<word.size(); i++){
+            int index = word[i] - 'a';
+            if(crawler->children[index] == NULL){
+                crawler->children[index] = new trieNode();
+            }
+            crawler = crawler->children[index];
+        }
+
+        crawler->isEnd = true;
+    }
+
+    string search(string word){
+        trieNode* crawler = root;
+
+        for(int i=0; i<word.size(); i++){
+            int index = word[i] - 'a';
+            if(crawler->children[index] == NULL){
+                // the root of this word is not present 
+                return word;
+            }
+            
+            crawler = crawler->children[index];
+            if(crawler->isEnd == true){
+                return word.substr(0, i+1);
+            }
+        }
+        return word;
+    }
+
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        root = new trieNode();
+
+        for(string &word : dictionary){
+            insert(word);
+        }
+
+        stringstream ss(sentence);
         string word;
 
         string result="";
 
         while(ss >> word){
-
-            // if(word == " "){
-            //     result.push_back(' ');
-            //     continue;
-            // }
-
-            string temp="";
-            bool flag = false;
-            for(int i=0; i<word.size(); i++){
-                temp += word[i];
-                if(st.find(temp) == st.end()) continue;
-
-                else{
-                    flag = true;
-                    result += (temp);
-                    break;
-                }
-            }
-            if(flag == false) result += (temp);
-            result +=" ";
-            
+            result += search(word) +" ";
         }
+
         result.pop_back();
         return result;
     }
