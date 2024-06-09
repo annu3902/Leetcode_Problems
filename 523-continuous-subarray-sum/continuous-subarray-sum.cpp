a9cf83ea253ea518befdd6aba21e463727f9297c
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-09 23:02:03 +0530

    Time: 176 ms (66.72%) | Memory: 134.4 MB (96.47%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        // Using the remainder map for checking
-
         int n = nums.size();
-        vector<int> prefixSum(n, 0);
-        prefixSum[0] = nums[0];
-
-        for(int i=1; i<n; i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
 
         unordered_map<int, int> mp;
-        for(int i=0; i<n; i++){
-            int remainder = prefixSum[i] % k;
+        int sum = 0;
 
-            if(remainder==0 && i + 1 >= 2) return true;
+        for(int i = 0; i < n; i++){
 
-            else if(mp.count(remainder)){
-                int index = mp[remainder];
-                if(i-index >= 2) return true;
+            sum += nums[i];
+
+            int remainder = sum % k;
+            if(remainder == 0 && i + 1 >= 2) return true;
+
+            else if(mp.count(remainder) && i - mp[remainder] >= 2){
+                return true;
             }
-            
-            if(mp.count(remainder) == 0)
+
+            if(mp.count(remainder) == 0){
                 mp[remainder] = i;
+            }
+
         }
 
         return false;
