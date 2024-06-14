ccda494008457d800a782270e5d52934d1eacb1a
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-14 19:58:05 +0530

    Time: 7 ms (63.94%) | Memory: 12 MB (34.52%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
-int dp[502][502];
     int minDistance(string word1, string word2) {
         int m = word1.length();
         int n = word2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
 
-        memset(dp, -1, sizeof(dp));
+        // dp[i][j] -> minimum number of operations required to convert string 1 of length i and string2 of length j.
+
+        for(int col=0; col <= n; col++){
+            dp[0][col] = col;
+        }
+
+        for(int row=0; row <= m; row++){
+            dp[row][0] = row;
+        }
+
+        for(int i=1; i <= m; i++){
+            for(int j=1; j <= n; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+                else{
+                    int op1 = 1 + dp[i][j-1];
+                    int op2 = 1 + dp[i-1][j-1];
+                    int op3 = 1 + dp[i-1][j];
+
+                    dp[i][j] = min({op1, op2, op3});
+                }
+
+            }
+        }
+
+        return dp[m][n];
+        
+        
         // dp[i][j] -> minimum number of operations required to convert string1 of length i to string2 of length j.
 
-        return dfs(word1, word2, 0, 0);
+        // return solve(word1, word2, m, n);
     }
 
     // Top to down Recursion
-    int dfs(string &word1, string &word2, int i, int j){
+    /*int dfs(string &word1, string &word2, int i, int j){
 
         if(i == word1.length() && j == word2.length()) return 0;
 
@@ -40,9 +69,7 @@ int dp[502][502];
 
         if(m == 0 && n == 0) return 0;
 
-        if(n == 0) return 1 + solve(word1, word2, m-1, n);
-
-        if(m == 0) return 1 + solve(word1, word2, m, n-1);
+        if(n == 0 || m == 0) return m + n;
 
         if(dp[m][n] != -1) return dp[m][n];
 
@@ -56,6 +83,6 @@ int dp[502][502];
 
         return dp[m][n] = min({op1, op2, op3});
 
-    }
+    }*/
 
 };
