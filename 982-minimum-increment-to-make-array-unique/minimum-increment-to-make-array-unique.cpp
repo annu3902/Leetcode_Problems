eaa1eebaec7b1733d15cdeea7b51803b60763233
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-15 11:23:18 +0530

    Time: 125 ms (42.18%) | Memory: 69.1 MB (79.77%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -2,37 +2,18 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         int n = nums.size();
-        int max_val = 0;
-        int minIncrements = 0;
-
-        // Find maximum value in nums using a loop
-        for (int val : nums) {
-            max_val = max(max_val, val);
+        
+        sort(nums.begin(), nums.end());
+        int count = 0;
+        
+        // 1 1 2 2 3 7
+        for(int i=1; i < nums.size(); i++){
+            if(nums[i] <= nums[i-1]){
+                count += (nums[i-1] - nums[i] + 1);
+                nums[i] = nums[i-1] + 1;
+            } 
         }
-
-        // Create a frequencyCount vector to store the frequency of each value
-        // in nums
-        vector<int> frequencyCount(n + max_val + 1, 0);
-
-        // Populate frequencyCount vector with the frequency of each value in
-        // nums
-        for (int val : nums) {
-            frequencyCount[val]++;
-        }
-
-        // Iterate over the frequencyCount vector to make all values unique
-        for (int i = 0; i < frequencyCount.size(); i++) {
-            if (frequencyCount[i] <= 1) continue;
-
-            // Determine excess occurrences, carry them over to the next value,
-            // ensure single occurrence for current value, and update
-            // minIncrements.
-            int duplicates = frequencyCount[i] - 1;
-            frequencyCount[i + 1] += duplicates;
-            frequencyCount[i] = 1;
-            minIncrements += duplicates;
-        }
-
-        return minIncrements;
+        
+        return count;
     }
 };
