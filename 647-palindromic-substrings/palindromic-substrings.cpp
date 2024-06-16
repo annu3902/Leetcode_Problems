f75a873843f71bc419add59aaed22f2fb7e6b09d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-17 00:02:01 +0530

    Time: 7 ms (72.12%) | Memory: 7.6 MB (72.93%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,57 +1,33 @@
 class Solution {
 public:
-// int dp[1000][1000];
+    int count = 0;
+
     int countSubstrings(string s) {
+        count = 0;
+
         int n = s.size();
+        for(int i=0; i<n; i++){
 
-        // memset(dp, -1, sizeof(dp));
+            checkPalindrome(i, i, s, n);
+            checkPalindrome(i, i+1, s, n);
 
-        // int count = 0;
-        // for(int i=0; i<s.size(); i++){
-        //     for(int j=i; j<s.size(); j++){
-        //         if(checkPalindrome(i, j, s)){
-        //             count++;
-        //         }
-        //     }
-        // }
-
-        // return count; 
-
-
-        vector<vector<bool>> dp(n, vector<bool>(n, 0));
-
-        // dp[i][j] -> it represents weather a string starting from index i and ending to index j is a palindrome or not.
-        int count = 0;
-
-        for(int i=n-1; i >= 0; i--){
-
-            for(int j=0; j <= n-1; j++){
-                if(i >= j){
-                    if(i == j) count++;
-                    dp[i][j] = true;
-                }
-
-                else if(s[i] == s[j] && dp[i+1][j-1] == true){
-                    dp[i][j] = true;
-                    count++;
-                }
-                else{
-                    dp[i][j] = false;
-                }
-            }
         }
+
         return count;
+
     }
 
-    // bool  checkPalindrome(int start, int end, string &s){
-    //     if(start > end) return true;
+    void checkPalindrome(int i, int j, string& s, int n){
 
-    //     if(dp[start][end] != -1) return dp[start][end];
+        if(i > j) return;
 
-    //     if(s[start] == s[end]){
-    //         return dp[start][end] = checkPalindrome(start+1, end-1, s);
-    //     }
+        while(i >= 0 && j < n && s[i] == s[j]){
+            count++;
+            i--;
+            j++;
+        }
 
-    //     return dp[start][end] = false;
-    // }
+        return;
+
+    }
 };
