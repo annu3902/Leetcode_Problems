38b1c096a3be92c429e7ddd41a11471667d7f0ce
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-06-24 16:09:30 +0530

    Time: 125 ms (7.99%) | Memory: 134.9 MB (5.12%) - LeetSync

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int result = 0;
+        int currFlip = 0;
+        unordered_map<int, bool> mp;
+
+        for(int i=0; i < n; i++){
+
+            if(i-k >= 0 && mp[i-k] == true){
+                currFlip--;
+            }
+
+            if((nums[i] == 0 && (currFlip % 2 == 0)) || (nums[i] == 1 && (currFlip % 2 == 1))){
+                if(i+k <= n){
+                    result++;
+                    currFlip++;
+                    mp[i] = true;
+                }
+                else{
+                    return -1;
+                }
+            }
+
+        }
+
+        return result;
+
+    }
+};
