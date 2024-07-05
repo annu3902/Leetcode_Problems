b0e7035773ed609180f8fbe52e9047836cc774ab
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-06 01:26:21 +0530

    Time: 829 ms (53.43%) | Memory: 328.2 MB (41.68%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-
-        // 1. graph formation
-        unordered_map<int, vector<int>> adj;
+        unordered_map<int,vector<int>> adj;
 
         for(int i=0; i<edges.size(); i++){
-            // i=0;
-            // edges[0] = {0,1}
-            // 0 - 1
-            // 1 - 0
-            vector<int> vec = edges[i];
-            int u = vec[0];
-            int v = vec[1];
+            int u = edges[i][0];
+            int v = edges[i][1];
 
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
-
+        
         vector<bool> visited(n, false);
         queue<int> q;
+
         q.push(source);
         visited[source] = true;
 
         while(!q.empty()){
-
-            int u = q.front();
+            int frontNode = q.front();
+            if(frontNode == destination) return true;
             q.pop();
-            if(u == destination) return true;
 
-            for(auto &v : adj[u]){
+            for(auto &v : adj[frontNode]){
                 if(!visited[v]){
-                    visited[v] = true;
                     q.push(v);
+                    visited[v] = true;
                 }
             }
+
         }
+
         return false;
+
     }
 };
