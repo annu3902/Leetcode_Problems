b3822e4a8423230663f4d74b45f8049c83affa9c
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-05 19:45:19 +0530

    Time: 22 ms (27.80%) | Memory: 16.1 MB (64.24%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,86 +1,29 @@
-// class Solution {
-// public:
-    /*  Checking Bipartite using DFS
-
+class Solution {
+public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        
-        // To check if we can divide the element of edge in 2 different nodes -> By colouring it with two different colours
-
-        // 1 - > Red
-        // 2 - > Blue
-        vector<int> color(n,-1);
-
-        for(int node=0; node<n; node++){
-
-            if(color[node] == -1 && dfsBipartite(node, graph, color, 1) == false){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    bool dfsBipartite(int node, vector<vector<int>> &graph, vector<int> &color, int currColor){
-
-        color[node] = currColor;
-
-        currColor = 1-currColor;
-        for(auto &v : graph[node]){
-            if(color[v] == color[node]) return false;
-            
-            else if(color[v] == -1 && dfsBipartite(v, graph, color, currColor) == false){
-                return false;
-            }
-        }
-
-        return true;
-    }
-};
-
-*/
-
-
-class Solution{
-public:
-    bool isBipartite(vector<vector<int>>& graph){
-
-        int n=graph.size();
         vector<int> color(n, -1);
 
-        for(int node=0; node<n; node++){
-            if(color[node] == -1 && bfsBipartite(node, graph, color, 0) == false){
+        for(int i=0; i<n; i++){
+            if(color[i] == -1 && dfs(graph, color, i, 0) == false){
                 return false;
             }
-
         }
 
         return true;
     }
 
-    bool bfsBipartite(int node, vector<vector<int>> &graph, vector<int> &color, int currColor){
+    bool dfs(vector<vector<int>> &graph, vector<int> &color, int u, int currColor){
 
-        color[node] = currColor;
-        queue<pair<int, int>> q;
-        q.push({node, currColor});
+        color[u] = currColor;
 
-        while(!q.empty()){
-            pair<int, int> frontNode = q.front();
-            int u = frontNode.first;
-            int clr = frontNode.second;
-            q.pop();
-
-            for(auto &v : graph[u]){
-                if(color[v] == clr) return false;
-
-                if(color[v] == -1){
-                    color[v] = 1-clr;
-                    q.push({v, color[v]});
-                }
+        for(auto &v : graph[u]){
+            if(color[v] == -1 && dfs(graph, color, v, 1-currColor) == false){
+                return false;
             }
+            else if(color[v] == color[u]) return false;
         }
+
         return true;
-
     }
-
 };
