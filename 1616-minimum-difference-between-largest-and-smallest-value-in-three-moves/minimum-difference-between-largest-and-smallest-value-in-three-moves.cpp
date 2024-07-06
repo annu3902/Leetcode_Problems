9672d39135c52c6100de63ddc611b8de7d7a1402
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-06 12:51:54 +0530

    Time: 78 ms (10.41%) | Memory: 38.7 MB (77.77%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -5,15 +5,19 @@ public:
         int  n = nums.size();
 
         sort(begin(nums), end(nums));
+        int mini = INT_MAX;
 
-        // for(int i=1; i<=4; i++){
-        //     int diff = 
-        // }
-        int op1 = nums[n-1] - nums[3];
-        int op2 = nums[n-2] - nums[2];
-        int op3 = nums[n-3] - nums[1];
-        int op4 = nums[n-4] - nums[0];
+        for(int i=0; i<=3; i++){
+            mini = min(mini, nums[n-(i+1)] - nums[3-i]);
+        }
 
-        return min({op1, op2, op3, op4});     
+        return mini;
+
+        // int op1 = nums[n-1] - nums[3];
+        // int op2 = nums[n-2] - nums[2];
+        // int op3 = nums[n-3] - nums[1];
+        // int op4 = nums[n-4] - nums[0];
+
+        // return min({op1, op2, op3, op4});     
     }
 };
