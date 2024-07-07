8c68e5a2308e5482a0d286bb672400dc6364d991
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-07 15:15:05 +0530

    Time: 401 ms (20.00%) | Memory: 278.5 MB (20.00%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -13,19 +13,15 @@ public:
         return ans;
     }
 
-    vector<string> validStrings(int n) {
+    /*vector<string> validStrings(int n) {
         vector<string> ans;
         if(n == 1) return {"0", "1"};
         vector<string> nums;
         
         for(int i=1; i<pow(2,n); i++){
-            // cout<<i <<" ";
             string s = detobi(i, n);
             nums.push_back(s);
         }
-        // cout<<endl;
-
-        // for(string s : nums) cout <<s<<" ";
 
         for(int i=0; i < nums.size(); i++){
             bool flag = true;
@@ -39,5 +35,35 @@ public:
         }
 
         return ans;
+    }*/
+
+    vector<string> validStrings(int n){
+
+        string s;
+        for(int i=0; i<n; i++) s+='0';
+        vector<string> ans;
+        dfs(s, 0, n, ans);
+
+        return ans;
     }
+
+    void dfs(string s, int start, int length, vector<string> &ans){
+        if(start >= length){
+            for(int i=1; i<length; i++){
+                if(s[i] == '0' && s[i-1] == '0') return; 
+            }
+            ans.push_back(s);
+            return;
+        }
+
+        dfs(s, start+1, length, ans);
+
+        s[start] = '1';
+        dfs(s, start+1, length, ans);
+    }
+
+
+
+
+
 };
