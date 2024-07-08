c48c8c9d9381f98895ac9e3418fe068d9deb2374
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-08 16:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (62.35%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
-    int countVowelSubstrings(string nums) {
+    int countVowelSubstrings(string word) {
+        return (atMost(word, 5) - atMost(word, 4));
+    }
+
+    int atMost(string nums, int k){
+
         int n = nums.size();
+        int i=0;
+        int j=0;
         int cnt = 0;
+        unordered_map<char, int> mp;
 
-        for(int i=0; i<n; i++){
-            unordered_map<char, int> mp;
+        while(j < n){
+            if(isVowel(nums[j])){
+                mp[nums[j]] ++;
 
-            for(int j=i; j<n && isVowel(nums[j]); j++){ // THis loop is to count kha tak ham substring bana skte h
-                mp[nums[j]]++;
-                if(mp.size() == 5) cnt++;
+                while(mp.size() > k){
+                    mp[nums[i]]--;
+                    if(mp[nums[i]] == 0){
+                        mp.erase(nums[i]);
+                    }
+                    i++;
+                }
+
+                if(mp.size() <= k)
+                    cnt += (j-i+1);
             }
+
+            else{
+                mp.clear();
+                i = j+1;
+            }
+
+            j++;
         }
+        cout<<cnt<<" ";
+
         return cnt;
+
     }
 
     bool isVowel(char c){
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; 
     }
 };
