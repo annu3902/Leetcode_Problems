5e3fc40307421a859b07b2b14d0ffad63724b079
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-08 14:38:43 +0530

    Time: 136 ms (37.86%) | Memory: 56 MB (36.77%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,48 +1,35 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        
         int n = nums.size();
 
-        int count1 = slidingWindow(nums, k);
+        int cnt2 = slidingWindow(nums, k);
+        int cnt1 = slidingWindow(nums, k-1);
+        cout<<cnt1<<" "<<cnt2;
 
-        int count2 = slidingWindow(nums, k-1);
-    
-        return (count1-count2);
+        return (cnt2 - cnt1);
     }
 
     int slidingWindow(vector<int> &nums, int k){
-
         int n = nums.size();
 
         int i=0;
         int j=0;
-        unordered_map<int, int> mpp;
-        int countSubarrays = 0;
+        int cnt = 0;
+        unordered_map<int, int> mp;
 
         while(j < n){
+            mp[nums[j]]++;
 
-            mpp[nums[j]]++;
-
-            while(mpp.size() > k){
-
-                mpp[nums[i]]--;
-                if(mpp[nums[i]] == 0){
-                    mpp.erase(nums[i]);
-                }
-
+            while(mp.size() > k){
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0) mp.erase(nums[i]);
                 i++;
             }
-
-            if(mpp.size() <= k){
-                countSubarrays += (j-i+1); // counting the subarays having the unique elements less than or equals to k
-            }
-
+            if(mp.size() <= k) cnt += (j-i+1); // Count all the subarrays ending at index j; they will have less than or equal to k different elements.
+            
             j++;
-
         }
-
-        return countSubarrays;
+        return cnt;
     }
-
 };
