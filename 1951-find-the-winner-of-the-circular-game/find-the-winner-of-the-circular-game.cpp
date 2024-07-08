becbb39ec6a1a0f099871f548f1ae55cd4b92636
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-08 10:50:38 +0530

    Time: 3 ms (65.36%) | Memory: 7.2 MB (76.96%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        return dfs(n, k) + 1;      
+    }
+
+    int dfs(int n, int k){
+        if(n == 1) return 0;
+
+        return ((dfs(n-1, k) + k) % n);
+    }
+};
