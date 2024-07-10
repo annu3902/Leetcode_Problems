938c1e6fb3cb9c147610b74d3075e2744d50046d
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-10 21:22:59 +0530

    Time: 292 ms (46.91%) | Memory: 146.1 MB (25.94%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,28 +1,39 @@
 class NumMatrix {
 public:
 vector<vector<int>> prefixSum;
+int m,n;
     NumMatrix(vector<vector<int>>& matrix) {
-        prefixSum = matrix;
-        for(int i=0; i<prefixSum.size(); i++){
-            for(int j=1; j<prefixSum[0].size(); j++){
-                prefixSum[i][j] += prefixSum[i][j-1];
+        m = matrix.size();
+        n = matrix[0].size();
+
+        prefixSum.resize(m+1, vector<int> (n+1, 0));
+        
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                prefixSum[i][j] = matrix[i-1][j-1] + prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1]; 
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-
         int ans = 0;
-        for(int i=row1; i<=row2; i++){
-            if(col1 == 0) ans += prefixSum[i][col2];
-            else ans += prefixSum[i][col2] - prefixSum[i][col1 - 1];
-        }
+        ans += prefixSum[row2+1][col2+1] - prefixSum[row2+1][col1] - prefixSum[row1][col2+1] + prefixSum[row1][col1];
+
+        // return prefixSum[row2 + 1][col2 + 1] - prefixSum[row2 + 1][col1] - prefixSum[row1][col2 + 1] + prefixSum[row1][col1];
+
+        // for(int i=0; i<prefixSum.size(); i++){
+        //     for(int j=0; j<prefixSum[0].size(); j++){
+        //         cout<<prefixSum[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
         return ans;
     }
 };
 
-/**
+/*
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix* obj = new NumMatrix(matrix);
  * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
+*/
