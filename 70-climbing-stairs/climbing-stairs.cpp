f8f507ff7dc5723ca219b255eaa3777ca2fb658b
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-12 01:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (11.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,47 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        // solve(n, dp);
-        // return dp[n];
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
 
-        // dp[i] -> No. of ways to reach to the ith step
-        // dp[0] = 1;
-        // dp[1] = 1;
+        for(int i=1; i<=n; i++){
+            dp[i] = dp[i-1] + ((i-2 >= 0) ? dp[i-2] : 0);
+        }
 
-        // for(int i=2; i<=n; i++){
-        //     dp[i] = dp[i-1] + dp[i-2];
-        // }
-
-        // return dp[n];
-
-
-        // Solution 3
-        return solve2(0, n, dp);
-    }
-
-    // Recursion for going down
-
-    // int solve(int n, vector<int> & dp){ 
-    //     if(n==0 || n==1) return dp[n] = 1;
-
-    //     if(dp[n] != -1) return dp[n];
-
-    //     dp[n] = solve(n-1, dp) + solve(n-2, dp);
-
-    //     return dp[n];
-    // }
-    
-
-    // Recursion for coming up
-    int solve2(int i, int n, vector<int> &dp){
-
-        if(i == n) return dp[i] = 1;
-        else if(i > n) return 0;
-
-        else if(dp[i] != -1) return dp[i];
-
-        dp[i] = solve2(i+1, n, dp) + solve2(i+2, n, dp);
-        return dp[i];
+        return dp[n];
     }
 };
