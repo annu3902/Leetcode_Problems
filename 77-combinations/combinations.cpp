1c74b1cb2d4b965393a41af66056af21debab8f6
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-16 22:31:10 +0530

    Time: 76 ms (95.01%) | Memory: 62 MB (56.89%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,25 +1,24 @@
 class Solution {
-private : 
-    void solve(int ind, vector<int> &temp, vector<vector<int>> &ans, int k, int n){
-        // Base Case
-        if(temp.size() == k){
-            ans.push_back(temp);
-            return;
-        }
-
-        // Processing
-        for(int i = ind; i <= n; i++){
-            temp.push_back(i);
-            solve(i + 1, temp, ans, k, n);
-            temp.pop_back();
-        }
-        return;
-    }
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;
         vector<int> temp;
-        solve(1, temp, ans, k, n);
-        return ans;
+        vector<vector<int>> result;
+        solve(1, n, k, temp, result);
+        return result;
+    }
+
+    void solve(int num, int n, int k, vector<int> &temp, vector<vector<int>> &result){
+        if(temp.size() == k){
+            result.push_back(temp);
+            return;
+        }
+        // if(temp.size() > k) return;
+
+        for(int i=num; i<=n; i++){
+            temp.push_back(i);
+            solve(i+1, n, k, temp, result);
+
+            temp.pop_back();
+        }
     }
 };
