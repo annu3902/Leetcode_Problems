89f75656f14cd02a1547fb1dcca51948eb6c8627
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-19 01:20:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (7.65%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -6,9 +6,19 @@ public:
 
         if(obstacleGrid[m-1][n-1] == 1) return 0;
 
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        dp[0][0] = 1;
 
-        return f(m-1, n-1, obstacleGrid, dp);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) continue;
+                dp[i][j] = ((i>=1 && obstacleGrid[i-1][j] == 0) ? dp[i-1][j] : 0) + ((j>=1 && obstacleGrid[i][j-1] == 0) ? dp[i][j-1] : 0);
+            }
+        }
+
+        return dp[m-1][n-1];
+
+        // return f(m-1, n-1, obstacleGrid, dp);
     }
 
     int f(int i, int j, vector<vector<int>> &obstacleGrid, vector<vector<int>>& dp){
