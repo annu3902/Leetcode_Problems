1e089c9c561cf7707217836f9c73664146885321
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-19 01:46:10 +0530

    Time: 6 ms (20.66%) | Memory: 10.5 MB (10.74%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -6,17 +6,31 @@ public:
 
         if(obstacleGrid[m-1][n-1] == 1) return 0;
 
-        vector<vector<int>> dp(m, vector<int> (n, 0));
-        dp[0][0] = 1;
+        // vector<vector<int>> dp(m, vector<int> (n, 0));
+        // dp[0][0] = 1;
+        vector<int> prev(n, 0);
+        bool flag = false;
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i==0 && j==0) continue;
-                dp[i][j] = ((i>=1 && obstacleGrid[i-1][j] == 0) ? dp[i-1][j] : 0) + ((j>=1 && obstacleGrid[i][j-1] == 0) ? dp[i][j-1] : 0);
+        for(int i=0; i<n; i++){
+            while(i < n && obstacleGrid[0][i] == 0 && flag == 0) {
+                prev[i] = 1;
+                i++;
             }
+            flag = true;
+            if(flag && i < n) prev[i] = 0;
         }
 
-        return dp[m-1][n-1];
+        for(int i=1; i<m; i++){
+
+            vector<int> curr(n, 0);
+            for(int j=0; j<n; j++){
+                curr[j] = ((i>=1 && obstacleGrid[i-1][j] == 0) ? prev[j] : 0) + ((j>=1 && obstacleGrid[i][j-1] == 0) ? curr[j-1] : 0);
+            }
+
+            prev = curr;
+        }
+
+        return prev[n-1];
 
         // return f(m-1, n-1, obstacleGrid, dp);
     }
