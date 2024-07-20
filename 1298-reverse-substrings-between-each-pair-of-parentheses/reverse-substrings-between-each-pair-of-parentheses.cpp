a335ab1fc64726a8e087bc0bf2c9b40115e62388
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-20 11:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (34.43%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<int> st;  // to store the length of the string in the answer to be reversed
-
-        string result="";
+        string result;
+        unordered_map<int, int> mp;
+        bool direction = 0; //  0 means LTR
+                            // 1 means RTL
+        stack<int> st;
 
         for(int i=0; i<s.size(); i++){
             if(s[i] == '('){
-                st.push(result.size());
-            }
+                st.push(i);
+            }   
 
             else if(s[i] == ')'){
-                int length = st.top();
+                int startIdx = st.top();
                 st.pop();
-                reverse(result.begin() + length, result.end());
+                mp[startIdx] = i;
+                mp[i] = startIdx;
+            }
+
+            else continue;
+        }
+
+
+        // for(auto it : mp){
+        //     cout<<it.first<<" "<<it.second<<" "<<endl;
+        // }
+
+        int i = 0;
+        while(i >= 0 && i < s.size()){
+            if(s[i] == '(' || s[i] == ')'){
+                int x = mp[i];
+                i = x;
+                direction = 1 - direction;
+                if(direction == 0){
+                    i++;
+                }
+                else{
+                    i--;
+                }
             }
 
             else{
-                result.push_back(s[i]);
+                if(direction == 0){
+                    result.push_back(s[i]);
+                    i++;
+                }
+                else{
+                    result.push_back(s[i]);
+                    i--;
+                }
             }
         }
 
         return result;
-
 
     }
 };
