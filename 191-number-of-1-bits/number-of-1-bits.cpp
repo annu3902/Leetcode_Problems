ddd00ac4ab682ce4306a07f2b316524ccdd4b829
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-21 13:03:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (14.66%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -2,8 +2,13 @@ class Solution {
 public:
     int hammingWeight(int n) {
         int count = 0;
-        for(int pos=0; pos<32; pos++){
+        /*for(int pos=0; pos<32; pos++){
             if((n & (1<<pos)) != 0) count++;
+        }*/
+
+        while(n){
+            count += (n%2);
+            n /= 2;
         }
         return count;
     }
