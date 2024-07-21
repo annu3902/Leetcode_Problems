7c7dbb12bb22ef8adfa8d42e32ce343fd8a129f5
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-21 12:48:20 +0530

    Time: 28 ms (100.00%) | Memory: 14.1 MB (33.33%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int n = s.size();
         int count = 0;
-        int sum = 0;
-        int flag = 0;
-        for(int i=0; i<n; i++){
-            if(s[i] == '1') {
-                flag = 1;
+        int ans = 0;
+        bool flag = false;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '1'){
+                flag = false;
                 count++;
             }
             else{
-                if(flag == 1)sum += count;
-                flag = 0;
+                if(!flag) ans += count;
+                flag = true;
             }
         }
-        return sum;
+
+        return ans;
     }
 };
