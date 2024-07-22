28a3bfb20cfc9f5508f1a31e24ba081a5829a319
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-23 02:15:28 +0530

    Time: 395 ms (68.05%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-Select author_id as id
+Select distinct author_id as id
 from Views
-Where author_id = viewer_id
-Union
-Select author_id
-from Views
-Where author_id = viewer_id
-order by id
+where author_id = viewer_id
+order by author_id
