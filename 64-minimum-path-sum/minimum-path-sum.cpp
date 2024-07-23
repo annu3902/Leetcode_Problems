835424d7dab60cdabc2b6d29f2276e6f90a387ff
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-23 19:40:23 +0530

    Time: 3 ms (95.31%) | Memory: 13.1 MB (5.70%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -3,18 +3,21 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return dfs(m-1, n-1, grid, dp);
+    }
 
-        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
+    int dfs(int row, int col, vector<vector<int>>& points, vector<vector<int>>& dp){
 
-        for(int i=m-1; i >= 0; i--){
-            for(int j = n-1; j >= 0; j--){
-                if(i+1 >= m || j+1 >= n){
-                    dp[i][j] = grid[i][j] + max(dp[i][j+1], dp[i+1][j]);
-                }
-                else
-                    dp[i][j] = grid[i][j] + min(dp[i][j+1], dp[i+1][j]);
-            }
-        }
-        return dp[0][0];
+        if(row == 0 && col == 0) return points[0][0];
+
+        else if(row < 0 || col < 0) return 1e9;
+
+        if(dp[row][col] != -1) return dp[row][col];
+
+        int up = dfs(row-1, col, points, dp);
+        int left = dfs(row, col-1, points, dp);
+
+        return dp[row][col] = points[row][col] + min(left, up);
     }
 };
