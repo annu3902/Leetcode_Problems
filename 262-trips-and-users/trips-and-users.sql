cdf23bc457beeed611f0b4b7e09ba50ce18e342a
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-23 12:20:05 +0530

    Time: 599 ms (35.86%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,24 +1,18 @@
 # Write your MySQL query statement below
-with temp as (Select users_id from Users
-            Where banned = 'No')
-            -- Created or extracted the required imformation and store it in temp table
-,temp2 as (   
-    Select request_at, status,
-    case when status in ('cancelled_by_client', 'cancelled_by_driver') then 1 
-    else 0 
-    end as flag
-    from Trips
-    Where client_id in(Select users_id from temp)
-    and driver_id in(Select users_id from temp)
+with t1 as (Select users_id from Users
+where banned = 'NO')
+
+, t2 as(Select status, request_at,
+Case when status in ('cancelled_by_driver','cancelled_by_client') then 1
+else 0
+end as flag
+from Trips
+where client_id in(Select users_id from t1)
+and driver_id in(Select users_id from t1)
 )
 
-Select request_at as Day, round((sum(flag)*1.0) / count(*), 2) as 'Cancellation Rate' 
-from temp2
-Where request_at >= '2013-10-01' and request_at <= '2013-10-03'
+Select request_at as Day,
+round(sum(flag)/count(*), 2)as 'Cancellation Rate'
+from t2
+Where request_at between '2013-10-01' and '2013-10-03'
 group by request_at
-
-
-
-
-
-
