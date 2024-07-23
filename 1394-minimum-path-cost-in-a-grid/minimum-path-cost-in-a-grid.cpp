f7078a8fc3a895150afa3c3eff7f9dff2965f760
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-24 00:36:16 +0530

    Time: 227 ms (38.33%) | Memory: 84.3 MB (17.03%) - LeetSync

--- 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
+++ 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
@@ -5,17 +5,42 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        // int mini = 1e9;
+        // for(int j=0; j<n; j++){
+        //     mini = min(mini, dfs(0, j, m, n, grid, moveCost, dp));
+        // }
+
+        // Tabulation
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for(int col=0; col<n; col++){
+            dp[m-1][col] = grid[m-1][col];
+        }
+
+        for(int i=m-2; i>=0; i--){
+            for(int j=0; j<n; j++){
+
+                int mini = 1e9;
+                int u = grid[i][j];
+                for(int k=0; k<n; k++){
+                    mini = min(u + moveCost[u][k] + dp[i+1][k], mini);
+                }
+
+                dp[i][j] = mini;
+            }
+        }
 
         int mini = 1e9;
         for(int j=0; j<n; j++){
-            mini = min(mini, dfs(0, j, m, n, grid, moveCost, dp));
+            mini = min(mini, dp[0][j]);
         }
 
         return mini;
     }
 
-    int dfs(int row, int col, int m, int n, vector<vector<int>>& grid, vector<vector<int>>& moveCost, vector<vector<int>>& dp){
+    // Memoization
+    /*int dfs(int row, int col, int m, int n, vector<vector<int>>& grid, vector<vector<int>>& moveCost, vector<vector<int>>& dp){
 
         if(row == m-1){
             return grid[row][col];
@@ -31,5 +56,5 @@ public:
         }
         
         return dp[row][col] = mini;
-    }
+    }*/
 };
