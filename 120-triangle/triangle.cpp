c5b4e99fee199d995817a1a3b8e7f42eb4447db1
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-24 16:43:54 +0530

    Time: 4 ms (73.24%) | Memory: 11.7 MB (15.86%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        vector<vector<int>> dp(m, vector<int> (triangle[m-1].size(), 1e9));
+        /*vector<vector<int>> dp(m, vector<int> (triangle[m-1].size(), 1e9));
 
+        // Tabulation or Real Dynamic Programming in which we build the solution from the base case that we already known.
         for(int j=0; j<triangle[m-1].size(); j++){
             dp[m-1][j] = triangle[m-1][j];
         }
@@ -17,7 +18,22 @@ public:
             }
         }
 
-        return dp[0][0];
+        return dp[0][0];*/
 
+
+        vector<vector<int>> dp(m, vector<int>(triangle.size(), -1));
+
+        return dfs(0, 0, triangle, dp);
+
+    }
+
+    int dfs(int row, int col, vector<vector<int>>& triangle, vector<vector<int>>& dp){
+        if(row == triangle.size()) return 0;
+        if(dp[row][col] != -1) return dp[row][col];
+
+        int first = dfs(row+1, col, triangle, dp);
+        int second = dfs(row+1, col+1, triangle, dp);
+
+        return dp[row][col] = min(first, second) + triangle[row][col];
     }
 };
