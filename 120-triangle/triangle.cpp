1c00e576bc0d4b3963b533a08b615b70f8104b04
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-24 16:37:06 +0530

    Time: 8 ms (27.38%) | Memory: 11.6 MB (22.49%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        if(n<2) return triangle[0][0];
-        vector<vector<int>> minSumdp(n,vector<int>(n,INT_MAX));
+        int m = triangle.size();
+        vector<vector<int>> dp(m, vector<int> (triangle[m-1].size(), 1e9));
 
-        // dp[k][i] -> minimum path sum till kth row and taking the ith index
-        minSumdp[0][0]=triangle[0][0];
+        for(int j=0; j<triangle[m-1].size(); j++){
+            dp[m-1][j] = triangle[m-1][j];
+        }
 
-        for(int k=1; k<n; ++k){
-            for(int i=0; i<=k; ++i){
-                int path1 = INT_MAX;
-                if(minSumdp[k-1][i] != INT_MAX)
-                path1=minSumdp[k-1][i];
-                else path1=minSumdp[k-1][i-1];
-                
-                int path2=INT_MAX;
-                if(i>=1)
-                    path2=minSumdp[k-1][i-1];
-                minSumdp[k][i] = min(path1, path2) + triangle[k][i];
+        for(int i=m-2; i>=0; i--){
+            for(int j=0; j<triangle[i].size(); j++){
+                int first = dp[i+1][j];
+                int second = dp[i+1][j+1];
+
+                dp[i][j] = min(first, second) + triangle[i][j];
             }
         }
 
-        int miniPathSum = minSumdp[n-1][0];
-        for(int x : minSumdp[n-1]){
-            miniPathSum = min(miniPathSum, x);
-        }
-        return miniPathSum;
+        return dp[0][0];
+
     }
 };
