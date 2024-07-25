0d85e2e45eaeff09bbd80a2c63a80684400024ec
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-25 06:54:16 +0530

    Time: 8 ms (33.15%) | Memory: 11.6 MB (22.40%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -4,9 +4,11 @@ public:
         int m=dungeon.size();
         int n=dungeon[0].size();
 
-        vector<vector<int>> dp(m,vector<int>(n, 0));
+        // Tabulation
+        // vector<vector<int>> dp(m,vector<int>(n, 0));
+        // 0 health is showing that i am easily sail at this value
 
-        for(int i=m-1; i>=0; i--){
+        /*for(int i=m-1; i>=0; i--){
             for(int j=n-1; j>=0; j--){
                 if(i == m-1 && j == n-1){
                     dp[i][j] = min(0, dungeon[i][j]);
@@ -26,6 +28,27 @@ public:
             }
         }
 
-        return abs(dp[0][0]) + 1;
+        return abs(dp[0][0]) + 1;*/
+
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+
+        return abs(dfs(0, 0, m, n, dungeon, dp)) + 1;
     }
+
+    int dfs(int row, int col, int m, int n, vector<vector<int>>& dungeon, vector<vector<int>>& dp){
+
+        if(row==m-1 && col==n-1){
+            return min(0, dungeon[row][col]);
+        }
+
+        else if(row >= m || col >= n){
+            return -1e9;
+        }
+
+        if(dp[row][col] != -1) return dp[row][col];
+
+        return dp[row][col] = min(0, dungeon[row][col] + max(dfs(row+1, col, m, n, dungeon, dp), dfs(row, col+1, m, n, dungeon, dp)));
+
+    }
+
 };
