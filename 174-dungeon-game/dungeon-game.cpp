19e1e217989cf573405d626d15e27e5a9f788959
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-25 07:15:53 +0530

    Time: 4 ms (78.40%) | Memory: 11.5 MB (30.02%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -5,20 +5,33 @@ public:
         int n=dungeon[0].size();
 
         // Tabulation
-        vector<vector<int>> dp(m,vector<int>(n, 0));
-        // 0 health is showing that i am easily sail at this value
-        dp[m-1][n-1] = min(0, dungeon[m-1][n-1]);
+        // vector<vector<int>> dp(m,vector<int>(n, 0));
 
-        for(int i=m-1; i>=0; i--){
-            for(int j=n-1; j>=0; j--){
-                if(i == m-1 && j == n-1) continue;
+        // Space Optimization
+        vector<int> prev(n, 0);
 
-                dp[i][j] = min(0, dungeon[i][j] + max(((j+1 < n) ? dp[i][j+1] : static_cast<int> (-1e9)), ((i+1 < m) ?dp[i+1][j] :static_cast<int> (-1e9))));
-
+        for(int j=n-1; j>=0; j--){
+            if(j == n-1) prev[n-1] = min(0, dungeon[m-1][j]);
+            else{
+                prev[j] = min(0, dungeon[m-1][j] + prev[j+1]);
             }
         }
 
-        return abs(dp[0][0]) + 1;
+        // dp[m-1][n-1] = min(0, dungeon[m-1][n-1]);
+
+        for(int i=m-2; i>=0; i--){
+
+            vector<int> curr(n, 0);
+            for(int j=n-1; j>=0; j--){
+
+                curr[j] = min(0, dungeon[i][j] + max(((j+1 < n) ? curr[j+1] : static_cast<int> (-1e9)), prev[j]));
+
+            }
+
+            prev = curr;
+        }
+
+        return abs(prev[0]) + 1;
 
         /*vector<vector<int>> dp(m, vector<int> (n, -1));
 
