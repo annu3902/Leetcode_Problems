7e6c102432fbffe737ad899f86d2566f0a52bace
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-07-25 21:24:15 +0530

    Time: 697 ms (11.93%) | Memory: 287.8 MB (7.20%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,50 +1,51 @@
 class Solution {
 public:
-    void merge(vector<int> &nums, int start, int mid, int end){
-        int left = start;
-        int right = mid + 1;
-        vector<int> temp;
-
-        while(left <= mid && right <= end){
-            if(nums[left] <= nums[right]){
-                temp.push_back(nums[left]);
-                left++;
-            }
-            else{
-                temp.push_back(nums[right]);
-                right++;
-            }
-        }
-
-        while(left <= mid){
-            temp.push_back(nums[left]);
-            left++;
-        }
-
-        while(right <= end){
-            temp.push_back(nums[right]);
-            right++;
-        }
-        for(int i = start; i <= end; i++){
-            nums[i] = temp[i - start];
-        }
-        temp.empty();
-    }
-
-    void mergeSort(vector<int> &nums, int start, int end){
-        // Base Case
-        if(start >= end) return;
-
-        // Processing
-        int mid = (start + end) / 2;
-        // nume.begin() , nums.begin() + mid
-        mergeSort(nums, start, mid);
-        mergeSort(nums, mid + 1, end);
-        merge(nums, start, mid, end);
-    }
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size();
-        mergeSort(nums, 0, n - 1);
+        dfs(0, n-1, nums);
         return nums;
+    }
+
+    void dfs(int start, int end, vector<int>& nums){
+        if(start >= end){
+            return;
+        }
+
+        int mid = (start + end)/2;
+        dfs(start, mid, nums);
+        dfs(mid+1, end, nums);
+        merge(start, mid, end, nums);
+    }
+
+    void merge(int start, int mid, int end, vector<int>& nums){
+        int i = start;
+        int j = mid+1;
+        vector<int> temp;
+
+        while(i <= mid && j <= end){
+            if(nums[i] <= nums[j]){
+                temp.push_back(nums[i]);
+                i++;
+            }
+            else if(nums[i] > nums[j]){
+                temp.push_back(nums[j]);
+                j++;
+            }
+        }
+
+        while(i<=mid){
+            temp.push_back(nums[i]);
+            i++;
+        }
+
+        while(j<=end){
+            temp.push_back(nums[j]);
+            j++;
+        }
+
+        for(int i=start; i<=end; i++){
+            nums[i] = temp[i-start];
+        }
+
     }
 };
