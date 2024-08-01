5c7327c250246cc4c16596a13af68779519820f4
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-08-01 15:51:33 +0530

    Time: 344 ms (10.03%) | Memory: 16.3 MB (68.56%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,43 +2,26 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
+        int total_sum = accumulate(begin(nums), end(nums), 0);
 
-        int sum = accumulate(begin(nums), end(nums), 0);
+        if(total_sum & 1 != 0) return false;
 
-        if((sum & 1) != 0) return false;
+        int sum = total_sum/2;
 
-        int target = sum/2;
-
-        vector<vector<bool>> dp(n, vector<bool> (target+1, 0));
+        vector<vector<bool>> dp(n, vector<bool> (sum+1, 0));
 
         for(int i=0; i<n; i++){
             dp[i][0] = true;
         }
 
-        if(target == nums[0]) dp[0][nums[0]] = true;
+        if(nums[0] <= sum) dp[0][nums[0]] = true;
 
         for(int i=1; i<n; i++){
-            for(int j=1; j<=target; j++){
-                dp[i][j] = dp[i-1][j] || ((j >= nums[i]) ? dp[i-1][j-nums[i]] : 0);
+            for(int j=1; j<=sum; j++){
+                dp[i][j] = dp[i-1][j] | ((nums[i] <= j) ? dp[i-1][j-nums[i]] : 0);
             }
         }
 
-        return dp[n-1][target];
+        return dp[n-1][sum];
     }
-
-    /*bool dfs(int i, int target, vector<int>& nums, vector<vector<int>>& dp){
-
-        if(target == 0) return true;
-
-        if(i == 0) return (target == nums[0]);
-
-        if(dp[i][target] != -1) return dp[i][target];
-
-        bool pick = false;
-        if(target >= nums[i]) pick = dfs(i-1, target-nums[i], nums, dp);
-
-        bool notPick = dfs(i-1, target, nums, dp);
-
-        return dp[i][target] = pick || notPick;
-    }*/
 };
