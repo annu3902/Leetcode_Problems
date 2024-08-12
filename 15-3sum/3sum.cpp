a2d17a6434da1be438343a168cf62ac28fb3fcc2
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-08-13 00:32:48 +0530

    Time: 56 ms (84.07%) | Memory: 27.2 MB (90.61%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,75 +1,38 @@
 class Solution {
 public:
-    // Brute Force
-    // vector<vector<int>> threeSum(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<vector<int>> ans;
-    //     vector<vector<int>>:: iterator it;
+   
 
-    //     for(int i = 0; i < n; i++){
-    //         int firstElement = nums[i];
-    //         for(int j = i + 1; j < n; j++){
-    //             int secondElement = nums[j];
-    //             int diff = -(firstElement + secondElement);
-    //             vector<int> vec;
-    //             for(int k = j + 1; k < n; k++){
-    //                 if(nums[k] == diff) {
-    //                     vec.push_back(firstElement);
-    //                     vec.push_back(secondElement);
-    //                     vec.push_back(diff);
-    //                     break;
-    //                 }
-    //             }
-    //             if(!vec.empty()){
-    //                 sort(vec.begin(), vec.end());
-    //                 it = find(ans.begin(), ans.end(), vec);
-    //                 if(it == ans.end()){
-    //                     ans.push_back(vec);
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return ans;
-
-    vector<vector<int>> threeSum(vector<int>& nums){
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        // unorder
         vector<vector<int>> ans;
-        // vector<vector<int>> :: iterator it;
+        // a + b + c = 0;
+        // b + c = -a;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(i!=0 && nums[i] == nums[i-1]) continue;
+            int target = -nums[i];
+            
+            int j = i+1;
+            int k = nums.size()-1;
 
-        // Base Case
-        if(n < 3) return ans;
-        
-        for(int i = 0; i < n - 2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue; // Skip Duplicates
-            int target  = -nums[i];
-            int left = i + 1;
-            int right = n - 1;
-
-            while(left < right){
-                if(left > i + 1 && nums[left] == nums[left - 1]){
-                    left++;
-                    continue;
-                } 
-                 
-                long long sum = nums[left] + nums[right];
-
-                vector<int> result;
-                if(sum < target) left++;
-                else if(sum > target) right--;
-                else{
-                    result.push_back(nums[i]);
-                    result.push_back(nums[left]);
-                    result.push_back(nums[right]);
-
-                    // it = find(ans.begin(), ans.end(), result);
-                    // if(it == ans.end()){
-                        ans.push_back(result);
-                    // }
-                    left++;
+            while(j<k){                
+                int sum = nums[j]+nums[k];
+                if(sum <= target){
+                    if(sum == target) ans.push_back({nums[i] , nums[j] , nums[k]});
+                    j++;
+                      while(j<k && nums[j] == nums[j-1]) j++;
+                    
+                }
+                else if(sum>target){
+                    k--;
+                    while(k>j && nums[k] == nums[k+1]) k--;
                 }
             }
+
         }
         return ans;
     }
 };
+
+// -4 -1 -1 0 1 2
