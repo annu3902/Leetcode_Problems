4ae8c20c256e304530d44b894c7ad3621f4aed9e
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-08-20 22:53:41 +0530

    Time: 55 ms (39.59%) | Memory: 12.1 MB (70.55%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int> suffixSum(n + 1, 0);
+        
+        // Calculate the suffix sums to efficiently calculate sum of piles from i to end.
+        for (int i = n - 1; i >= 0; --i) {
+            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        }
+        
+        // Bottom-up DP
+        for (int i = n - 1; i >= 0; --i) {
+            for (int M = 1; M <= n; ++M) {
+                for (int X = 1; X <= 2 * M && i + X <= n; ++X) {
+                    dp[i][M] = max(dp[i][M], suffixSum[i] - dp[i + X][max(M, X)]);
+                }
+            }
+        }
+        
+        return dp[0][1];
+    }
+};
