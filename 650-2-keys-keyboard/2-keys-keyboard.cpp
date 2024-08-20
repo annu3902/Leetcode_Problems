b6cb7540be8765e8a9d5c5e7246f3c90cc38e12f
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-08-20 22:41:04 +0530

    Time: 7 ms (50.37%) | Memory: 157.6 MB (8.65%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     int minSteps(int n) {
-        if(n == 1){
-            return 0;
-        }
+        if(n == 1) return 0;
 
-        int countChar = 1;
-        int steps = 0;
-        int temp = 0;
-        while(countChar < n){
-            if(n % countChar == 0){
-                steps++; // copy
-                temp = countChar;
+        // If n is prime then the number of operations required will be equal to n
+
+        // Seave of Eratostenes
+        vector<int> seive (n+1, true);
+        for(int i = 2; i<=n; i++){
+            if(i == true)
+            for(int j=2; i*j <= n; j++){
+                seive[i*j] = false;
             }
-            steps++; // paste
-            countChar = countChar + temp;
+
         }
 
-        return steps;
+        if(n == true) return n;
+
+        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+
+        return 1 + dfs(1, 1, n, dp);
     }
+
+    int dfs(int copiedChar, int currA, int n, vector<vector<int>>& dp){
+        if(currA > n) return 1e9;
+
+        if(currA == n) return 0;
+
+        if(dp[copiedChar][currA] != -1) return dp[copiedChar][currA];
+
+        //  copy then paste
+        int op1 = 2 + dfs(currA, currA + currA, n, dp);
+
+        // direct paste
+        int op2 = 1 + dfs(copiedChar, currA + copiedChar, n, dp);
+
+        return dp[copiedChar][currA] = min(op1, op2);
+    }
+
 };
