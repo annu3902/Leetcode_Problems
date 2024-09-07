b4d2607bd8e4560c1390b2a657e40bd40bf6e7ac
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-09-07 10:47:15 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (79.21%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int ind = -1;
+        int n = nums.size();
+
+        // Find the breakpoint
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                ind = i;
+                break;
+            }
+        }
+        // cout<<ind<<" ";
+
+        if(ind == -1){
+            reverse(begin(nums), end(nums));
+            return;
+        }
+
+        // Find the number just greater than the nums[ind]
+        for(int i=n-1; i>ind; i--){
+            if(nums[i] > nums[ind]){
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+
+        // arrange the remaining in the sorted order
+        reverse(nums.begin()+ind+1, nums.end());
     }
 };
