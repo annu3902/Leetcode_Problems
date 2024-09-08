93ca9c5f967008dbff8917fb72a63709476e7fcb
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-09-08 12:12:13 +0530

    Time: 367 ms (36.94%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -5,5 +5,5 @@ FROM Employee e1
 LEFT JOIN Employee e2
 ON e1.id = e2.managerID
 GROUP BY e1.id
-HAVING COUNT(e1.id) >= 5
--- HAVING COUNT(e2.managerID) >= 5
+-- HAVING COUNT(e1.id) >= 5
+HAVING COUNT(e2.managerID) >= 5
