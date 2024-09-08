2befaab1dd696adbf2277e0e83ef0915013aaa70
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-09-08 10:33:51 +0530

    Time: 101 ms (31.23%) | Memory: 93.2 MB (8.28%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long long n = chalk.size();
-        // Use long long for the sum to avoid overflow
-        long long s = accumulate(chalk.begin(), chalk.end(), 0LL); // Notice the 0LL to specify long long
-        int m = k % s;  // 'm' can remain an int since its value will be less than 's'
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (m - chalk[i] < 0) {
-                ans = i;
-                break;
-            } else {
-                m -= chalk[i]; // Subtract chalk[i] from m
+        int ans = -1;
+        int n = chalk.size();
+
+        vector<long long> prefixSum;
+        for(auto it : chalk){
+            prefixSum.push_back(it);
+        }
+        // prefixSum[0] = chalk[0];
+        for(int i=1; i<n; i++){
+            prefixSum[i] += prefixSum[i-1];
+        }
+
+        // Using Binary Search to find when the chalk will over
+        k %= prefixSum[n-1];
+        int low = 0;
+        int high = n-1;
+        // cout<<k<<" ";
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(prefixSum[mid] < k){
+                low = mid + 1;
+            }
+
+            else if(prefixSum[mid] > k){
+                high = mid - 1;
+            }
+
+            else{
+                return (mid + 1) % n;
             }
         }
-        return ans;
+        return low;
     }
 };
