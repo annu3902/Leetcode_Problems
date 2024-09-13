e8ce771d83a3123fdc0f265936970b029302578c
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-09-13 11:39:44 +0530

    Time: 41 ms (20.70%) | Memory: 26.2 MB (9.49%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-    int t[1001][1001];
-    int solve(string &text1, int i, int j, string& text2){
+    int dfs(int i, int j, string &text1, string &text2, vector<vector<int>> &dp){
 
-        if(i >= text1.size() && j >= text2.size()) return 0;
-        else if(i >= text1.size() && j < text2.size()) return 0;
-        else if(i < text1.size() && j >= text2.size()) return 0;
+        if(i < 0 || j < 0) return 0;
 
-        else if(t[i][j] != -1) return t[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
 
         if(text1[i] == text2[j]){
-            return 1 + solve(text1, i+1, j+1, text2);
+            return 1 + dfs(i-1, j-1, text1, text2, dp);
         }
-        
-        int op1 = solve(text1, i+1, j, text2);
-        int op2 = solve(text1, i, j+1, text2);
 
-        return t[i][j] = max(op1, op2);
+        return dp[i][j] = max(dfs(i-1, j, text1, text2, dp), dfs(i, j-1, text1, text2, dp));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
+        
         int m = text1.size();
         int n = text2.size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
 
-        memset(t, 0, sizeof(t));
-        for(int j=0; j<n; j++) t[0][j] = 0;
-        for(int i=0; i<m; i++) t[i][0] = 0;
-        // return solve(text1, 0, 0, text2);
-        // t[i][j] -> Length of longest common subsequence ending at index i,j
-        for(int i=1; i<=m; i++){
-
-            for(int j=1; j<=n; j++){
-                if(text1[i-1] == text2[j-1]){
-                    t[i][j] = 1 + t[i-1][j-1];
-                }
-                else{
-                    t[i][j] = max(t[i-1][j] , t[i][j-1]);
-                }
-            }
-
-        }
-
-        return t[m][n];
-
+        return dfs(m-1, n-1, text1, text2, dp);
     }
 };
