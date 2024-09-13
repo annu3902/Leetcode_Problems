b2a039b05db41fa9563e03264bce42dd14c37bc0
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-09-13 11:45:22 +0530

    Time: 24 ms (60.15%) | Memory: 25.8 MB (71.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -17,8 +17,23 @@ public:
         
         int m = text1.size();
         int n = text2.size();
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
 
-        return dfs(m-1, n-1, text1, text2, dp);
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+
+            }
+        }
+
+        return dp[m][n];
+        // return dfs(m-1, n-1, text1, text2, dp);
     }
 };
