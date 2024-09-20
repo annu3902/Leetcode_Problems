9dade2e66768802e6bf7b93ec1911129c1943a64
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2024-09-20 19:23:05 +0530

    Time: 3 ms (56.60%) | Memory: 13 MB (5.32%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,56 +1,24 @@
-// class Solution {
-// public:
-//     int findMin(vector<int>& nums) {
-//         set<int> st(nums.begin(),nums.end());
-//         // st.insert(nums.begin(), nums.end());
-//         auto it = st.begin();
-//         return *it;
-//     }
-// };
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
 
-class Solution{
-    public:
-    int a;
-    int func (vector<int>& nums, int low, int high, int mini){
-        if(low > high){
-            // return;
-            // a = mini;
-            return mini;
+        int left = 0;
+        int right = n-1;
+
+        while(left < right){
+
+            int mid = left + (right - left)/2;
+
+            if(nums[mid] > nums[right]){
+                left = mid + 1;
+            }
+
+            else{
+                right = mid;
+            }
         }
 
-        int mid = low + (high-low)/2;
-        if(nums[low] <= nums[mid]){
-            mini = min(mini,nums[low]);
-           return func(nums,low+1,high,mini);
-        }
-        else{
-            mini = min(mini, nums[mid]);
-            high = mid -1;
-            return func(nums,low,mid-1,mini);
-        }
-        // return mini;
-    }
-
-    int findMin(vector<int>& nums){
-        int n =nums.size();
-        // int low = 0;
-        // int high = n-1;
-        // int mini =INT_MAX;
-        // while(low <= high){
-        //     int mid = (low+high)/2;
-        //     // Left Sorted
-        //     if(nums[low] <= nums[mid]){
-        //         mini = min(mini, nums[low]);
-        //         low = mid+1;
-        //     }
-
-        //     else{
-        //         mini = min(mini,nums[mid]);
-        //         high = mid -1;
-        //     }
-        // }
-        // return mini;
-        // int mn = func()
-        return func(nums,0,n-1,INT_MAX);
+        return nums[left];
     }
 };
