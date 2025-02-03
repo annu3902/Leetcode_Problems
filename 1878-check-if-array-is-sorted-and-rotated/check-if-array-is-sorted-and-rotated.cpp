90f508d7cbb232bd3a11913a10bcfa978859571c
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2025-02-04 01:14:14 +0530

    Time: 2 ms (3.81%) | Memory: 11.8 MB (6.01%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -3,28 +3,20 @@ public:
     bool check(vector<int>& nums) {
         vector<int> temp;
         temp=nums;
-        
         sort(temp.begin(),temp.end());
-        // auto maxInd = max_element(nums.begin(),nums.end());
-        // auto minInd = min_element(nums.begin(),nums.end());
-
-        // int maxi = *max_element(nums.begin(),nums.end());
-        // int mini = *min_element(nums.begin(),nums.end());
-
         int n=nums.size();
         if(temp!=nums)
         {
             int largest = temp.size()-1;
             int smallest = 0;
             int x;
-            
             for(int i=0; i<n; i++)
             {
                 if(nums[i] == temp[largest] && i!=n-1)
                 {
                     if(nums[i+1]==temp[smallest])
                     {
-                        x=i+1;  // Shows the pivot index where the rotation occurs
+                        x=i+1;
                     }
                 }
             }
@@ -48,6 +40,4 @@ public:
         }
     }
 };
-
-
 
