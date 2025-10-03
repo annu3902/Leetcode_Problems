c0c416cdc0906c114486feb971947fb20de42af9
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2025-10-04 01:25:18 +0530

    Time: 3 ms (6.97%) | Memory: 9.7 MB (30.99%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,17 +3,17 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
 
-        for(int i=1; i<=numRows; i++){
-            vector<int> res(i, 1);
-            ans.push_back(res);
+        for(int i=0; i<numRows; i++){
 
-            for(int j=1; j<i-1; j++){
-                ans[i-1][j] = ans[i-2][j-1] + ans[i-2][j];
-                // ans[i][j] = ((i>=1) ? ans[i-1][j] : 0) + ((i>=1 && j <= i) ? ans[i-1][j+1] : 0);
+            vector<int> res(i+1, 1);
+            for(int j=1; j<i; j++){
+ 
+                res[j] = ans[i-1][j-1] + ans[i-1][j];
+
             }
-
+            ans.push_back(res);
         }
-        
+
         return ans;
     }
 };
