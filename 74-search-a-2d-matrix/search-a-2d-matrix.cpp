a1dc907261a4d7e78bab9ecb8a09d1baef3d3fe6
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2025-10-10 01:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (76.29%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,62 +1,24 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& nums, int num){
+    bool searchMatrix(vector<vector<int>>& nums, int target) {
         int n = nums.size();
-        for(int n : nums)cout<<n<<" ";
+        int m = nums[0].size();
 
         int left = 0;
-        int right = n-1;
-        // int mid=-1;
+        int right = n*m-1;
 
         while(left <= right){
-            int mid = (left + right)/2;
 
-            if(nums[mid] == num) return true;
-            else if(num > nums[mid]){
-                left = mid+1;
-            }
-            else{
-                right = mid-1;
-            }
+            int mid = (left + right)/2;
+            int row = mid/m;
+            int col = mid%m;
+
+            if(nums[row][col] == target) return true;
+            else if(nums[row][col] < target) left = mid+1;
+            else right = mid-1;
+
         }
 
         return false;
-
-    }
-
-    bool searchMatrix(vector<vector<int>>& nums, int target) {
-        int n=nums.size();
-        int m=nums[0].size();
-
-        int top = 0;
-        int bottom = n-1;
-        int mid;
-        bool flag = false;
-
-        while(top <= bottom){
-            mid = (top + bottom)/2;
-
-            if(mid == top && target > nums[mid][m-1]){
-                mid = bottom;
-                break;
-                // break;
-            }
-
-            if(target > nums[mid][0]){
-                top = mid;
-                if(target <= nums[mid][m-1]) break;
-            }
-
-            else if(target < nums[mid][0]){
-                bottom = mid-1;
-            }
-
-            else return true;
-        }
-
-        // return binarySearch(nums[mid], target) | ((mid+1 < n) ? binarySearch(nums[bottom], target) : 0);
-
-        return binarySearch(nums[mid], target);
-
     }
 };
