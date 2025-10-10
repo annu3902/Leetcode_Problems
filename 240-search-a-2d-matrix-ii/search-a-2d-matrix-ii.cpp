3a4701847f1d8b9965e3e6be5fa8f13b5dbac500
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2025-10-10 15:52:49 +0530

    Time: 48 ms (78.80%) | Memory: 18.6 MB (68.00%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,50 +1,26 @@
 class Solution {
-private:
-    bool binarySearch(vector<int>& nums, int num){
-        int n = nums.size();
-        // for(int n : nums)cout<<n<<" ";
-
-        int left = 0;
-        int right = n-1;
-        // int mid=-1;
-
-        while(left <= right){
-            int mid = (left + right)/2;
-
-            if(nums[mid] == num) return true;
-            else if(num > nums[mid]){
-                left = mid+1;
-            }
-            else{
-                right = mid-1;
-            }
-        }
-
-        return false;
-
-    }
 public:
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
 
-        // int left = 0;
-        // int right = 0;
-        // int top = 0;
-        // int bottom = n-1;
-        // int rowMid, colMid;
-        // int left = 0;
-        // int right = m-1;
+        int row = 0;
+        int col = m-1;
 
-        // while(top <= bottom){
+        while(row <= n-1 && col >= 0){
+            if(matrix[row][col] > target){
+                col--;
+            }
 
-        // }
-        for(int i=0; i<n; i++){
-            if(binarySearch(matrix[i], target)) return true;
+            else if(target > matrix[row][col]){
+                row++;
+            }
+
+            else{
+                return true;
+            }
         }
 
         return false;
-
     }
 };
