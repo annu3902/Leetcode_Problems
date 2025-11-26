0b1e7bdf4108a64a12615c8ef95da071f44e2b69
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2025-11-26 21:30:31 +0530

    Time: 31 ms (31.23%) | Memory: 23.7 MB (26.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+vector<vector<int>> neighbours{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited){
         visited[i][j] = true;
 
@@ -19,6 +20,44 @@ public:
         return;
     }
 
+    void bfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited){
+        visited[i][j] = true;
+        queue<pair<int, int>> q;
+        q.push({i, j});
+
+        
+        while(!q.empty()){
+            int size = q.size();
+
+            while(size--){
+                pair<int, int> p = q.front();
+                int u = p.first;
+                int v = p.second;
+                
+                
+                q.pop();
+                for(auto& neighbour: neighbours){
+                    int x = u + neighbour[0];
+                    int y = v + neighbour[1];
+
+            // Boundry Check
+                if(x < 0 || y < 0 || x >= grid.size() || y >= grid[0].size()){
+                    continue;
+                }
+
+                else if(visited[x][y] == 0 && grid[x][y] == '1'){
+                    q.push({x, y});
+                    visited[x][y] = true;
+                    
+                }
+            }
+            }
+ 
+        }
+
+        return;
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -29,7 +68,7 @@ public:
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] == '1' && !visited[i][j]){
-                    dfs(grid, i, j, visited);
+                    bfs(grid, i, j, visited);
                     count++;
                 }
             }
