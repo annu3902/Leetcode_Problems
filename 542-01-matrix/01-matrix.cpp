acde11a0b205fb2fc8a34f6d216bc2a2cfdd87f5
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2025-12-28 21:13:38 +0530

    Time: 22 ms (44.38%) | Memory: 38 MB (47.54%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,44 +3,45 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-
-        vector<vector<int>> distance(m, vector<int>(n, 0));
-        vector<vector<bool>> visited(m, vector<bool>(n, 0));
+        vector<vector<int>> distance(m, vector<int>(n , -1));
+        vector<vector<bool>> visited(m, vector<bool> (n, false));
 
         queue<pair<pair<int, int>, int>> q;
-
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    q.push({{i, j}, 0});
+                    distance[i][j] = 0;
                     visited[i][j] = true;
+                    q.push({{i, j}, 0});
                 }
             }
         }
 
-        vector<int> vicinity = {-1, 0, 1, 0, -1};
+        vector<int> neighbour = {-1, 0, 1, 0, -1};
 
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int dist = q.front().second;
+            int x = q.front().first.first;
+            int y = q.front().first.second;
 
-            visited[row][col] = true;
+            int dist = q.front().second;
             q.pop();
 
             for(int i=0; i<4; i++){
-                int x = row + vicinity[i];
-                int y = col + vicinity[i+1];
+                int nx = x + neighbour[i];
+                int ny = y + neighbour[i+1];
 
-                if(x<0 || y<0 || x>=m || y>=n) continue;
-
-                else if(visited[x][y] == false){
-                    visited[x][y] = true;
-                    q.push({{x,y}, dist+1});
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+                else{
+                    if(distance[nx][ny] == -1){
+                        visited[nx][ny] = true;
+                        distance[nx][ny] = dist+1;
+                        q.push({{nx, ny}, dist+1});
+                    }
+                    else if(visited[nx][ny] == true){
+                        distance[nx][ny] = min(distance[nx][ny], dist+1);
+                    }
                 }
             }
-            distance[row][col] = dist;
-
         }
 
         return distance;
