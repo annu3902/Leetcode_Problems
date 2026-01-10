f8dc6dc9aa59809ac32ed9a5b8549a4798d54bb3
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-10 18:56:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (50.72%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,23 +1,36 @@
 class Solution {
+private:
+    int dfs(int m, int n, vector<vector<int>>& dp){
+
+        if(m == 0 || n == 0) return dp[m][n] = 1;
+
+        if(dp[m][n] != -1) return dp[m][n];
+
+        dp[m][n] = ((m>=1) ? dfs(m-1, n, dp) : 0) + ((n>=1) ? dfs(m, n-1, dp) : 0);
+
+        return dp[m][n];
+    }
+
 public:
     int uniquePaths(int m, int n) {
         // build-up
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return dfs(m-1, n-1, dp);
 
-        for(int i=0; i<m; i++){
-            dp[i][0] = 1;
-        }
+        // for(int i=0; i<m; i++){
+        //     dp[i][0] = 1;
+        // }
 
-        for(int j=0; j<n; j++){
-            dp[0][j] = 1;
-        }
+        // for(int j=0; j<n; j++){
+        //     dp[0][j] = 1;
+        // }
 
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-            }
-        }
+        // for(int i=1; i<m; i++){
+        //     for(int j=1; j<n; j++){
+        //         dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        //     }
+        // }
 
-        return dp[m-1][n-1];
+        // return dp[m-1][n-1];
     }
 };
