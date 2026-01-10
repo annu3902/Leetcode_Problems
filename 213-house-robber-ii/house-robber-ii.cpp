f8cdcc7a7202e08f65e16c19927811f19784271e
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-10 18:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (18.72%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,22 +1,37 @@
 class Solution {
+private:
+    int dfs(int node, vector<int>& nums, vector<int>& dp){
+        if(node == 0) return dp[node] = nums[0];
+
+        if(dp[node] != -1) return dp[node];
+
+        int op1 = nums[node] + ((node >= 2) ? dfs(node-2, nums, dp) : 0);
+        int op2 = (node >= 1) ? dfs(node-1, nums, dp) : 0;
+
+        dp[node] = max(op1, op2);
+        return dp[node];
+    }
+
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
-        return max(dfs(0, n-2, nums), dfs(1, n-1, nums));
-    }
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
 
-    int dfs(int start, int end, vector<int>& nums){
-        int n = nums.size();
-        vector<int> dp(n-1, 0);
+        vector<int> nums1;
+        vector<int> nums2;
 
-        dp[0] = nums[start];
-
-        for(int i=start+1; i<=end; i++){
-            int op1 = dp[i-1-start];
-            int op2 = ((i-2 >= start) ? dp[i-2-start] : 0) + nums[i];
-            dp[i - start] = max(op1, op2);
+        for(int i=0; i<nums.size()-1; i++){
+            nums1.push_back(nums[i]);
+            nums2.push_back(nums[i+1]);
         }
-        return dp[n-2];
+
+        vector<int> dp1(n-1, -1);
+        vector<int> dp2(n-1, -1);
+
+        dfs(n-2, nums1, dp1);
+        dfs(n-2, nums2, dp2);
+
+        return max(dp1[n-2], dp2[n-2]);
     }
 };
