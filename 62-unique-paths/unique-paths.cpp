9250dba88b67792ce02585d908f342ccf463c686
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-10 18:50:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (27.63%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, 0));
-        vector<int> prev(n, 1);
-        // dp[0][0] = 1;
+        // build-up
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        for(int i=1; i<m; i++){
-            vector<int> curr(n, 0);
-            for(int j=0; j<n; j++){
-                // if(i == 0 && j == 0) continue;
-                curr[j] = ((i>=1) ? prev[j] : 0) + ((j>=1) ? curr[j-1] : 0);
-            }
-            prev = curr;
+        for(int i=0; i<m; i++){
+            dp[i][0] = 1;
         }
 
-        return prev[n-1];
-    }
+        for(int j=0; j<n; j++){
+            dp[0][j] = 1;
+        }
 
-    int totalWays(int i, int j, vector<vector<int>>& dp){
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
 
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int up, left = 0;
-        if(i >= 0) up = totalWays(i-1, j, dp);
-        if(j >= 0) left = totalWays(i, j-1, dp);
-
-        return dp[i][j] = up + left;
+        return dp[m-1][n-1];
     }
 };
