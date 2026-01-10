a751655c28bdfe65cb35a826aa4733ba4543d4f3
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-10 17:18:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (61.35%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,4 +1,15 @@
 class Solution {
+private:
+    int dfs(int node, vector<int>& nums, vector<int>& dp){
+        if(node == 0) return nums[node];
+
+        if(dp[node] != -1) return dp[node];
+
+        dp[node] = max(nums[node] + ((node >= 2) ? dfs(node-2, nums, dp) : 0) , ((node >= 1) ? dfs(node-1, nums, dp) : 0));
+
+        return dp[node];
+    }
+
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -6,12 +17,13 @@ public:
 
         // dp[i] ->max money till ith house
         vector<int> dp(n, -1);
-        dp[0] = nums[0];
-        dp[1] = max(dp[0], nums[1]);
+        dfs(n-1, nums, dp);
+        // dp[0] = nums[0];
+        // dp[1] = max(dp[0], nums[1]);
 
-        for(int i=2; i<n; i++){
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
-        }
+        // for(int i=2; i<n; i++){
+        //     dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+        // }
 
         return dp[n-1];
     }
