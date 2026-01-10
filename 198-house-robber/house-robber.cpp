1f9edfd0f39c9b6bf93bc0936966a97cbf9c3399
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-10 17:09:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (20.96%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    int dfs(int index, vector<int>& nums, vector<int>& dp){
-
-        if(index < 0) return 0;
-        if(dp[index] != -1) return dp[index];
-
-        int pick = nums[index] + ((index >= 2) ? dfs(index - 2, nums, dp) : 0);
-        int skip = dfs(index - 1, nums, dp);
-
-        return dp[index] = max(pick, skip);
-    }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 0);
+        if(n == 1) return nums[0];
 
+        // dp[i] ->max money till ith house
+        vector<int> dp(n, -1);
         dp[0] = nums[0];
+        dp[1] = max(dp[0], nums[1]);
 
-        for(int i=1; i<n; i++){
-            int op1 = dp[i-1];
-            int op2 = nums[i] + ((i >= 2) ? dp[i-2] : 0);
-            dp[i] = max(op1, op2);
+        for(int i=2; i<n; i++){
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
         }
 
         return dp[n-1];
