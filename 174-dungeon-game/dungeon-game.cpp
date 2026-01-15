dfc392797643dc37204b48efcdc6115000c83939
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-15 21:30:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (46.62%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,57 +1,36 @@
 class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        int m=dungeon.size();
-        int n=dungeon[0].size();
+        // dp[i][j] -> min health requ. to survive at (i, j);
+        int m = dungeon.size();
+        int n = dungeon[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 1e8));
 
-        // Tabulation
-        // vector<vector<int>> dp(m,vector<int>(n, 0));
 
-        // Space Optimization
-        vector<int> prev(n, 0);
-
-        for(int j=n-1; j>=0; j--){
-            if(j == n-1) prev[n-1] = min(0, dungeon[m-1][j]);
-            else{
-                prev[j] = min(0, dungeon[m-1][j] + prev[j+1]);
-            }
-        }
-
-        // dp[m-1][n-1] = min(0, dungeon[m-1][n-1]);
-
-        for(int i=m-2; i>=0; i--){
-
-            vector<int> curr(n, 0);
+        for(int i=m-1; i>=0; i--){
             for(int j=n-1; j>=0; j--){
+                if(i == m-1 && j == n-1){
+                    if(dungeon[m-1][n-1] <= 0){
+                        dp[m-1][n-1] = abs(dungeon[m-1][n-1]) + 1;
+                    }else{
+                        dp[m-1][n-1] = 1;
+                    }
+                }
+                else{
+                    int op1 = (i+1 <= m-1) ? dp[i+1][j] : 1e8;
+                    int op2 = (j+1 <= n-1) ? dp[i][j+1] : 1e8;
+                    int minHealth = min(op1, op2);
 
-                curr[j] = min(0, dungeon[i][j] + max(((j+1 < n) ? curr[j+1] : static_cast<int> (-1e9)), prev[j]));
-
+                    int requiredHealth = minHealth - dungeon[i][j];
+                    if(requiredHealth <= 0){
+                        dp[i][j] = 1;
+                    }else{
+                        dp[i][j] = requiredHealth;
+                    }
+                }                
             }
-
-            prev = curr;
         }
-
-        return abs(prev[0]) + 1;
-
-        /*vector<vector<int>> dp(m, vector<int> (n, -1));
-
-        return abs(dfs(0, 0, m, n, dungeon, dp)) + 1;*/
-    }
-
-    int dfs(int row, int col, int m, int n, vector<vector<int>>& dungeon, vector<vector<int>>& dp){
-
-        if(row==m-1 && col==n-1){
-            return min(0, dungeon[row][col]);
-        }
-
-        else if(row >= m || col >= n){
-            return -1e9;
-        }
-
-        if(dp[row][col] != -1) return dp[row][col];
-
-        return dp[row][col] = min(0, dungeon[row][col] + max(dfs(row+1, col, m, n, dungeon, dp), dfs(row, col+1, m, n, dungeon, dp)));
+        return dp[0][0];
 
     }
-
 };
