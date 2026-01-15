e4fcb719a3b2f3b73d4c493f6278b0cb8b9e281a
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-15 13:36:35 +0530

    Time: 4 ms (6.82%) | Memory: 14 MB (7.58%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,39 +1,29 @@
 class Solution {
+private:
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        /*vector<vector<int>> dp(m, vector<int> (triangle[m-1].size(), 1e9));
+        
+        vector<vector<int>> dp;
+        for(int i=0; i<m; i++){
+            vector<int> temp;
+            for(int j=0; j<triangle[i].size(); j++){
+                temp.push_back(0);
+            }
+            dp.push_back(temp);
+        }
 
-        // Tabulation or Real Dynamic Programming in which we build the solution from the base case that we already known.
-        for(int j=0; j<triangle[m-1].size(); j++){
-            dp[m-1][j] = triangle[m-1][j];
+        for(int i=0; i<triangle[m-1].size(); i++){
+            dp[m-1][i] = triangle[m-1][i];
         }
 
         for(int i=m-2; i>=0; i--){
             for(int j=0; j<triangle[i].size(); j++){
-                int first = dp[i+1][j];
-                int second = dp[i+1][j+1];
-
-                dp[i][j] = min(first, second) + triangle[i][j];
+                dp[i][j] = min(dp[i+1][j] , dp[i+1][j+1]) + triangle[i][j];
             }
         }
+        return dp[0][0];
 
-        return dp[0][0];*/
-
-
-        vector<vector<int>> dp(m, vector<int>(triangle.size(), -1));
-
-        return dfs(0, 0, triangle, dp);
-
-    }
-
-    int dfs(int row, int col, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        if(row == triangle.size()) return 0;
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int first = dfs(row+1, col, triangle, dp);
-        int second = dfs(row+1, col+1, triangle, dp);
-
-        return dp[row][col] = min(first, second) + triangle[row][col];
     }
 };
