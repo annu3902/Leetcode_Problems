09bd26f3d7195b0303ba2c5f92ab17295880e435
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-15 12:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (69.54%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,10 +1,15 @@
 class Solution {
 private:
-    // int dfs(int x, int y, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+    long long dfs(int x, int y, vector<vector<int>>& obstacleGrid, vector<vector<long long>>& dp){
+        if(x >= obstacleGrid.size() || y >= obstacleGrid[0].size()) return 0;
+        if(obstacleGrid[x][y] == 1) return dp[x][y] = 0;
+        if(x == obstacleGrid.size()-1 && y == obstacleGrid[0].size()-1 && obstacleGrid[x][y] == 0) return dp[x][y] = 1;
 
+        if(dp[x][y] != -1) return dp[x][y];
 
-
-    // }
+        dp[x][y] = dfs(x+1, y, obstacleGrid, dp) + dfs(x, y+1, obstacleGrid, dp);
+        return dp[x][y];
+    }
 
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
@@ -12,24 +17,8 @@ public:
         int n = obstacleGrid[0].size();
         // build up
         // dp[x][y] -> No. of ways to reach to (m-1, n-1).
-        vector<vector<long long>> dp(m, vector<long long>(n, 0));
-        // if(obstacleGrid[m-1][n-1] == 0) dp[m-1][n-1] = 1;
+        vector<vector<long long>> dp(m, vector<long long>(n, -1));
 
-        for(int i=m-1; i>=0 && obstacleGrid[i][n-1] == 0; i--){
-            dp[i][n-1] = 1;
-        }
-
-        for(int j=n-1; j>=0 && obstacleGrid[m-1][j] == 0; j--){
-            dp[m-1][j] = 1;
-        }
-
-        for(int i=m-2; i>=0; i--){
-            for(int j=n-2; j>=0; j--){
-                if(obstacleGrid[i][j] == 0){
-                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
-                }
-            }
-        }
-        return dp[0][0];
+        return dfs(0, 0, obstacleGrid, dp);
     }
 };
