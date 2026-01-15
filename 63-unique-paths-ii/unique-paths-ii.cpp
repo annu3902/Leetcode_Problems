6d6d1f8dfc79d25e12cd49b924597d684088d66e
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-15 12:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.43%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,52 +1,35 @@
 class Solution {
+private:
+    // int dfs(int x, int y, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+
+
+
+    // }
+
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
+        // build up
+        // dp[x][y] -> No. of ways to reach to (m-1, n-1).
+        vector<vector<long long>> dp(m, vector<long long>(n, 0));
+        // if(obstacleGrid[m-1][n-1] == 0) dp[m-1][n-1] = 1;
 
-        if(obstacleGrid[m-1][n-1] == 1) return 0;
-
-        // vector<vector<int>> dp(m, vector<int> (n, 0));
-        // dp[0][0] = 1;
-        vector<int> prev(n, 0);
-        bool flag = false;
-
-        for(int i=0; i<n; i++){
-            while(i < n && obstacleGrid[0][i] == 0 && flag == 0) {
-                prev[i] = 1;
-                i++;
-            }
-            flag = true;
-            if(flag && i < n) prev[i] = 0;
+        for(int i=m-1; i>=0 && obstacleGrid[i][n-1] == 0; i--){
+            dp[i][n-1] = 1;
         }
 
-        for(int i=1; i<m; i++){
-
-            vector<int> curr(n, 0);
-            for(int j=0; j<n; j++){
-                curr[j] = ((i>=1 && obstacleGrid[i-1][j] == 0) ? prev[j] : 0) + ((j>=1 && obstacleGrid[i][j-1] == 0) ? curr[j-1] : 0);
-            }
-
-            prev = curr;
+        for(int j=n-1; j>=0 && obstacleGrid[m-1][j] == 0; j--){
+            dp[m-1][j] = 1;
         }
 
-        return prev[n-1];
-
-        // return f(m-1, n-1, obstacleGrid, dp);
+        for(int i=m-2; i>=0; i--){
+            for(int j=n-2; j>=0; j--){
+                if(obstacleGrid[i][j] == 0){
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+                }
+            }
+        }
+        return dp[0][0];
     }
-
-    int f(int i, int j, vector<vector<int>> &obstacleGrid, vector<vector<int>>& dp){
-        if(i == 0 && j == 0) return dp[0][0] = 1;
-
-        if(i < 0 || j < 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = 0;
-        int up = 0;
-
-        if(i >= 1 && obstacleGrid[i-1][j] == 0) up = f(i-1, j, obstacleGrid, dp);
-        if(j >= 1 && obstacleGrid[i][j-1] == 0) left = f(i, j-1, obstacleGrid, dp);
-
-        return dp[i][j] = up + left;
-    }   
 };
