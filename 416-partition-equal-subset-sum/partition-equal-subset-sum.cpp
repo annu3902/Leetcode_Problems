dad09b83a47e1cf75e0b8ceec239e6b8092e2f80
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-17 12:01:10 +0530

    Time: 257 ms (6.28%) | Memory: 20.9 MB (51.64%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -15,9 +15,25 @@ bool dfs(int index, int sum, int totalSum, vector<int>& nums, vector<vector<int>
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        int totalSum = accumulate(begin(nums), end(nums), 0);
+        int sum = accumulate(begin(nums), end(nums), 0);
 
-        vector<vector<int>> dp(n, vector<int>(totalSum+1, -1));
-        return dfs(n-1, 0, totalSum, nums, dp);
+        if(sum & 1 == 1) return 0;
+
+        vector<vector<bool>> dp(n, vector<bool>(sum + 1, 0));
+        for(int i=0; i<n; i++){
+            dp[i][0] = true;
+        }
+
+        dp[0][nums[0]] = true;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=sum; j++){
+                bool pick = (j >= nums[i]) ? dp[i-1][j - nums[i]] : 0;
+                bool notPick = dp[i-1][j];
+
+                dp[i][j] = pick || notPick;
+            }
+        }
+
+        return dp[n-1][sum/2];
     }
 };
