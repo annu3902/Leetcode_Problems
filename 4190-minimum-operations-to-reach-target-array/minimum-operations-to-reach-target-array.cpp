86b295c6ae2162b00b3e4cfae99aed0d4a789713
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-18 10:39:42 +0530

    Time: 91 ms (75.00%) | Memory: 257.4 MB (33.33%) - LeetSync

---
+++ 4190-minimum-operations-to-reach-target-array/minimum-operations-to-reach-target-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& target) {
+        int n = nums.size();
+
+        int cnt = 0;
+        unordered_set<int> st;
+        for(int i=0; i<n; i++){
+            if(nums[i] != target[i]){
+                st.insert(nums[i]);
+            }
+        }
+
+        return st.size();
+    }
+};
