a081e1c7a60174198cb2eb83cb4997f204fb5bc8
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-18 14:41:46 +0530

    Time: 78 ms (33.33%) | Memory: 161.4 MB (41.67%) - LeetSync

---
+++ 4143-number-of-alternating-xor-partitions/number-of-alternating-xor-partitions.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long mod = 1e9+7;
+    int alternatingXOR(vector<int>& nums, int t1, int t2){
+        int n = nums.size();
+
+        long long px=0;
+        // dp1[i] -> No of valid partitions ending at index i having last block as t1;
+        // dp2[i] -> No of valid parititions ending at index i having last block as t2
+        vector<long long> dp1(n, 0);
+        vector<long long> dp2(n, 0);
+        unordered_map<long long, long long> cnt1;
+        unordered_map<long long, long long> cnt2;
+
+        for(int i=0; i<n; i++){
+
+            px = px^nums[i];
+
+            if(px == t1){
+                dp1[i] = 1;
+            }
+            dp1[i] = (dp1[i] + cnt2[px ^ t1]) % mod;
+            dp2[i] = (dp2[i] + cnt1[px ^ t2]) % mod;
+
+            cnt1[px] = (cnt1[px] + dp1[i]) % mod;
+            cnt2[px] = (cnt2[px] + dp2[i]) % mod;
+        }
+        return (dp1[n-1] + dp2[n-1]) % mod;
+    }
+
+    
+
+
+};
