33016ff3e3708af751ebb79136b033f5f4d4e96b
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-01-25 00:12:38 +0530

    Time: 49 ms (26.65%) | Memory: 48.9 MB (37.79%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,47 +1,30 @@
 class Solution {
-private:
-// Dp Memoization
-    int f(int amount, vector<int> &coins, vector<vector<int>> &dp,int index){
-        int n = coins.size();
-       
-        if(amount == 0) return 0;
-        if(index>=coins.size()) return 1e9;
-
-        if(dp[index][amount] != -1) return dp[index][amount];
-
-        // int pick = INT_MAX;
-        int pick = 1e9;
-        if(amount>=coins[index]) pick = 1 + f(amount - coins[index] , coins , dp,index);
-        int notpick = f(amount , coins , dp , index+1);
-        int mini = min(pick, notpick);
-        
-        return dp[index][amount] = min(pick , notpick);
-    }
-
-// Dp Tabulation
-    // int fTab(vector<int> &coins, int amount){
-    //     int n = coins.size();
-    //     vector<int> dp(amount + 1, INT_MAX);
-    //     dp[0] = 0;
-
-    //     for(int i = 1; i < dp.size(); i++){
-    //         for(int x: coins){
-    //             if(i - x >= 0 && dp[i - x] != INT_MAX)
-    //             dp[i] = min(dp[i - x], dp[i]);
-    //         }
-    //         if(dp[i] != INT_MAX){
-    //             dp[i] += 1;
-    //         }
-    //     }
-
-    //     return dp[amount];
-    // }
-
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1,-1));
-        int ans = f(amount, coins ,dp, 0);
-        if(ans == 1e9) return -1;
-        return ans;
+        int n = coins.size();
+        sort(begin(coins), end(coins));
+        reverse(begin(coins), end(coins));
+
+        vector<vector<int>> dp(n, vector<int>(amount+1, 1e9));
+
+        for(int i=0; i<n; i++){
+            dp[i][0] = 0;
+        }
+
+        for(int j=1; j*coins[0]<=amount; j++){
+            dp[0][coins[0]*j] = j;
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<=amount; j++){
+                int notPick = dp[i-1][j];
+                int pick = (j >= coins[i]) ? 1 + dp[i][j - coins[i]] : 1e9;
+
+                dp[i][j] = min(pick, notPick);
+            }
+        }
+
+        if(dp[n-1][amount] != 1e9) return dp[n-1][amount];
+        return -1;
     }
 };
