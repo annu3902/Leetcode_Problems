14ab0382d9b9e87848c7a76b0d2277c3fe418ffc
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-01 01:42:13 +0530

    Time: 94 ms (24.80%) | Memory: 76.1 MB (7.87%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,62 +1,22 @@
 class Solution {
 public:
-int dp[2][1001];
+    int longestPalindromeSubseq(string s1) {
+        int n = s1.size();
+        string s2 = s1;
+        reverse(begin(s2), end(s2));
 
-    int lcs(string s, string t, int i, int j){
-        if(i >= s.size() && j >= t.size()){
-            return 0;
-        }
-        else if(i >= s.size() && j < t.size()) return 0;
-
-        else if(i < s.size() && j >= t.size()) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i] == t[j]){
-            int result = 1 + lcs(s, t, i+1, j+1);
-            dp[i][j] = result;
-            return result;
-        }
-
-        int op1 = lcs(s, t, i, j+1);
-        int op2 = lcs(s, t, i+1, j);
-
-        return dp[i][j] = max(op1, op2);
-
-    }
-
-    int longestPalindromeSubseq(string s) {
-        int n = s.size();
-        string t = s;
-        reverse(t.begin(), t.end());
-        memset(dp, 0, sizeof(dp));
-
-        // return lcs(s,t,0,0);
-
-        // dp[i][j] -> Longest Common Subsequence ending at length i at string 1 and j at string 2
-
-        // Base Case
-        /*for(int i=0; i<=n; i++) dp[i][0] = 0;
-        for(int j=0; j<=n; j++) dp[0][j] = 0;*/
-        vector<int> prev(n+1, 0);
-        vector<int> curr(n+1, 0); 
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
 
         for(int i=1; i<=n; i++){
-
             for(int j=1; j<=n; j++){
-                if(s[i-1] == t[j-1]){
-                    curr[j] = 1 + prev[j-1];
-                }
-                else{
-                    curr[j] = max(prev[j], curr[j-1]);
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-            prev = curr;
-
         }
-        return prev[n];
-    }
 
-    // bbbab -> bab
-    // babbb -> bab --> LCS between the string and its reverse
+        return dp[n][n];
+    }
 };
