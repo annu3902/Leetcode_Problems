1abb0ebecaa00fcd92a2e4b9e897ee4983e4b004
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-01 19:16:35 +0530

    Time: 23 ms (39.03%) | Memory: 11 MB (50.88%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -2,26 +2,36 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.size();
-        // vector<vector<int>> dp(n, vector<int> (n, -1));
 
-        int count = 0;
+        // dp[i][j] -> Returns if the substring starting from i and ending at j is a palindrome or not
+        vector<vector<bool>> dp(n, vector<bool>(n, 0));
+        // Length 1 string is always a palindrome
+        int cntPalindormicSubstring = 0;
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(dfs(i, j, s)){
-                    count++;
+            dp[i][i] = 1;
+            cntPalindormicSubstring++;
+        }
+
+        for(int i=0; i<n-1; i++){
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = 1;
+                cntPalindormicSubstring++;
+            }
+        }
+
+        for(int length=3; length<=n; length++){
+            for(int start=0; start + length <= n; start++){
+                int end = start + length - 1;
+
+                if(s[start] == s[end] && dp[start + 1][end - 1] == 1){
+                    dp[start][end] = 1;
+                    cntPalindormicSubstring++;
+                }else{
+                    dp[start][end] = 0;
                 }
             }
         }
-        return count;
-    }
 
-    bool dfs(int start, int end, string& s){
-        if(start > end){
-            return true;
-        }
-
-        if(s[start] != s[end]) return false;
-
-        return dfs(start+1, end-1, s);
+        return cntPalindormicSubstring;
     }
 };
