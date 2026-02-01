89a1424353577eb3d9d8bfa66728aadf5454103c
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-01 11:18:41 +0530

    Time: 15 ms (53.12%) | Memory: 18.7 MB (38.57%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,44 +1,37 @@
 class Solution {
+private:
+    string expand(int l, int r, string& s){
+        int start = l;
+        int length = 1;
+        
+        while(l >= 0 && r < s.size() && s[l] == s[r]){
+            start = l;
+            length = r - l + 1;
+            l--;
+            r++;
+        }
+
+        return s.substr(start, length);
+    }
 public:
     string longestPalindrome(string s) {
-        int n =s.size();
-        // dp[i][j] -> true if Substring s[i..j] is a Palindrome 
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int startInd = 0;
-        int maxLength = 0;
+        int n = s.length();
+        string ans="";
 
-        // length 1 substring
         for(int i=0; i<n; i++){
-            maxLength = 1;
-            startInd = i;
-            dp[i][i] = true;
-        }
+            string odd = expand(i, i, s);
 
-        // length 2 substrng
-        for(int i=0; i<n-1; i++){
-            if(s[i] == s[i+1]){
-                maxLength = 2;
-                startInd = i;
-                dp[i][i+1] = true;
+            string even = expand(i, i+1, s);
+
+            if(odd.size() > ans.size()){
+                ans = odd;
+            }
+
+            if(even.size() > ans.size()){
+                ans = even;
             }
         }
 
-        // Check from the length 3
-
-        for(int length = 3; length <= n; length++){
-            for(int start = 0; start + length <= n; start++){
-                int end = start + length - 1;
-
-                if(s[start] == s[end] && dp[start+1][end-1] == true){
-                    dp[start][end] = true;
-                    startInd = start;
-                    maxLength = length;
-                }  
-            }
-        }
-
-        string ans = s.substr(startInd, maxLength);
         return ans;
-
     }
 };
