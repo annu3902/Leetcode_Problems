7c3ad654beed7ddfddbb64053b1984313f6669c6
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-08 15:40:32 +0530

    Time: 30 ms (57.28%) | Memory: 44.5 MB (12.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,32 +1,47 @@
 class Solution {
 private:
-    long long dfs(int i, int j, string& s, string& t, vector<vector<long long>>& memo) {
+    int dfs(int i, int j, vector<vector<int>>& dp, string& s, string& t, vector<vector<int>>& memo){
 
-        if(j == 0) return 1;     // formed t completely
-        if(i == 0) return 0;     // s finished but t remains
+        if(j == 0) return 1;
+        if(i == 0) return 0;
 
-        if(memo[i][j] != -1)
-            return memo[i][j];
+        if(memo[i][j] != -1) return memo[i][j];
 
-        long long ans = 0;
-
-        if(s[i-1] == t[j-1]) {
-            ans += dfs(i-1, j-1, s, t, memo);  // take
+        int op1 = 0;
+        int op2 = 0;
+        
+        if(s[i-1] == t[j-1]){
+            op1 = dfs(i-1, j-1, dp, s, t, memo);
         }
 
-        ans += dfs(i-1, j, s, t, memo);        // skip
-
-        return memo[i][j] = ans;
+        if(dp[i][j] == dp[i-1][j]){
+            op2 = dfs(i-1, j, dp, s, t, memo);
+        }
+        return memo[i][j] = op1 + op2;
     }
 
 public:
     int numDistinct(string s, string t) {
-
         int m = s.size();
         int n = t.size();
 
-        vector<vector<long long>> memo(m+1, vector<long long>(n+1, -1));
+        if(m < n) return 0;
 
-        return dfs(m, n, s, t, memo);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1] == s[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        vector<vector<int>> memo(m+1, vector<int>(n+1, -1));
+
+        return dfs(m, n, dp, s, t, memo);
+
     }
 };
