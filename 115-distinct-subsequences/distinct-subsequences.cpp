9342663a8936dbcbc38d0d1998c22ba525ec10ee
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-08 20:47:07 +0530

    Time: 18 ms (78.35%) | Memory: 44.3 MB (20.71%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -25,23 +25,22 @@ public:
 
     vector<vector<double>> dp(m+1, vector<double>(n+1, 0));
 
-    // Space optimization
+    // Single array Space optimization
     vector<double> prev(n+1, 0);
-    vector<double> curr(n+1, 0);
+    // vector<double> curr(n+1, 0);
 
     prev[0] = 1;
-    curr[0] = 1;
+    // curr[0] = 1;
 
     for(int i = 1; i <= m; i++){
-        for(int j = 1; j <= n; j++){
+        for(int j = n; j >= 1; j--){
 
             if(s[i-1] == t[j-1])
-                curr[j] = prev[j-1] + prev[j];
+                prev[j] = prev[j-1] + prev[j];
             else{
-                curr[j] = prev[j];
+                prev[j] = prev[j];
             }           
         }
-        prev = curr;
     }
 
     return (int)prev[n];
