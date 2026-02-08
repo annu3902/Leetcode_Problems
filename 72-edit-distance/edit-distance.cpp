c3976d104e87c730d3a45907ff9b8b7c900113e7
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-08 21:34:16 +0530

    Time: 8 ms (46.87%) | Memory: 13.3 MB (31.80%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,88 +1,56 @@
 class Solution {
+private:
+    int dfs(int i, int j, string& s, string& t, vector<vector<int>>& dp){
+        if(j < 0 && i < 0) return 0;
+        if(i < 0) return 1 + dfs(i, j-1, s, t, dp);
+        if(j < 0) return 1 + dfs(i-1, j, s, t, dp);
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int ins = 0, del = 0, rep = 0;
+        if(s[i] == t[j]){
+            return dp[i][j] = dfs(i-1, j-1, s, t, dp);
+        }
+        
+        else{
+            ins = 1 + dfs(i, j-1, s, t, dp);
+            del = 1 + dfs(i-1, j, s, t, dp);
+            rep = 1 + dfs(i-1, j-1, s, t, dp);
+        }
+       
+        return dp[i][j] = min({ins, del, rep});
+    }
+
 public:
-    int minDistance(string word1, string word2) {
-        int m = word1.length();
-        int n = word2.length();
+    int minDistance(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        
         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        // dp[i][j] -> Min. no of operations required to convert s[0..i] to t[0..j];
 
-        // dp[i][j] -> minimum number of operations required to convert string 1 of length i and string2 of length j.
-
-        for(int col=0; col <= n; col++){
-            dp[0][col] = col;
+        for(int j=0; j<=n; j++){
+            dp[0][j] = j;
         }
 
-        for(int row=0; row <= m; row++){
-            dp[row][0] = row;
+        for(int i=0; i<=m; i++){
+            dp[i][0] = i;
         }
 
-        for(int i=1; i <= m; i++){
-            for(int j=1; j <= n; j++){
-                if(word1[i-1] == word2[j-1]){
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1] == t[j-1]){
                     dp[i][j] = dp[i-1][j-1];
                 }
-
                 else{
-                    int op1 = 1 + dp[i][j-1];
-                    int op2 = 1 + dp[i-1][j-1];
-                    int op3 = 1 + dp[i-1][j];
+                    int ins = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int rep = 1 + dp[i-1][j-1];
 
-                    dp[i][j] = min({op1, op2, op3});
+                    dp[i][j] = min({ins, del, rep});
                 }
-
             }
         }
-
         return dp[m][n];
-        
-        
-        // dp[i][j] -> minimum number of operations required to convert string1 of length i to string2 of length j.
-
-        // return solve(word1, word2, m, n);
     }
-
-    // Top to down Recursion
-    /*int dfs(string &word1, string &word2, int i, int j){
-
-        if(i == word1.length() && j == word2.length()) return 0;
-
-        else if(j == word2.size()) return (word1.size()-i);
-
-        else if(i == word1.size()) return (word2.size()-j);
-
-        else if(dp[i][j] != -1) return dp[i][j];
-
-        if(word1[i] == word2[j]) return dfs(word1, word2, i+1, j+1);
-
-        int op1 = 1 + dfs(word1, word2, i, j+1);
-
-        int op2 = 1 + dfs(word1, word2, i+1, j+1);
-
-        int op3 = 1 + dfs(word1, word2, i+1, j);
-
-        return dp[i][j] = min({op1, op2, op3});
-        
-    }
-
-    // Bottom to up Recursion
-
-    int solve(string &word1, string &word2, int m, int n){
-
-        if(m == 0 && n == 0) return 0;
-
-        if(n == 0 || m == 0) return m + n;
-
-        if(dp[m][n] != -1) return dp[m][n];
-
-        if(word1[m-1] == word2[n-1]) return solve(word1, word2, m-1, n-1);
-
-        int op1 = 1 + solve(word1, word2, m, n-1);
-
-        int op2 = 1 + solve(word1, word2, m-1, n-1);
-
-        int op3 = 1 + solve(word1, word2, m-1, n);
-
-        return dp[m][n] = min({op1, op2, op3});
-
-    }*/
-
 };
