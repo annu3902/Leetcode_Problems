539e2a9aa047b83d3dda20201ecf2a30a1183465
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-09 00:02:15 +0530

    Time: 51 ms (53.78%) | Memory: 54.5 MB (23.17%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -19,12 +19,10 @@ private:
         }
         
         else if(p[j] == '*'){
-
             for(int k=i; k>=0; k--){
                 if(dfs(k-1, j, s, p, dp) == true) return dp[i][j] = true;
             }
             return dp[i][j] = dfs(i, j-1, s, p, dp);
-            // return dfs(i-1, j, s, p, dp) || dfs(i, j-1, s, p, dp);
         }
         
         return dp[i][j] = false;
@@ -36,8 +34,30 @@ public:
         int m = s.size();
         int n = p.size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        // dp[i][j] -> gives true if the string s[0..i] matches with string t[0..j].
+        dp[0][0] = 1;
 
-        return dfs(m-1, n-1, s, p, dp);
+        for(int j=1; j<=n; j++){
+            if(p[j-1] == '*') dp[0][j] = dp[0][j-1];
+        }
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+                else if(p[j-1] == '*'){
+                    // for(int k=i; k<=m; k++){
+                    //     if(dp[k][j] == true) dp[i][j] = true;
+                    // }
+                    // dp[i][j] = dp[i][j-1];
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                }
+
+            }
+        }
+        return dp[m][n];
     }
 };
