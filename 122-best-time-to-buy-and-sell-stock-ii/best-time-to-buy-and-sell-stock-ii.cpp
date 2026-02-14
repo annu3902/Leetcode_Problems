e6e1fe30f93239c46f3f67a9273a9ca0493c9907
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-14 22:53:19 +0530

    Time: 7 ms (22.38%) | Memory: 24.8 MB (7.84%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -22,8 +22,26 @@ int dfs(int ind, int optionToBuy, vector<int>& prices, vector<vector<int>>& dp){
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();   
-        vector<vector<int>> dp(n, vector<int>(2, -1));
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        // dp[i][1] ->  max profit generate by giving the option to buy the ith stock
+        // dp[i][0] -> max profit generate by not giving the option to buy ith stock
 
-        return dfs(0, 1, prices, dp);
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int i=n-1; i>=0; i--){
+            int profit = 0;
+            for(int j=0; j<=1; j++){
+                if(j == 1){
+                   profit = max(-prices[i] + dp[i+1][0], 0 + dp[i+1][1]);
+                }
+                else{
+                    profit = max(prices[i] + dp[i+1][1], 0 + dp[i+1][0]);
+                }
+                dp[i][j] = profit;
+            }            
+        }
+
+        return dp[0][1];
     }
+
 };
