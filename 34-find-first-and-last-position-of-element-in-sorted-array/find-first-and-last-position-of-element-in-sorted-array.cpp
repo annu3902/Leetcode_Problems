b758b6b64b00a8a9ac31197b41e3d8b8e2879cc2
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-02-16 23:54:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (87.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,76 +1,58 @@
 class Solution {
+private:
+    int findStart(vector<int>& nums, int target){
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] > target){
+                end = mid - 1;
+            }else if (nums[mid] < target){
+                start = mid + 1;
+            }else {
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int findEnd(vector<int>& nums, int target){
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        int ans = -1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] > target){
+                end = mid - 1;
+            }else if (nums[mid] < target){
+                start = mid + 1;
+            }else {
+                ans = mid;
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+
+
 public:
-
-    int findStart(vector<int>& nums, int target, int start){
-        int n = nums.size();
-        int low = 0;
-        int high = n-1;
-
-        while(low <= high){
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] < target){
-                low = mid + 1;
-            }
-
-            else if(nums[mid] > target){
-                high = mid - 1;
-            }
-
-            else{
-                start = mid;
-                high = mid - 1;
-                // [8 8 8 8 8]
-            }
-        }
-
-        return start;
-    }
-
-    int findEnd(vector<int>& nums, int target, int end){
-        int n = nums.size();
-        int low = 0;
-        int high = n-1;
-
-        while(low <= high){
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] < target){
-                low = mid + 1;
-            }
-
-            else if(nums[mid] > target){
-                high = mid - 1;
-            }
-
-            else{
-                end = mid;
-                low = mid + 1;
-                // [8 8 8 8 8]
-            }
-        }
-
-        return end;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result;
-
-        int start = -1;
-        int end = -1;
         int n = nums.size();
 
-        int low = 0;
-        int high = n-1;
+        int start = findStart(nums, target);
+        int end = findEnd(nums, target);
 
-        start = findStart(nums, target, start);
-
-        if(start == -1) return {-1, -1};
-        end = findEnd(nums, target, end);
-
-        result.push_back(start);
-        result.push_back(end);
-
-        return result;
+        return {start, end};
     }
 };
