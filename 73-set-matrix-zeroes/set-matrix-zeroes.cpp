1dc0180f628b9a09a487424a4bc7165f7b36eb0e
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-05-09 18:48:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (92.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -15,24 +15,42 @@ public:
         // vector<vector<int>> duplicate = matrix;
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> row(m, 0);
-        vector<int> col(n, 0);
+        int col0 = 1;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+                   if(j == 0){
+                        col0 = 0;
+                   }
+                   else{
+                        matrix[0][j] = 0;
+                   }
+                   matrix[i][0] = 0;
                 }
             }
         }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(row[i] == 1 || col[j] == 1){
+        for(int i=m-1; i>=1; i--){
+            for(int j=n-1; j>=1; j--){
+                if(matrix[0][j] == 0|| matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
+
+        for(int j=n-1; j>=0; j--){
+            if(matrix[0][0] == 0){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(col0 == 0){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
+
     }
 };
