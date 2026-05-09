77184d88fc79cb0e7044237496c6468a9a1f7794
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-05-09 17:50:43 +0530

    Time: 3 ms (29.31%) | Memory: 21.8 MB (10.12%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -15,11 +15,27 @@ public:
         vector<vector<int>> duplicate = matrix;
         int m = matrix.size();
         int n = matrix[0].size();
+        unordered_set<int> row;
+        unordered_set<int> col;
+
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(duplicate[i][j] == 0){
-                    changeMatrix(matrix, i, j);
+                    row.insert(i);
+                    col.insert(j);
                 }
+            }
+        }
+
+        for(auto it : row){
+            for(int j=0; j<n; j++){
+                matrix[it][j] = 0;
+            }
+        }
+
+        for(int it: col){
+            for(int i=0; i<m; i++){
+                matrix[i][it] = 0;
             }
         }
     }
