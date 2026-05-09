3f93fa914c360c7b53ecb1bbebc2f32862d8c9f8
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-05-09 17:35:03 +0530

    Time: 27 ms (13.38%) | Memory: 21.3 MB (15.97%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,55 +1,26 @@
 class Solution {
+private:
+    void changeMatrix(vector<vector<int>>& matrix, int row, int col){
+        for(int j=0; j<matrix[0].size(); j++){
+            matrix[row][j] = 0;
+        }
+
+        for(int i=0; i<matrix.size(); i++){
+            matrix[i][col] = 0;
+        }
+    }
+
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        vector<vector<int>> duplicate = matrix;
         int m = matrix.size();
         int n = matrix[0].size();
-
-        bool rowZ = 0;
-        bool colZ = 0;
-
-        for(int j = 0; j < n; j++){
-            if(matrix[0][j] == 0){
-                rowZ = 1;
-                break;
-            }
-        }
-
-        for(int i = 0; i < m; i++){
-            if(matrix[i][0] == 0){
-                colZ = 1;
-                break;
-            }
-        }
-
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                if(matrix[i][j] == 0){
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(duplicate[i][j] == 0){
+                    changeMatrix(matrix, i, j);
                 }
             }
         }
-
-        for(int i = 1; i<m; i++){
-            for(int j = 1; j < n; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        if(rowZ == true){
-            for(int j = 0; j < n; j++){
-                matrix[0][j] = 0;
-            }
-        }
-
-        if(colZ == true){
-            
-            for(int i = 0; i < m; i++){
-                matrix[i][0] = 0;
-            }
-        }
-        
     }
 };
