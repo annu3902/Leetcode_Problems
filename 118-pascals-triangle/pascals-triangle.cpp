788d4e7eaf5cd2a043b5cf26ab2df82e28915e9f
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-05-09 19:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (88.87%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int n) {
-        vector<vector<int>> matrix(n);
-
-        for(int i = 0; i < n; i++){
-            vector<int> temp(i + 1, 0);
-
-            for(int j = 0; j <= i; j++){
-                if(j == 0) temp[j] = 1;
-                else if(j <= i-1){
-                    temp[j] = matrix[i-1][j] + matrix[i-1][j-1];
-                }
-                else{
-                    temp[j] = 1;
-                }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        ans.push_back({1});
+        for(int i = 1; i<numRows; i++){
+            vector<int> cols(i+1, 1);
+            for(int j = 1; j < i; j++){
+                cols[j] = ans[i-1][j] + ans[i-1][j-1];
             }
-            matrix[i] = (temp);
+            ans.push_back(cols);
         }
-
-        return matrix;
+    return ans;
     }
 };
