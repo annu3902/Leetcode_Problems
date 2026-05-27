88a62b40a2116d321fb0e0f37263fc1a70ec392a
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-05-27 09:01:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (53.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,94 +1,22 @@
-// class Solution {
-// public:
-//     bool isPalindrome(string s) {
-//         string t = "";
-//         for(int i =0; i<s.size(); i++)
-//         {
-//             if(s[i] >= 65 && s[i] <= 90){
-//                 t.push_back(s[i] - 'A' + 'a');
-//             }
-
-//             else if(s[i] >= 97 && s[i] <= 122){
-//                 t.push_back(s[i]);
-//             }
-
-//             else if(s[i] >= 48 && s[i] <= 57){
-//                 t.push_back(s[i]);
-//             }
-
-//             else{
-//                 continue;
-//             }
-//         }
-
-//         for(int i=0; i<t.size(); i++)
-//         {
-//             cout<<t[i];
-//         }
-//         string temp = t;
-//         reverse(temp.begin(), temp.end());
-        
-//         for(int i=0; i<temp.size(); i++)
-//         {
-//             if(temp[i] != t[i]){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// };
-
-// Best Solution
-// class Solution {
-// public:
-//     bool isPalindrome(string s) {
-//         int i = 0, j = s.size() - 1;
-//         while (i < j) {
-//             if (!isalnum(s[i])) {
-//                 i++;
-//                 continue;
-//             }
-//             if (!isalnum(s[j])) {
-//                 j--;
-//                 continue;
-//             }
-//             if (tolower(s[i]) != tolower(s[j]))
-//                 return false;
-//             i++;
-//             j--;
-//         }
-//         return true;
-//     }
-// };
-
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string temp;
+        int left = 0;
+        int right = s.size()-1;
 
-        for(int i=0; i<s.size(); i++){
-
-            if(s[i] >= 65 && s[i] <= 90){
-                temp.push_back(tolower(s[i]));
-            }
-            else if(s[i] >= 97 && s[i] <= 122){
-                temp.push_back(s[i]);
-            }
-            else if(s[i] >= 48 && s[i] <= 57){
-                temp.push_back(s[i]);
-            }
-        }
-
-        int i=0;
-        int j=temp.size()-1;
-
-        while(i < j){
-            if(temp[i] != temp[j]){
+        while(left <= right){
+            if(!isalnum(s[left])){
+                left++;
+            }else if(!isalnum(s[right])){
+                right--;
+            }else if(tolower(s[left]) != tolower(s[right])){
                 return false;
+            }else{
+                left++;
+                right--;
             }
-            i++;
-            j--;
         }
-        return true; 
-   }
+
+        return true;
+    }
 };
