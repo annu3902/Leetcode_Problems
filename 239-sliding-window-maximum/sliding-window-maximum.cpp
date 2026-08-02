fbe7377b3ad27952148f9c3d7aebd29ddc6cb369
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-08-02 16:41:01 +0530

    Time: 56 ms (18.05%) | Memory: 153.1 MB (16.57%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,111 +1,27 @@
-// class Solution {
-// public:
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         int n =nums.size();
-//         int i =0;
-//         int j=0;
-//         list <int> ls;
-//         int maxi =INT_MIN;
-//         vector<int> ans;
-
-//         while(j < n){
-//             while(ls.size() > 0 && ls.back() < nums[j]){
-//                 ls.pop_back();
-//             }
-//                 ls.push_back(nums[j]);
-
-//             if(j - i +1 <k){
-//                 j++;
-//             }
-
-//             else if(j -i +1 == k)
-//             {
-//                 ans.push_back(ls.front());
-//                 maxi = ls.front();
-
-//                 if( nums[i] == maxi){
-//                     ls.pop_front();
-//                 }
-
-//                 i++;
-//                 j++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-// // class Solution {
-// // public:
-// //     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-// //         int n = nums.size();
-// //         int i = 0;
-// //         int j = 0;
-// //         list<int> ls;
-// //         vector<int> ans;
-
-// //         while (j < n) {
-// //             while (!ls.empty() && ls.back() < nums[j]) {
-// //                 ls.pop_back();
-// //             }
-// //             ls.push_back(nums[j]);
-
-// //             if (j - i + 1 < k) {
-// //                 j++;
-// //             } else if (j - i + 1 == k) {
-// //                 ans.push_back(ls.front());
-
-// //                 if (nums[i] == ls.front()) {
-// //                     ls.pop_front();
-// //                 }
-
-// //                 i++;
-// //                 j++;
-// //             }
-// //         }
-
-// //         return ans;
-// //     }
-// // };
-
-class Solution{
+class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k){
-        
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        int i =0;
-        int j =0;
-        int n =nums.size();
-        list<int> ls;
-        
-        while(j < n){
-            // Add calcultaions for nums[j]
-            while(!ls.empty() && ls.back() < nums[j]){
-                ls.pop_back();
+        int left = 0;
+        int right = 0;
+        int n = nums.size();
+        priority_queue<pair<int,int>> pq; 
+
+        while(right < n){
+            pq.push({nums[right], right});
+            if(right - left + 1 < k){
+                right++;
+                continue;
             }
-
-            ls.push_back(nums[j]);
-
-            if(j-i+1 < k){
-                j++;
+            
+            while(pq.top().second < right - k + 1){
+                pq.pop();
+                left++;
             }
-
-            else if(j-i+1 == k){
-                // ans calculation
-                ans.push_back(ls.front());
-
-
-                // remove calculations for nums[i]
-                if(nums[i] == ls.front()){
-                    ls.pop_front();
-                }
-
-
-                // Slide the window
-                i++;
-                j++;
-            }
+            ans.push_back(pq.top().first);
+            right++;
         }
+
         return ans;
     }
 };
