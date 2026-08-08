9e598cddcd5cfa2f70a89ee9dfd8d2446eb101d6
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-08-08 20:06:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.64%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // int temp =x;
-        // long long int i;
-        // for(i=0; i*i<=temp; i++)
-        // {
-        //     if(i*i == temp){
-        //         return i;
-        //     }
-        // }
-        // return i-1;
-
+        if(x == 0) return 0;
         
-            double low = 1;
-            double high = x;
-            long long int mid = -1;
-            if(x == 0) return 0;
-            double ans = INT_MIN;
-            while(low <= high){
-                mid = low + (high-low)/2;
+        int left = 1;
+        int right = x;
+        int ans = -1;
 
-                if(mid*mid <= x){
-                    ans = mid;
-                    low = mid+1;
-                }
-                else{
-                    high = mid-1;
-                }
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(mid > x/mid){
+                right = mid - 1;
+            }else{
+                ans = mid;
+                left = mid + 1;
             }
-            cout<<ans<<endl;
-            return ans;
+        }
+
+        return ans;
     }
 };
