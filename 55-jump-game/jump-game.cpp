3c1ea172ce926e504baaa755561ea8a34bdcbacf
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-08-11 10:17:29 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (58.40%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 0);
-        dp[n-1] = true;
+        // vector<int> dp(n, 0);
+        // dp[n-1] = true;
+        int lastReachableIndex = n-1;
 
         for(int i=n-2; i>=0; i--){
-            for(int j=0; j<=nums[i]; j++){
-                if(dp[j + i] == true){
-                    dp[i] = true;
-                    break;
-                }
-            }
+            if(i + nums[i] >= lastReachableIndex){
+                lastReachableIndex = i;
+            }  
         }
 
-        return dp[0];
+        return (lastReachableIndex == 0) ? true : false;
     }
 };
