467978ae56b237063309db6a8180d5c86c881655
Anmol Gupta <100219424+annu3902@users.noreply.github.com>
2026-08-21 08:52:07 +0530

    Time: 4 ms (74.17%) | Memory: 45.3 MB (50.25%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -2,29 +2,35 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
-        int ans[] = new int[n];
-        Arrays.fill(ans, -1);
-        // ArrayList<Integer> arr = new ArrayList<>();
 
-        for(int i=0; i<n; i++){
-            // Find the element in nums2
-            int idx = -1;
-            for(int j=0; j<m; j++){
-                if(nums2[j] == nums1[i]){
-                    idx = j;
-                    break;
-                }
-            }
-            ans[i] = -1;
+        Map<Integer, Integer> ans = new HashMap<>();
 
-            for(int j=idx+1; j<m; j++){
-                if(nums2[j] > nums2[idx]){
-                    ans[i] = nums2[j];
-                    break;
-                }
+        // unordered_map<int, int> ans;
+        Stack<Integer> st = new Stack<>();
+        // stack<int> st;
+        st.push(nums2[m-1]);
+        ans.put(nums2[m-1], -1);
+        // ans[nums2[m-1]] = -1;
+
+        for(int i=m-2; i>=0; i--){
+            while(!st.empty() && nums2[i] > st.peek()){
+                st.pop();
             }
+            if(!st.empty()){
+                ans.put(nums2[i], st.peek());
+            }else{
+                ans.put(nums2[i], -1);
+            }
+
+            // ans.put(nums2[i]) = (!st.empty()) ? st.top() : -1;
+            st.push(nums2[i]);
         }
 
-        return ans;
+        int[] result = new int[n];
+        for(int i=0; i<n; i++){
+            result[i] = (ans.get(nums1[i]));
+        }
+
+        return result;
     }
 }
